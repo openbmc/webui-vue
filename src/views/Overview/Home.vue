e497b8fb2fc13869973b942d9f9f340798733edb
Dixsie Wolmers <dixsiew@gmail.com>
2019-12-02 05:07:57 -0600

    Add system overview view
    - Sets up system overview layout with out of the box Bootstrap
    - Creates overview quick links and overview events
    - Add Date-FNS
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Id0a3d4d3b82ef2e1a4f838b3c7e725558e602504

---
+++ src/views/Overview/Home.vue
@@ -0,0 +1,147 @@
+<template>
+  <b-container fluid>
+    <h1>Overview</h1>
+    <b-row>
+      <b-col lg="8" sm="12">
+        <section>
+          <h2>Server Information</h2>
+          <b-row>
+            <b-col sm="6">
+              <dl>
+                <dt>MODEL</dt>
+                <dd>{{ system.Model || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>MANUFACTURER</dt>
+                <dd>{{ system.Manufacturer || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>SERIAL NUMBER</dt>
+                <dd>{{ system.SerialNumber || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>FIRMWARE VERSION</dt>
+                <dd>{{ software.Version || "N/A" }}</dd>
+              </dl>
+            </b-col>
+          </b-row>
+        </section>
+        <section>
+          <h2>BMC information</h2>
+          <b-row>
+            <b-col sm="6">
+              <dl>
+                <dt>HOSTNAME</dt>
+                <dd>{{ network.config.HostName || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>MAC ADDRESS</dt>
+                <dd>{{ network.eth0.MACAddress || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>IP ADDRESS</dt>
+                <dd>{{ network.ipv4.Address || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>FIRMWARE VERSION</dt>
+                <dd>{{ logging.entry.Version || "N/A" }}</dd>
+              </dl>
+            </b-col>
+          </b-row>
+        </section>
+        <section>
+          <h2>Power consumption</h2>
+          <b-row>
+            <b-col sm="6">
+              <dl>
+                <dt>POWER CONSUMPTION</dt>
+                <dd>{{ total_power.description || "N/A" }}</dd>
+              </dl>
+            </b-col>
+            <b-col sm="6">
+              <dl>
+                <dt>POWER CAP</dt>
+                <dd v-if="!power_cap.PowerCapEnable">Not enabled</dd>
+                <dd v-else>{{ power_cap.PowerCap }}</dd>
+              </dl>
+            </b-col>
+          </b-row>
+        </section>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <quickLinks />
+      </b-col>
+    </b-row>
+    <section>
+      <h2>High priority events</h2>
+      <events />
+    </section>
+  </b-container>
+</template>
+
+<script>
+import OverviewQuickLinks from "./OverviewQuickLinks";
+import Events from "./Events";
+
+export default {
+  name: "Overview",
+  components: {
+    quickLinks: OverviewQuickLinks,
+    events: Events
+  },
+  data() {
+    return {
+      logging: {
+        entry: {
+          Description:
+            "An internal failure has occurred while performing an operation.",
+          EventID: "FQPSPCR0021F",
+          Id: 1,
+          Resolved: false,
+          Severity: "xyz.openbmc_project.Logging.Entry.Level.Error",
+          Timestamp: 1574782085071,
+          Version: "ibm-v2.7.0-rc1-5-gfd9b55f-r19-1-g8c075d3"
+        }
+      },
+      network: {
+        config: {
+          HostName: "witherspoon"
+        },
+        eth0: {
+          MACAddress: "00:00:00:00:00:00"
+        },
+        ipv4: {
+          Address: "00.00.00.00"
+        }
+      },
+      power_cap: {
+        PowerCap: 0,
+        PowerCapEnable: false
+      },
+      software: {
+        Version: "IBM-witherspoon-OP9-v2.4-4.22"
+      },
+      system: {
+        Manufacturer: "",
+        Model: "0000000000000000",
+        SerialNumber: "0000000000000000"
+      },
+      total_power: {
+        description: "0"
+      }
+    };
+  }
+};
+</script>
---
+++ src/views/Overview/OverviewQuickLinks.vue
@@ -0,0 +1,64 @@
+<template>
+  <b-list-group>
+    <b-list-group-item
+      href="#"
+      variant="danger"
+      v-show="logging.entry.Severity === 'Error'"
+      >View 1 high priority event</b-list-group-item
+    >
+    <b-list-group-item>
+      <dl>
+        <dt>BMC time</dt>
+        <dd>{{ bmc.Elapsed | date("MMM, DD YYYY HH:MM:SS A ZZ") }}</dd>
+      </dl>
+    </b-list-group-item>
+    <b-list-group-item>
+      <!-- TODO: add toggle LED on/off funtionality -->
+      <b-form-checkbox v-model="checked" name="check-button" switch>
+        Turn
+        <span v-if="!checked">on</span>
+        <span v-else>off</span> server LED
+      </b-form-checkbox>
+    </b-list-group-item>
+    <b-list-group-item
+      href="#"
+      class="d-flex justify-content-between align-items-center"
+    >
+      <!-- TODO: link to SOL -->
+      <span>Serial over LAN console</span>
+      <ChevronRight16 />
+    </b-list-group-item>
+    <b-list-group-item
+      href="#"
+      class="d-flex justify-content-between align-items-center"
+    >
+      <!-- TODO: link to network settings -->
+      <span>Edit network settings</span>
+      <ChevronRight16 />
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+import ChevronRight16 from "@carbon/icons-vue/es/chevron--right/16";
+
+export default {
+  name: "quickLinks",
+  components: {
+    ChevronRight16
+  },
+  data() {
+    return {
+      bmc: {
+        Elapsed: 1574782085071
+      },
+      logging: {
+        entry: {
+          Severity: "Error"
+        }
+      },
+      checked: false
+    };
+  }
+};
+</script>
