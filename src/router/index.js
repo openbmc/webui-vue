97d86b33d78c3b92b29915d6afba125b67bc6714
Dixsie Wolmers <dixsiew@gmail.com>
2019-12-02 05:07:57 -0600

    Add system overview view
    - Sets up system overview layout with out of the box Bootstrap
    - Creates overview quick links and overview events
    - Add Date-FNS
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Id0a3d4d3b82ef2e1a4f838b3c7e725558e602504

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../views/Overview/Home.vue";
 
 Vue.use(VueRouter);
 
---
+++ src/views/Overview/Events.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-list-group>
+    <b-list-group-item href="#" class="flex-column align-items-start">
+      #{{ logging.entry.Id }}
+      <b-badge variant="danger">{{ logging.entry.Severity }}</b-badge>
+      <div class="d-flex w-100 justify-content-between">
+        <small>{{
+          logging.entry.Timestamp | date("MMM, DD YYYY HH:MM:SS A ZZ")
+        }}</small>
+        <ChevronRight16 />
+      </div>
+      <p class="mb-1">
+        {{ logging.entry.EventID }}: {{ logging.entry.Description }}
+      </p>
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+import ChevronRight16 from "@carbon/icons-vue/es/chevron--right/16";
+
+export default {
+  name: "events",
+  components: {
+    ChevronRight16
+  },
+  data() {
+    return {
+      logging: {
+        entry: {
+          Description:
+            "An internal failure has occurred while performing an operation.",
+          EventID: "ABCDEF123",
+          Id: 1,
+          Resolved: false,
+          Severity: "Error",
+          Timestamp: 1574782085071
+        }
+      }
+    };
+  }
+};
+</script>
