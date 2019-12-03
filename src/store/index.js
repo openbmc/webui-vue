b8b6f7914f9371fc386fd5b4ea2e4b7a89892730
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-03 14:47:32 -0800

    Add GlobalStore module
    Create a GlobalStore to store global variables,
    including data in app header.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Id498dec3925feec9a7e31ede91328f89ae591a0b

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import LocalUserManagementStore from "./modules/AccessControl/LocalUserMangementStore";
+import GlobalStore from "./modules/GlobalStore";
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {
+    global: GlobalStore,
     localUsers: LocalUserManagementStore
   }
 });
