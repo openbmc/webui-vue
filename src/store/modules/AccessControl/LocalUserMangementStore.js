566e3ec22936fe16de7a6ae2e5b9cfbc646c8b03
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-03 10:45:46 -0800

    Add proxy dev server for local development
    Adding proxy server to vue config to handle API requests
    for local development. You need to create a .env.development.local file with
    BASE_URL, VUE_APP_USERNAME, VUE_APP_PASSWORD defined.
    
    Temporarily adding authentication to defaults until
    login flow is functional.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Ib1a1a992508fb9567af66ecb5775638e6ae6ee8d

--- src/store/modules/AccessControl/LocalUserMangementStore.js
+++ src/store/modules/AccessControl/LocalUserMangementStore.js
@@ -1,4 +1,4 @@
-import Axios from "axios";
+import api from "../../api";
 
 const LocalUserManagementStore = {
   namespaced: true,
@@ -17,38 +17,21 @@ const LocalUserManagementStore = {
   },
   actions: {
     getUsers({ commit }) {
-      let base;
-      let username;
-      let password;
-      if (base && username && password) {
-        Axios.defaults.baseURL = base;
-        Axios.defaults.auth = {};
-        Axios.defaults.auth.username = username;
-        Axios.defaults.auth.password = password;
-        Axios.get("redfish/v1/AccountService/Accounts")
-          .then(response => {
-            return response.data.Members.map(user => user["@odata.id"]);
-          })
-          .then(userIds => {
-            return Axios.all(userIds.map(user => Axios.get(user)));
-          })
-          .then(users => {
-            const userData = users.map(user => user.data);
-            commit("setUsers", userData);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      } else {
-        // Faking async call with timeout
-        setTimeout(() => {
-          const users = [
-            { UserName: "root", RoleId: "Admin", Locked: false, Enabled: true },
-            { UserName: "user1", RoleId: "user", Locked: false, Enabled: false }
-          ];
-          commit("setUsers", users);
-        }, 3000);
-      }
+      api
+        .get("/redfish/v1/AccountService/Accounts")
+        .then(response => {
+          return response.data.Members.map(user => user["@odata.id"]);
+        })
+        .then(userIds => {
+          return api.all(userIds.map(user => api.get(user)));
+        })
+        .then(users => {
+          const userData = users.map(user => user.data);
+          commit("setUsers", userData);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+  devServer: {
+    proxy: process.env.BASE_URL
+  }
+};
