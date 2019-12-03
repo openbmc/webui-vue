ac07cf5dbf218ba244e424e3479c72afdbf32e31
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-03 14:47:32 -0800

    Add GlobalStore module
    Create a GlobalStore to store global
    variables and include data in app header.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Id498dec3925feec9a7e31ede91328f89ae591a0b

---
+++ src/store/modules/GlobalStore.js
@@ -0,0 +1,35 @@
+import api from "../api";
+
+const GlobalStore = {
+  namespaced: true,
+  state: {
+    hostName: "--",
+    hostStatus: null
+  },
+  getters: {
+    hostName(state) {
+      return state.hostName;
+    },
+    hostStatus(state) {
+      return state.hostStatus;
+    }
+  },
+  mutations: {
+    setHostName(state, hostName) {
+      state.hostName = hostName;
+    }
+  },
+  actions: {
+    getHostName({ commit }) {
+      api
+        .get("/xyz/openbmc_project/network/config/attr/HostName")
+        .then(response => {
+          const hostName = response.data.data;
+          commit("setHostName", hostName);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+};
+
+export default GlobalStore;
---
+++ src/views/AccessControl/LocalUserManagement/TableRoles.vue
@@ -0,0 +1,110 @@
+<template>
+  <b-table bordered small head-variant="dark" :items="items" :fields="fields">
+    <template v-slot:cell(administrator)="data">
+      <template v-if="data.value">
+        <Checkmark20 />
+      </template>
+    </template>
+    <template v-slot:cell(operator)="data">
+      <template v-if="data.value">
+        <Checkmark20 />
+      </template>
+    </template>
+    <template v-slot:cell(readonly)="data">
+      <template v-if="data.value">
+        <Checkmark20 />
+      </template>
+    </template>
+    <template v-slot:cell(noaccess)="data">
+      <template v-if="data.value">
+        <Checkmark20 />
+      </template>
+    </template>
+  </b-table>
+</template>
+
+<script>
+import Checkmark20 from "@carbon/icons-vue/es/checkmark/20";
+
+export default {
+  components: {
+    Checkmark20
+  },
+  data() {
+    return {
+      items: [
+        {
+          description: "Configure components managed by this service",
+          administrator: true,
+          operator: false,
+          readonly: false,
+          noaccess: false
+        },
+        {
+          description: "Configure manager resources",
+          administrator: true,
+          operator: false,
+          readonly: false,
+          noaccess: false
+        },
+        {
+          description: "Update password for current user account",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: false
+        },
+        {
+          description: "Configure users and their accounts",
+          administrator: true,
+          operator: false,
+          readonly: false,
+          noaccess: false
+        },
+        {
+          description: "Log in to the service and read resources",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: false
+        },
+        {
+          description: "IPMI access point",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: true
+        },
+        {
+          description: "Redfish access point",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: false
+        },
+        {
+          description: "SSH access point",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: false
+        },
+        {
+          description: "WebUI access point",
+          administrator: true,
+          operator: true,
+          readonly: true,
+          noaccess: false
+        }
+      ],
+      fields: [
+        { key: "description", label: "" },
+        { key: "administrator", label: "Administrator", class: "text-center" },
+        { key: "operator", label: "Operator", class: "text-center" },
+        { key: "readonly", label: "ReadOnly", class: "text-center" },
+        { key: "noaccess", label: "NoAccess", class: "text-center" }
+      ]
+    };
+  }
+};
+</script>
