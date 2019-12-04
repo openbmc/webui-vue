b5caa1af24e422553a051ecf1b4630a5ff7fe87a
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-04 08:41:22 -0800

    Add host status plugin
    - Create WebSocket and get host state changes from server
    - Changed webpack devServer to https to allow for
      secure WebSocket creation (wss)
    - Updates to AppHeader to visually indicate changes
      in host state
    - Cleaned up api.js file
    - Check if user is logged in when creating WebSocket
    - Adds check if user is already authenticated so WebSocket
      is created when browser refreshed.
    - Add appliation header styles
    - Add sass loader config changes to allow sass variables to
      be used in single file components
    
    URL must use https protocol when running locally or the page
    will not load.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I35e89bdc09e1aa35a6215ef952409a8ed16dd9e1

--- src/store/modules/GlobalStore.js
+++ src/store/modules/GlobalStore.js
@@ -1,10 +1,36 @@
 import api from '../api';
 
+const HOST_STATE = {
+  on: 'xyz.openbmc_project.State.Host.HostState.Running',
+  off: 'xyz.openbmc_project.State.Host.HostState.Off',
+  error: 'xyz.openbmc_project.State.Host.HostState.Quiesced',
+  transition: {
+    on: 'xyz.openbmc_project.State.Host.Transition.On',
+    off: 'xyz.openbmc_project.State.Host.Transition.Off',
+    reboot: 'xyz.openbmc_project.State.Host.Transition.Reboot'
+  }
+};
+
+const hostStateMapper = hostState => {
+  switch (hostState) {
+    case HOST_STATE.on:
+      return 'on';
+    case HOST_STATE.off:
+      return 'off';
+    case HOST_STATE.error:
+      return 'error';
+    case HOST_STATE.transition.reboot:
+      return 'rebooting';
+    default:
+      return 'unreachable';
+  }
+};
+
 const GlobalStore = {
   namespaced: true,
   state: {
     hostName: '--',
-    hostStatus: null
+    hostStatus: 'unreachable'
   },
   getters: {
     hostName(state) {
@@ -17,6 +43,9 @@ const GlobalStore = {
   mutations: {
     setHostName(state, hostName) {
       state.hostName = hostName;
+    },
+    setHostStatus(state, hostState) {
+      state.hostStatus = hostStateMapper(hostState);
     }
   },
   actions: {
@@ -26,6 +55,15 @@ const GlobalStore = {
         .then(response => {
           const hostName = response.data.data;
           commit('setHostName', hostName);
+        })
+        .catch(error => console.log(error));
+    },
+    getHostStatus({ commit }) {
+      api
+        .get('/xyz/openbmc_project/state/host0/attr/CurrentHostState')
+        .then(response => {
+          const hostState = response.data.data;
+          commit('setHostStatus', hostState);
         })
         .catch(error => console.log(error));
     }
