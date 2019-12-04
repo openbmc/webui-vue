638abfbd05a04863ac01d2537f12a2dcd0b370ab
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-04 08:41:22 -0800

    Add host status plugin
    - Create WebSocket and get host state changes from server
    - Changed webpack devServer to https to allow for
      secure WebSocket creation (wss)
    - Updates to AppHeader to visually indicate changes
      in host state
    - Cleaned up api.js file
    - Check if user is logged in when creating WebSocket
    - Adds check if user is already authenticated so WebSocket
      is created when browser refreshed.
    - Add appliation header styles
    - Add sass loader config changes to allow sass variables to
      be used in single file components
    
    URL must use https protocol when running locally or the page
    will not load.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I35e89bdc09e1aa35a6215ef952409a8ed16dd9e1

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ import GlobalStore from './modules/GlobalStore';
 import AuthenticationStore from './modules/Authentication/AuthenticanStore';
 import LocalUserManagementStore from './modules/AccessControl/LocalUserMangementStore';
 
+import WebSocketPlugin from './plugins/WebSocketPlugin';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -15,5 +17,6 @@ export default new Vuex.Store({
     global: GlobalStore,
     authentication: AuthenticationStore,
     localUsers: LocalUserManagementStore
-  }
+  },
+  plugins: [WebSocketPlugin]
 });
