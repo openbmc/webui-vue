a4a6643617bfa5044bc732e2c910592d3c1c6ff4
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-04 09:09:36 -0800

    Update local user component
    - Add, edit, delete user basic functionality complete
    - Rename components and creating separate modal components
    - Update button styles to match design and included icons
    - Update grid layout to use container with max width set
    - Add aria labels to table action buttons
    - Refactor LocalUserManagementStore
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Iab31ccabeb5a53ec03dc3ce3949fb20ded1ffbcf

--- src/views/AccessControl/LocalUserManagement/TableRoles.vue
+++ src/views/AccessControl/LocalUserManagement/TableRoles.vue
@@ -1,6 +1,6 @@
 <template>
   <b-table bordered small head-variant="dark" :items="items" :fields="fields">
-    <template v-slot:cell(admin)="data">
+    <template v-slot:cell(administrator)="data">
       <template v-if="data.value">
         <Checkmark20 />
       </template>
@@ -10,7 +10,7 @@
         <Checkmark20 />
       </template>
     </template>
-    <template v-slot:cell(user)="data">
+    <template v-slot:cell(readonly)="data">
       <template v-if="data.value">
         <Checkmark20 />
       </template>
@@ -35,73 +35,73 @@ export default {
       items: [
         {
           description: "Configure components managed by this service",
-          admin: true,
+          administrator: true,
           operator: false,
-          user: false,
+          readonly: false,
           noaccess: false
         },
         {
           description: "Configure manager resources",
-          admin: true,
+          administrator: true,
           operator: false,
-          user: false,
+          readonly: false,
           noaccess: false
         },
         {
           description: "Update password for current user account",
-          admin: true,
-          operator: true,
-          user: true,
+          administrator: true,
+          operator: false, // TODO Set to true when profile page added
+          readonly: false, // TODO Set to true when profile page added
           noaccess: false
         },
         {
           description: "Configure users and their accounts",
-          admin: true,
+          administrator: true,
           operator: false,
-          user: false,
+          readonly: false,
           noaccess: false
         },
         {
           description: "Log in to the service and read resources",
-          admin: true,
+          administrator: true,
           operator: true,
-          user: true,
+          readonly: true,
           noaccess: false
         },
         {
           description: "IPMI access point",
-          admin: true,
+          administrator: true,
           operator: true,
-          user: true,
-          noaccess: true
+          readonly: true,
+          noaccess: false
         },
         {
           description: "Redfish access point",
-          admin: true,
+          administrator: true,
           operator: true,
-          user: true,
+          readonly: true,
           noaccess: false
         },
         {
           description: "SSH access point",
-          admin: true,
+          administrator: true,
           operator: false,
-          user: false,
+          readonly: false,
           noaccess: false
         },
         {
           description: "WebUI access point",
-          admin: true,
+          administrator: true,
           operator: true,
-          user: true,
+          readonly: true,
           noaccess: false
         }
       ],
       fields: [
         { key: "description", label: "" },
-        { key: "admin", label: "Admin", class: "text-center" },
+        { key: "administrator", label: "Administrator", class: "text-center" },
         { key: "operator", label: "Operator", class: "text-center" },
-        { key: "user", label: "User", class: "text-center" },
+        { key: "readonly", label: "ReadOnly", class: "text-center" },
         { key: "noaccess", label: "NoAccess", class: "text-center" }
       ]
     };
