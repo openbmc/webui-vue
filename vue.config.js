b07e91d772c1f21753820f49728e3dfe206a2530
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-06 14:13:59 -0800

    Add cookie based login authentication
    - Changed POST request data structure to match
      backend requirements for set-cookie in response
      header
    - Added withCredentials property to default
      axios config
    - Modifying proxied response to remove 'Secure' flag
      so browser can create Cookie while running locally
    - Add logout api request
    - Add js-cookie package to manage browser cookies
    - Update the babel preset config to include useBuiltIns,
    which resolves MIME type errors when overlaying
    - Disable vue-router history mode to use routher hash mode
    to resolves 404 errors when refreshing certain pages. This
    is expected behavior with history mode enabled. Server
    configuration changes are required to support HTML5
    history mode: https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I5d43f36ef546962474b6cc8fff89564f29048fde

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   devServer: {
-    proxy: process.env.BASE_URL
+    proxy: {
+      "/": {
+        target: process.env.BASE_URL,
+        onProxyRes: proxyRes => {
+          if (proxyRes.headers["set-cookie"]) {
+            // Need to remove 'Secure' flag on set-cookie value so browser
+            // can create cookie for local development
+            const cookies = proxyRes.headers["set-cookie"].map(cookie =>
+              cookie.replace(/; secure/gi, "")
+            );
+            proxyRes.headers["set-cookie"] = cookies;
+          }
+        }
+      }
+    }
   }
 };
