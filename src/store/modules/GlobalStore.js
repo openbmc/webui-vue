b9cba5d20d3d787abdfe90c1f1c994a7aa4d1acc
Derick Montague <derick.montague@ibm.com>
2019-12-11 06:16:40 -0600

    Add singlequote override and fix files
    Based on phosphor-webui and JavaScript common practices we are setting
    ESLint to required single quote for JS files and double quote for
    SCSS files. This commit adds the ESLint override to the prettier rules
    and runs the npm lint script that fixes the files that violate the
    rule.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I50cba77b2d0797595ce94258029608efa8665005

--- src/store/modules/GlobalStore.js
+++ src/store/modules/GlobalStore.js
@@ -1,9 +1,9 @@
-import api from "../api";
+import api from '../api';
 
 const GlobalStore = {
   namespaced: true,
   state: {
-    hostName: "--",
+    hostName: '--',
     hostStatus: null
   },
   getters: {
@@ -22,10 +22,10 @@ const GlobalStore = {
   actions: {
     getHostName({ commit }) {
       api
-        .get("/xyz/openbmc_project/network/config/attr/HostName")
+        .get('/xyz/openbmc_project/network/config/attr/HostName')
         .then(response => {
           const hostName = response.data.data;
-          commit("setHostName", hostName);
+          commit('setHostName', hostName);
         })
         .catch(error => console.log(error));
     }
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   devServer: {
     proxy: {
-      "/": {
+      '/': {
         target: process.env.BASE_URL,
         onProxyRes: proxyRes => {
-          if (proxyRes.headers["set-cookie"]) {
+          if (proxyRes.headers['set-cookie']) {
             // Need to remove 'Secure' flag on set-cookie value so browser
             // can create cookie for local development
-            const cookies = proxyRes.headers["set-cookie"].map(cookie =>
-              cookie.replace(/; secure/gi, "")
+            const cookies = proxyRes.headers['set-cookie'].map(cookie =>
+              cookie.replace(/; secure/gi, '')
             );
-            proxyRes.headers["set-cookie"] = cookies;
+            proxyRes.headers['set-cookie'] = cookies;
           }
         }
       }
@@ -21,7 +21,7 @@ module.exports = {
   },
   productionSourceMap: false,
   configureWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
         new CompressionPlugin({
           deleteOriginalAssets: true
@@ -30,9 +30,9 @@ module.exports = {
     }
   },
   chainWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
-      config.plugins.delete("prefetch");
-      config.plugins.delete("preload");
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.delete('prefetch');
+      config.plugins.delete('preload');
     }
   }
 };
