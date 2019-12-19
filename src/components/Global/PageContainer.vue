79be75664d41eefbe74017960b05eff4bd6184ea
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-12-19 09:51:55 -0800

    Add page level layout components
    Adding components to help standardize type, size, spacing
    for common elements on a page.
    
    Also removed a conflicting classname and added modifications
    to the main container. The main container needed a min-height
    and height value set, which became apparent with added
    background color. Adding a background color will move us
    closer to agreed design solution to add a sublte background
    color instead of adding a border to separate main content from
    left hand navigation.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Ie63c4f0c0f3fd199fa0ca790065402e06a613691

---
+++ src/components/Global/PageContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <main id="main-content">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PageContainer'
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  padding-top: $spacer * 1.5;
+  padding-bottom: $spacer * 3;
+  padding-left: $spacer * 2;
+  padding-right: $spacer;
+  background-color: $gray-100;
+  height: 100%;
+  min-height: calc(100vh - 60px /*header height*/);
+}
+</style>
