d13faa20e10156f7c889bc3dc4ec92c506a46374
Derick Montague <derick.montague@ibm.com>
2019-12-20 17:08:59 -0600

    Redirect user to login on 401 response
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I58609486956239d90a0dfec630f249dc3fa28ea2

--- src/store/api.js
+++ src/store/api.js
@@ -4,6 +4,14 @@ const api = Axios.create({
   withCredentials: true
 });
 
+api.interceptors.response.use(undefined, error => {
+  let response = error.response;
+  // TODO: Provide user with a notification and way to keep system active
+  if (response.status == 401) {
+    window.location = '/login';
+  }
+});
+
 export default {
   get(path) {
     return api.get(path);
