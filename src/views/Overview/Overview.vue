874f3af3661869202dfbae58b20c5be63ce2d228
Dixsie Wolmers <dixsiew@gmail.com>
2020-01-06 17:22:25 -0500

    Update overview quicklinks with community approved design
    Resubmitting this commit after reverted.
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Ib4b2cc8015b86a9f6de11ebcade57abe086c5990

--- src/views/Overview/Overview.vue
+++ src/views/Overview/Overview.vue
@@ -1,9 +1,12 @@
 <template>
   <b-container fluid>
     <PageTitle />
+    <div class="quicklinks-section">
+      <OverviewQuickLinks />
+    </div>
     <b-row>
-      <b-col lg="8" sm="12">
-        <PageSection sectionTitle="Server information">
+      <b-col>
+        <PageSection sectionTitle="Server Information">
           <b-row>
             <b-col sm="6">
               <dl>
@@ -76,9 +79,6 @@
           </b-row>
         </PageSection>
       </b-col>
-      <b-col lg="4" sm="12">
-        <OverviewQuickLinks />
-      </b-col>
     </b-row>
     <PageSection sectionTitle="High priority events">
       <OverviewEvents />
@@ -127,3 +127,10 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.quicklinks-section {
+  margin-bottom: $spacer * 2;
+  margin-left: -1rem;
+}
+</style>
