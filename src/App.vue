eaf37b164d3a4205e61376ebc0b2f00299c2d10f
Derick Montague <derick.montague@ibm.com>
2020-01-17 13:46:30 -0600

    Create basic application using vue
    -  Create vue app
    -  Add AppHeader and AppNavigation
    -  Set up routes for server overview and local user management
    -  Add themes and set to gray-10 theme
    -  Add active class for current navigation
    - Add a prototype method named $http to vue prototype
    - The UI uses console logs in production to communicate to users
    - Add boostrap vue component library
    
    Change-Id: Ib6618e6357ca01bdac6de4ed031a15c8552226f3
    Signed-off-by: Derick Montague <derick.montague@ibm.com>

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <AppNavigation />
+    <main id="#main-content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/_obmc-custom";
+</style>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppNavigation from "@/components/AppNavigation";
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppNavigation
+  }
+};
+</script>
