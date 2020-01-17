210be6862feceeb24ab65ba08725ddd08f8ab460
Derick Montague <derick.montague@ibm.com>
2020-01-17 13:46:30 -0600

    Create basic application using vue
    -  Create vue app
    -  Add AppHeader and AppNavigation
    -  Set up routes for server overview and local user management
    -  Add themes and set to gray-10 theme
    -  Add active class for current navigation
    - Add a prototype method named $http to vue prototype
    - The UI uses console logs in production to communicate to users
    - Add boostrap vue component library
    
    Change-Id: Ib6618e6357ca01bdac6de4ed031a15c8552226f3
    Signed-off-by: Derick Montague <derick.montague@ibm.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <AppOverview />
+</template>
+
+<script>
+// @ is an alias to /src
+import AppOverview from "@/components/AppOverview";
+
+export default {
+  name: "app-overview",
+  components: {
+    AppOverview
+  }
+};
+</script>
