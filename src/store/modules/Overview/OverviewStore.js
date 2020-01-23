c84d1ca568b429d5dc30d099ccc0b03836ad9f83
Dixsie Wolmers <dixsie@ibm.com>
2020-01-22 19:47:56 -0600

    Add store modules needed to support overview view
    - Update overview page to get data from store
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Id2fcad660efc0da5c7b878e872355bf5773c7ed7

---
+++ src/store/modules/Overview/OverviewStore.js
@@ -0,0 +1,48 @@
+import api from '../../api';
+
+const OverviewStore = {
+  namespaced: true,
+  state: {
+    serverInfo: null,
+    serverModel: '--',
+    serverManufacturer: '--',
+    serverSerialNumber: '--'
+  },
+  getters: {
+    serverInfo: state => state.serverInfo,
+    serverModel: state => state.serverModel,
+    serverManufacturer: state => state.serverManufacturer,
+    serverSerialNumber: state => state.serverSerialNumber
+  },
+  mutations: {
+    setServerInfo: (state, serverInfo) => (state.serverInfo = serverInfo),
+    setServerModel: (state, serverModel) => (state.serverModel = serverModel),
+    setServerManufacturer: (state, serverManufacturer) =>
+      (state.serverManufacturer = serverManufacturer),
+    setServerSerialNumber: (state, serverSerialNumber) =>
+      (state.serverSerialNumber = serverSerialNumber)
+  },
+  actions: {
+    getServerInfo({ commit }) {
+      api
+        .get('/xyz/openbmc_project/inventory/system')
+        .then(response => {
+          const serverInfo = response.data.data;
+          if (serverInfo.Model) {
+            commit('setServerModel', serverInfo.Model);
+          }
+          if (serverInfo.SerialNumber) {
+            commit('setServerSerialNumber', serverInfo.SerialNumber);
+          }
+          if (serverInfo.Manufacturer) {
+            commit('setServerManufacturer', serverInfo.Manufacturer);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+
+export default OverviewStore;
