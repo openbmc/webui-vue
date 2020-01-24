18c7759aa7faa9726ef1b1bd3247ab452091d514
Dixsie Wolmers <dixsie@ibm.com>
2020-01-24 11:21:37 -0600

    Update overview events table and overview network layout
    - Convert events list group to bootstrap table
    - Update date format in events table
    - Add interface name in network setting store
    - Add ethernet interface name to system overview
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I633f03ba4a65358861664a869b35adf5358c45e6

--- src/store/modules/Configuration/NetworkSettingsStore.js
+++ src/store/modules/Configuration/NetworkSettingsStore.js
@@ -4,16 +4,20 @@ const NetworkSettingsStore = {
   namespaced: true,
   state: {
     networkData: null,
+    interfaceName: '--',
     ipAddress: '--',
     macAddress: '--'
   },
   getters: {
     networkData: state => state.networkData,
+    interfaceName: state => state.interfaceName,
     ipAddress: state => state.ipAddress,
     macAddress: state => state.macAddress
   },
   mutations: {
     setNetworkData: (state, networkData) => (state.networkData = networkData),
+    setInterfaceName: (state, interfaceName) =>
+      (state.interfaceName = interfaceName),
     setIpAddress: (state, ipAddress) => (state.ipAddress = ipAddress),
     setMacAddress: (state, macAddress) => (state.macAddress = macAddress)
   },
@@ -26,17 +30,19 @@ const NetworkSettingsStore = {
           const ipAddresses = [];
           const interfaceId = /eth[0-9]/;
           for (let key in networkData) {
-            if (key.includes('ipv4')) {
-              ipAddresses.push(networkData[key].Address);
-            }
-            if (
-              key.match(interfaceId) &&
-              networkData[key].MACAddress !== undefined
-            ) {
-              commit('setMacAddress', networkData[key].MACAddress);
+            if (key.match(interfaceId)) {
+              if (networkData[key].InterfaceName !== undefined) {
+                commit('setInterfaceName', networkData[key].InterfaceName);
+              }
+              if (key.includes('ipv4')) {
+                ipAddresses.push(networkData[key].Address);
+                commit('setIpAddress', ipAddresses);
+              }
+              if (networkData[key].MACAddress !== undefined) {
+                commit('setMacAddress', networkData[key].MACAddress);
+              }
             }
           }
-          commit('setIpAddress', ipAddresses);
         })
         .catch(error => {
           console.log('Network Data:', error);
