gtmills/phosphor-webui-vue

Add interceptor for 403 response
This is a simple solution that is in parity with the current BMC
functionality. Once we have mapped permissions, we can create a more
elegant solution.

src/router/index.js
@@ -28,6 +28,14 @@ const routes = [
         meta: {
           title: 'Local user management'
         }
+      },
+      {
+        path: '/unauthorized',
+        name: 'unauthorized',
+        component: () => import('@/views/Unauhorized'),
+        meta: {
+          title: 'Unauhorized'
+        }
       }
     ]
   },

openbmc/webui-vue

Signed-off-by: Derick Montague <derick.montague@ibm.com>
Change-Id: Id3ea36ba812462be04a450f84f98d0237d6c7c3d

src/router/index.js
@@ -28,6 +28,14 @@ const routes = [
         meta: {
           title: 'Local user management'
         }
+      },
+      {
+        path: '/unauthorized',
+        name: 'unauthorized',
+        component: () => import('@/views/Unauthorized'),
+        meta: {
+          title: 'Unauthorized'
+        }
       }
     ]
   },