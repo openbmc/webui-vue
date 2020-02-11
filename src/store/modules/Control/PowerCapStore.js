2e4267cda113c3c67abcb7794303ff5693ea6ac0
Dixsie Wolmers <dixsie@ibm.com>
2020-02-10 21:58:55 -0600

    Translate overview page
    - Power cap and power consumption state is translated
      in the store
    - Static overview text is translated in the template
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I9463072e38c82b4660743ba9785a606ef1c69eae

--- src/store/modules/Control/PowerCapStore.js
+++ src/store/modules/Control/PowerCapStore.js
@@ -3,18 +3,14 @@ import api from '../../api';
 const PowerCapStore = {
   namespaced: true,
   state: {
-    powerCapData: null,
-    powerCapValue: 'Not enabled'
+    powerCapData: false
   },
   getters: {
-    powerCapData: state => state.powerCapData,
-    powerCapValue: state => state.powerCapValue
+    powerCapData: state => state.powerCapData
   },
   mutations: {
     setPowerCapData: (state, powerCapData) =>
-      (state.powerCapData = powerCapData),
-    setPowerCapValue: (state, powerCapValue) =>
-      (state.powerCapValue = powerCapValue)
+      (state.powerCapData = powerCapData)
   },
   actions: {
     getPowerCapData({ commit }) {
@@ -22,9 +18,7 @@ const PowerCapStore = {
         .get('/xyz/openbmc_project/control/host0/power_cap')
         .then(response => {
           const powerCapData = response.data.data;
-          if (powerCapData.PowerCapEnable) {
-            commit('setPowerCapValue', powerCapData.PowerCap + ' W');
-          }
+          commit('setPowerCapData', powerCapData);
         })
         .catch(error => {
           console.log('Power cap error', error);
