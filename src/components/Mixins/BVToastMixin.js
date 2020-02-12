90bb21cabeba102ac7a170996e7c666e6c7b7b17
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-02-12 11:30:49 -0800

    Add batch actions to local user table
    - Create TableToolbar component for table batch actions
    - Added Toast warning type and toast title message translations
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I455beba4f56b8209b1201bbc5ff3f616e960d189

--- src/components/Mixins/BVToastMixin.js
+++ src/components/Mixins/BVToastMixin.js
@@ -1,18 +1,29 @@
+import i18n from '../../i18n';
+
 const BVToastMixin = {
   methods: {
-    successToast(message) {
+    successToast(message, title = i18n.t('global.response.success')) {
       this.$root.$bvToast.toast(message, {
-        title: 'Success',
+        title,
         variant: 'success',
         autoHideDelay: 10000, //auto hide in milliseconds
         isStatus: true,
         solid: true
       });
     },
-    errorToast(message) {
+    errorToast(message, title = i18n.t('global.response.error')) {
       this.$root.$bvToast.toast(message, {
-        title: 'Error',
+        title,
         variant: 'danger',
+        noAutoHide: true,
+        isStatus: true,
+        solid: true
+      });
+    },
+    warningToast(message, title = i18n.t('global.response.warning')) {
+      this.$root.$bvToast.toast(message, {
+        title,
+        variant: 'warning',
         noAutoHide: true,
         isStatus: true,
         solid: true
--- src/store/modules/AccessControl/LocalUserMangementStore.js
+++ src/store/modules/AccessControl/LocalUserMangementStore.js
@@ -1,4 +1,21 @@
 import api from '../../api';
+import i18n from '../../../i18n';
+
+const getResponseCount = responses => {
+  let successCount = 0;
+  let errorCount = 0;
+
+  responses.forEach(response => {
+    if (response instanceof Error) errorCount++;
+    else successCount++;
+  });
+
+  return {
+    responseCount: responses.length,
+    successCount,
+    errorCount
+  };
+};
 
 const LocalUserManagementStore = {
   namespaced: true,
@@ -73,6 +90,165 @@ const LocalUserManagementStore = {
           console.log(error);
           throw new Error(`Error deleting user '${username}'.`);
         });
+    },
+    async deleteUsers({ dispatch }, users) {
+      const promises = users.map(({ username }) => {
+        return api
+          .delete(`/redfish/v1/AccountService/Accounts/${username}`)
+          .catch(error => {
+            console.log(error);
+            return error;
+          });
+      });
+      return await api
+        .all(promises)
+        .then(response => {
+          dispatch('getUsers');
+          return response;
+        })
+        .then(
+          api.spread((...responses) => {
+            const {
+              responseCount,
+              successCount,
+              errorCount
+            } = getResponseCount(responses);
+
+            if (responseCount === errorCount) {
+              // If all requests failed then throw error and show
+              // error message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.errorDeleteUsers'
+              );
+              throw new Error(message);
+            } else if (responseCount !== successCount) {
+              // If some requests failed show partial success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.warningDeleteUsers',
+                {
+                  errorCount: errorCount,
+                  successCount: successCount
+                }
+              );
+              return { type: 'warning', message };
+            } else {
+              // If all requests successful, show success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.succcessDeleteUsers',
+                { count: responseCount }
+              );
+              return { type: 'success', message };
+            }
+          })
+        );
+    },
+    async enableUsers({ dispatch }, users) {
+      const data = {
+        Enabled: true
+      };
+      const promises = users.map(({ username }) => {
+        return api
+          .patch(`/redfish/v1/AccountService/Accounts/${username}`, data)
+          .catch(error => {
+            console.log(error);
+            return error;
+          });
+      });
+      return await api
+        .all(promises)
+        .then(response => {
+          dispatch('getUsers');
+          return response;
+        })
+        .then(
+          api.spread((...responses) => {
+            const {
+              responseCount,
+              successCount,
+              errorCount
+            } = getResponseCount(responses);
+
+            if (responseCount === errorCount) {
+              // If all requests failed then throw error and show
+              // error message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.errorEnableUsers'
+              );
+              throw new Error(message);
+            } else if (responseCount !== successCount) {
+              // If some requests failed show partial success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.warningEnableUsers',
+                {
+                  errorCount: errorCount,
+                  successCount: successCount
+                }
+              );
+              return { type: 'warning', message };
+            } else {
+              // If all requests successful, show success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.succcessEnableUsers',
+                { count: responseCount }
+              );
+              return { type: 'success', message };
+            }
+          })
+        );
+    },
+    async disableUsers({ dispatch }, users) {
+      const data = {
+        Enabled: false
+      };
+      const promises = users.map(({ username }) => {
+        return api
+          .patch(`/redfish/v1/AccountService/Accounts/${username}`, data)
+          .catch(error => {
+            console.log(error);
+            return error;
+          });
+      });
+      return await api
+        .all(promises)
+        .then(response => {
+          dispatch('getUsers');
+          return response;
+        })
+        .then(
+          api.spread((...responses) => {
+            const {
+              responseCount,
+              successCount,
+              errorCount
+            } = getResponseCount(responses);
+
+            if (responseCount === errorCount) {
+              // If all requests failed then throw error and show
+              // error message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.errorDisableUsers'
+              );
+              throw new Error(message);
+            } else if (responseCount !== successCount) {
+              // If some requests failed show partial success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.warningDisableUsers',
+                {
+                  errorCount: errorCount,
+                  successCount: successCount
+                }
+              );
+              return { type: 'warning', message };
+            } else {
+              // If all requests successful, show success message
+              const message = i18n.t(
+                'localUserManagement.toastMessages.succcessDisableUsers',
+                { count: responseCount }
+              );
+              return { type: 'success', message };
+            }
+          })
+        );
     }
   }
 };
