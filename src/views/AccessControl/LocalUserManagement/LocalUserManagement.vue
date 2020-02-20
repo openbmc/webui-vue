9e6db2008a42bd5248de166cc1bdbd7439b32107
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-02-20 10:18:36 -0800

    Add account settings to local user page
    Adds ability to change account LockoutThreshold and
    LockoutDuration properties from the GUI.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Ieeb75aa83c07b3de840bccdfc28e2d6e87512e2e

--- src/views/AccessControl/LocalUserManagement/LocalUserManagement.vue
+++ src/views/AccessControl/LocalUserManagement/LocalUserManagement.vue
@@ -81,7 +81,7 @@
       </b-col>
     </b-row>
     <!-- Modals -->
-    <modal-settings :settings="settings" />
+    <modal-settings :settings="settings" @ok="saveAccountSettings" />
     <modal-user
       :user="activeUser"
       :password-requirements="passwordRequirements"
@@ -217,11 +217,7 @@ export default {
         });
     },
     initModalSettings() {
-      if (this.settings) {
-        this.$bvModal.show('modal-settings');
-      } else {
-        // fetch settings then show modal
-      }
+      this.$bvModal.show('modal-settings');
     },
     saveUser({ isNewUser, userData }) {
       if (isNewUser) {
@@ -277,6 +273,12 @@ export default {
         default:
           break;
       }
+    },
+    saveAccountSettings(settings) {
+      this.$store
+        .dispatch('localUsers/saveAccountSettings', settings)
+        .then(message => this.successToast(message))
+        .catch(({ message }) => this.errorToast(message));
     }
   }
 };
