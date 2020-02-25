6dba1443617224949b227420ebd3bebbe5fa3006
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-02-25 15:54:07 -0800

    Add power operations page
    Add route, component and Control requests to enable
    power operations.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I54784b8cc1b6260e44e708c260ea4a531fc0a629

--- src/store/modules/Control/ControlStore.js
+++ src/store/modules/Control/ControlStore.js
@@ -3,6 +3,16 @@ import i18n from '../../../i18n';
 
 const ControlStore = {
   namespaced: true,
+  state: {
+    isOperationInProgress: false
+  },
+  getters: {
+    isOperationInProgress: state => state.isOperationInProgress
+  },
+  mutations: {
+    setOperationInProgress: (state, inProgress) =>
+      (state.isOperationInProgress = inProgress)
+  },
   actions: {
     async rebootBmc() {
       const data = { ResetType: 'GracefulRestart' };
@@ -13,6 +23,68 @@ const ControlStore = {
           console.log(error);
           throw new Error(i18n.t('pageRebootBmc.toast.errorRebootStart'));
         });
+    },
+    async hostPowerOn({ dispatch, commit }) {
+      const data = { ResetType: 'On' };
+      dispatch('hostPowerChange', data);
+      await dispatch('checkForHostStatus', 'on');
+      commit('setOperationInProgress', false);
+    },
+    async hostSoftReboot({ dispatch, commit }) {
+      const data = { ResetType: 'GracefulRestart' };
+      dispatch('hostPowerChange', data);
+      await dispatch('checkForHostStatus', 'on');
+      commit('setOperationInProgress', false);
+    },
+    async hostHardReboot({ dispatch, commit }) {
+      // TODO: Update when ForceWarmReboot property
+      // available
+      dispatch('hostHardPowerOff');
+      await dispatch('checkForHostStatus', 'off');
+      dispatch('hostPowerOn');
+      await dispatch('checkForHostStatus', 'on');
+      commit('setOperationInProgress', false);
+    },
+    async hostSoftPowerOff({ dispatch, commit }) {
+      const data = { ResetType: 'GracefulShutdown' };
+      dispatch('hostPowerChange', data);
+      await dispatch('checkForHostStatus', 'off');
+      commit('setOperationInProgress', false);
+    },
+    async hostHardPowerOff({ dispatch, commit }) {
+      const data = { ResetType: 'ForceOff' };
+      dispatch('hostPowerChange', data);
+      await dispatch('checkForHostStatus', 'off');
+      commit('setOperationInProgress', false);
+    },
+    hostPowerChange({ commit }, data) {
+      commit('setOperationInProgress', true);
+      api
+        .post('/redfish/v1/Systems/system/Actions/ComputerSystem.Reset', data)
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /**
+     * Watch for hostStatus changes in GlobalStore module
+     * to set isOperationInProgress state
+     * Stop watching status changes and resolve Promise when
+     * hostStatus value matches passed argument
+     * @param {string} hostStatus
+     * @returns {Promise}
+     */
+    checkForHostStatus(_, hostStatus) {
+      return new Promise(resolve => {
+        const unwatch = this.watch(
+          (_, getters) => getters['global/hostStatus'],
+          value => {
+            if (value === hostStatus) {
+              resolve();
+              unwatch();
+            }
+          }
+        );
+      });
     }
   }
 };
