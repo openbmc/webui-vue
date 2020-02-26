4a38de8c941bb7b82518b7423c44808fc385d399
Dixsie Wolmers <dixsie@ibm.com>
2020-02-26 11:23:52 -0600

    Move power control to redfish
    Replaces power cap store and power consumption store with power control store
    and uses redfish.
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I5cce223da17373bcae3e7c6736a4580e1bd8ae00

---
+++ src/store/modules/Control/PowerControlStore.js
@@ -0,0 +1,39 @@
+import api from '../../api';
+
+const PowerControlStore = {
+  namespaced: true,
+  state: {
+    powerCapValue: null,
+    powerConsumptionValue: null
+  },
+  getters: {
+    powerCapValue: state => state.powerCapValue,
+    powerConsumptionValue: state => state.powerConsumptionValue
+  },
+  mutations: {
+    setPowerCapValue: (state, powerCapValue) =>
+      (state.powerCapValue = powerCapValue),
+    setPowerConsumptionValue: (state, powerConsumptionValue) =>
+      (state.powerConsumptionValue = powerConsumptionValue)
+  },
+  actions: {
+    getPowerControl({ commit }) {
+      api
+        .get('/redfish/v1/Chassis/chassis/Power')
+        .then(response => {
+          const powerControl = response.data.PowerControl;
+          const powerCap = powerControl[0].PowerLimit.LimitInWatts;
+          // If system is powered off, power consumption is does not exist in the PowerControl
+          const powerConsumption = powerControl[0].PowerConsumedWatts || null;
+
+          commit('setPowerCapValue', powerCap);
+          commit('setPowerConsumptionValue', powerConsumption);
+        })
+        .catch(error => {
+          console.log('Power control', error);
+        });
+    }
+  }
+};
+
+export default PowerControlStore;
