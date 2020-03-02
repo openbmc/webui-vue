e6f0235c4580c02a89596a6669ccce3e6c4d9f67
Derick Montague <derick.montague@ibm.com>
2020-03-02 17:56:09 -0600

    Remove unused colors from color palette
    The color palette has been stripped down to a
    maximum of two colors shades per palette. This
    works for our design since components use a
    base color with a lighter color as an accent
    color. This change reduces the amount of CSS
    generated by Bootstrap when the CSS is compiled.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I2ddb37f5c89c749a7303799c6f7499ddd83d5a92

---
+++ docs/.vuepress/components/colors/all.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="color-tile-container">
+      <div v-for="item in baseColors">
+        <div
+          :style="{ backgroundColor: item.hex }"
+          :class="{ 'color-tile--border': item.border }"
+          class="color-tile"
+        ></div>
+        <dl class="color-tile-desc">
+          <dt>Variable:</dt>
+          <dd>${{ item.name }}</dd>
+        </dl>
+        <dl class="color-tile-desc">
+          <dt>Color Variable:</dt>
+          <dd>{{ item.variable }}</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      baseColors: [
+      {
+          name: 'blue',
+          variable: '$blue-500',
+          hex: '#2d60e5'
+        },
+        {
+          name: 'green',
+          variable: '$green-500',
+          hex: '#0a7d06'
+        },
+        {
+          name: 'red',
+          variable: '$red-500',
+          hex: '#da1416'
+        },
+        {
+          name: 'yellow',
+          variable: '$yellow-500',
+          hex: '#fedf39'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.color-tile-container {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  margin: 1rem 0;
+}
+
+.color-tile {
+  display: block;
+  width: 220px;
+  height: 140px;
+
+  &--border {
+    border: 1px dashed #ccc;
+  }
+}
+
+.color-tile-desc {
+  margin: 0.5rem 0;
+}
+
+.color-tile-desc dt {
+  display: inline;
+  font-weight: bold;
+}
+
+.color-tile-desc dd {
+  display: inline;
+  margin: 0;
+}
+</style>
---
+++ docs/.vuepress/components/colors/blues.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="color-tile-container">
+      <div v-for="color in colors">
+        <div
+          :style="{ backgroundColor: color.hex }"
+          :class="{ 'color-tile--border': color.border }"
+          class="color-tile"
+        ></div>
+        <dl class="color-tile-desc">
+          <dt>Color variable:</dt>
+          <dd>${{ color.variable }}</dd>
+        </dl>
+        <dl class="color-tile-desc">
+          <dt>Hex:</dt>
+          <dd>{{ color.hex }}</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: [
+        {
+          variable: 'blue-100',
+          hex: '#eff2fb',
+        },
+        {
+          variable: 'blue-500',
+          hex: '#2d60e5'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.color-tile-container {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  margin: 1rem 0;
+}
+
+.color-tile {
+  display: block;
+  width: 220px;
+  height: 140px;
+
+  &--border {
+    border: 1px dashed #ccc;
+  }
+}
+
+.color-tile-desc {
+  margin: 0.5rem 0;
+}
+
+.color-tile-desc dt {
+  display: inline;
+  font-weight: bold;
+}
+
+.color-tile-desc dd {
+  display: inline;
+  margin: 0;
+}
+</style>
