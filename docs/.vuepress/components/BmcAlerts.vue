540d0bff1a453cdf2ea1b7b7c21e55782d2af754
Derick Montague <derick.montague@ibm.com>
2020-03-03 06:11:48 -0600

    Add alert message documentation
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I5c0b696ca47ddba0df18041d6c5ee7509bf23572

---
+++ docs/.vuepress/components/BmcAlerts.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-alert show variant="success">This is a success alert message.</b-alert>
+        <b-alert show variant="warning">This is a warning alert message.</b-alert>
+        <b-alert show variant="danger">
+           <strong class="alert-title">Invalid username or password</strong>
+           <span>Verify your username and password and try again.</span>
+        </b-alert>
+        <b-alert show variant="info">This is an info alert message.</b-alert>
+    </div>
+</template>
+
+<script>
+import "../../../src/assets/styles/_obmc-custom.scss";
+import { BAlert } from "bootstrap-vue";
+
+export default {
+    name: 'BmcAlerts',
+    components: { BAlert }
+}
+</script>
