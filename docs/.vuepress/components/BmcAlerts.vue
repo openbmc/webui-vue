e881bb7a01549ed281a70e262a4dc23906ffc9e1
Derick Montague <derick.montague@ibm.com>
2020-03-03 06:11:48 -0600

    Add alert message component and documentation
    - Add custom alert component to simplify the use of custom alerts
    - Add documentation for using the custom alert
    - Update the login error alert to use the alert component instead of
    the Bootstrap-vue component.
    - Add the enhanceApp and bmcAppPlugin to extend vuepress to use both
    the BMC custom and Boostrap-Vue components along with the custom styles
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I5c0b696ca47ddba0df18041d6c5ee7509bf23572

---
+++ docs/.vuepress/components/BmcAlerts.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <alert show="true" variant="warning">This is a warning message</alert>
+        <alert show="true" variant="danger">This is an error message</alert>
+        <alert show="true" variant="info">This is an info message</alert>
+        <alert show="true" title="Error" variant="danger">This is an error message with a titile. An edge case used on the login page.</alert>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BmcAlerts',
+}
+</script>
