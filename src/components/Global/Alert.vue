7a885b98d7c8efb0fb6c2c9d1d9a201ca8d73e6b
Derick Montague <derick.montague@ibm.com>
2020-03-03 06:11:48 -0600

    Add alert message documentation
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I5c0b696ca47ddba0df18041d6c5ee7509bf23572

---
+++ src/components/Global/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-alert :show="show" :variant="variant">
+    <div v-if="variant == 'warning' || variant == 'danger'" class="alert-icon">
+      <icon-warning v-if="variant === 'warning'" />
+      <icon-danger v-else-if="variant === 'danger'" />
+    </div>
+    <div class="alert-content">
+      <strong v-if="title" class="alert-title">{{ title }}</strong>
+      <div class="alert-msg"><slot /></div>
+    </div>
+  </b-alert>
+</template>
+
+<script>
+import IconWarning from '@carbon/icons-vue/es/warning--filled/20';
+import IconError from '@carbon/icons-vue/es/error--filled/20';
+import { BAlert } from 'bootstrap-vue';
+
+export default {
+  name: 'Alert',
+  components: {
+    BAlert: BAlert,
+    iconDanger: IconError,
+    iconWarning: IconWarning
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    variant: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
