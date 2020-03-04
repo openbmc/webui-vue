eb5e532e3c566851fadda9a5091cb9ed26c3d4db
Derick Montague <derick.montague@ibm.com>
2020-03-03 18:11:44 -0600

    Add toast component documentation
    - Add documentation that describes when and how to use a toast message
    with code snippets
    - Add an informational toast method in the BVToastMixin
    - Add BVToastMixin to enhaceApp to register mixin globally.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I89bf2aa8b3fdb9294354a80c98ccf692b6e3615a

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,19 @@
 
+// OpenBMC Imports
 import "../../src/assets/styles/_obmc-custom.scss";
+import BVToastMixin from "../../src/components/Mixins/BVToastMixin";
+
+// Bootstrap-vue Plugin imports
 import {
-    ButtonPlugin
+    ButtonPlugin,
+    ToastPlugin
   } from 'bootstrap-vue';
 
 
 export default ({ Vue }) => {
       Vue.use(ButtonPlugin);
+      Vue.use(ToastPlugin);
+
+      // BMC Components and Mixins
+      Vue.mixin('BVToastMixin', BVToastMixin);
 }
