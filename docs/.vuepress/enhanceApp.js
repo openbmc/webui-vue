f8b9e862166c6a400087610f3fdf888ee2bcc97c
Derick Montague <derick.montague@ibm.com>
2020-03-04 00:59:47 -0600

    Add buttons documentation
    - Add documentation for how to use buttons
    - Update buttons to match design and documentation and move the icons
    to the left of the text
    - Update markup and CSS rulesets to support icons on the lefr or the
    right of text
    - Updated alignment for icons within buttons.
    - Remove left and right spacing for btn-link buttons for alignment
    since they do not have borders or background color. This helps with
    alignment. Otherwise the btn-link elements appear to have a left margin.
    - Add margin between buttons on local user management page to accomodate
    for remove of btn-link padding.
    
    Testing
    Reviewed all the buttons in use across the application to verify that
    the changes did not have a negative impact on any of the button
    implementations.
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Ic897f416e85824287360bc7ef5dc47c402d64eba

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,12 @@
 import {
     AlertPlugin,
+    ButtonPlugin
   } from 'bootstrap-vue';
 
 import BmcAppPlugin from './bmcAppPlugin';
 
 export default ({ Vue }) => {
       Vue.use(AlertPlugin);
+      Vue.use(ButtonPlugin);
       Vue.use(BmcAppPlugin);
 }
