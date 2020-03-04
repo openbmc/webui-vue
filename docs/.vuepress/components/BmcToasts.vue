05420871fce8926c618085e94bfc19f974759f64
Derick Montague <derick.montague@ibm.com>
2020-03-03 18:11:44 -0600

    Add toast component documentation
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I89bf2aa8b3fdb9294354a80c98ccf692b6e3615a

---
+++ docs/.vuepress/components/BmcToasts.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <b-button variant="success" @click="makeToast('success')">Show Success</b-button>
+        <b-button variant="danger" @click="makeToast('danger')">Show Error</b-button>
+        <b-button variant="info" @click="makeToast('info')">Show Info</b-button>
+        <b-button variant="warning" @click="makeToast('warning')">Show Warning</b-button>
+    </div>
+</template>
+
+<script>
+import "../../../src/assets/styles/_obmc-custom.scss";
+// import Vue from 'vue';
+// import {ToastPlugin} from "bootstrap-vue";
+import { BButton, BToast, BToaster } from "bootstrap-vue";
+// Vue.use(ToastPlugin);
+export default {
+    name: 'BmcToasts',
+    components: {
+        BButton,
+        BToaster,
+        BToaster
+    },
+    methods: {
+        makeToast(variant = null) {
+            let title = variant == 'danger' ? 'error' : variant;
+            this.$bvToast.toast(`This is a ${title} toast message`, {
+                title: `${title.charAt(0).toUpperCase()}${title.slice(1)}`,
+                variant: variant
+            });
+        }
+    }
+}
+</script>
+<style scoped>
+    button {
+        margin-right: .5rem;
+        margin-bottom: 1rem;
+    }
+</style>
