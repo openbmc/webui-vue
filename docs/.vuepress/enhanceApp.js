cf3226659152e58c875d681ef48a84d5ccb3cd25
Derick Montague <derick.montague@ibm.com>
2020-03-13 15:15:43 -0500

    Add alert message component and documentation
    - Add custom alert component to simplify the use of custom alerts
    - Add documentation for using the custom alert
    - Update the login error alert to use the alert component instead of
    the Bootstrap-vue component.
    - Register alert component in enhanceApp
    - Replace Sass variables used in the StatusIcon component style block
    to use the Boostrap theme-color and gray Sass functions so the colors
    can be used in the Vuepress documentation custom components
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: Ibd93402c919a42bd5c24cc9e7c6c8fc6f17a4db4

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,18 @@
-
+// OpenBMC Imports
 import "../../src/assets/styles/_obmc-custom.scss";
+import Alert from "../../src/components/Global/Alert";
+
+// Bootstrap-vue Plugin imports
 import {
+    AlertPlugin,
     ButtonPlugin
   } from 'bootstrap-vue';
 
 
 export default ({ Vue }) => {
+      Vue.use(AlertPlugin);
       Vue.use(ButtonPlugin);
+
+      // BMC Components and Mixins
+      Vue.component('Alert', Alert);
 }
