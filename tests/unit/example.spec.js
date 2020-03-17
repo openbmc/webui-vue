c9bd91d96f658505db4be1d7d818a54300f0269e
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-03-17 12:01:36 -0700

    Add example test spec with mocha and chai
    - Added setup and jsdom-global package to resolve
      localStorage reference error in i18n.js file
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Ife835ef5725a1810f10943c75d43958cbe4aca35

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,16 @@
+import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import AppHeader from '@/components/AppHeader';
+import $store from '@/store';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('AppHeader.vue', () => {
+  it('renders AppHeader component', () => {
+    const wrapper = shallowMount(AppHeader, {
+      mocks: {
+        $t: key => key,
+        $store
+      }
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.exists());
   });
 });
