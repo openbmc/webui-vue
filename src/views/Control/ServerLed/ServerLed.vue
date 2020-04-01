7c2c1a5543ff8c98e3726764789c153016e2319d
SurenNeware <sneware9@in.ibm.com>
2020-04-01 14:07:27 +0530

    Add ServerLED page
    Signed-off-by: SurenNeware <sneware9@in.ibm.com>
    Change-Id: Ia59eb0214530906dea840ff18ff22fc913870bb9

---
+++ src/views/Control/ServerLed/ServerLed.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-fluid px-0">
+    <page-title />
+    <page-section :section-title="$t('pageServerLed.serverLedTitle')" />
+    <b-row>
+      <b-col md="12">
+        <page-section>
+          <h6 class="font-weight-bold">
+            {{ $t('pageServerLed.serverLedSubTitle') }}
+          </h6>
+          <p class="mb-3">{{ $t('pageServerLed.serverLedInformation') }}</p>
+          <b-form-checkbox v-model="serverLedLight" name="check-button" switch>
+            <span v-if="serverLedLight">{{ $t('global.status.on') }}</span>
+            <span v-else>{{ $t('global.status.off') }}</span>
+          </b-form-checkbox>
+        </page-section>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import PageTitle from '../../../components/Global/PageTitle';
+import PageSection from '../../../components/Global/PageSection';
+
+export default {
+  name: 'ServerLed',
+  components: { PageTitle, PageSection },
+  data() {
+    return {
+      serverLedLight: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
