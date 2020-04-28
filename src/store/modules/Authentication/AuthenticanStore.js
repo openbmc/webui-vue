ef9b9e498fcf6a9c2e74c6667a14e9b0dff1bc10
Sukanya Pandey <sukapan1@in.ibm.com>
2020-04-28 20:18:28 +0530

    Profile settings page
    -To set the profile by setting password.
    -API called to save changed
    password is '/redfish/v1/AccountService/Accounts/'
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: Ie54a54beff8c85bc9ac5af21c35edc481b34cf44

--- src/store/modules/Authentication/AuthenticanStore.js
+++ src/store/modules/Authentication/AuthenticanStore.js
@@ -1,19 +1,23 @@
 import api from '../../api';
 import Cookies from 'js-cookie';
 import router from '../../../router';
+import i18n from '@/i18n';
 
 const AuthenticationStore = {
   namespaced: true,
   state: {
     authError: false,
-    cookie: Cookies.get('XSRF-TOKEN')
+    cookie: Cookies.get('XSRF-TOKEN'),
+    userName: ''
   },
   getters: {
     authError: state => state.authError,
-    isLoggedIn: state => !!state.cookie
+    isLoggedIn: state => !!state.cookie,
+    getUsername: state => state.userName
   },
   mutations: {
     authSuccess(state) {
+      console.log('state authsuccess', state);
       state.authError = false;
       state.cookie = Cookies.get('XSRF-TOKEN');
     },
@@ -22,14 +26,19 @@ const AuthenticationStore = {
     },
     logout() {
       Cookies.remove('XSRF-TOKEN');
+    },
+    updateUserName(state, userName) {
+      console.log('username in commit', userName);
+      state.userName = userName;
     }
   },
   actions: {
     login({ commit }, auth) {
+      console.log('login', auth);
       commit('authError', false);
       return api
         .post('/login', { data: auth })
-        .then(() => commit('authSuccess'))
+        .then(() => commit('authSuccess'), commit('updateUserName', auth[0]))
         .catch(error => {
           commit('authError');
           throw new Error(error);
@@ -41,6 +50,19 @@ const AuthenticationStore = {
         .then(() => commit('logout'))
         .then(() => router.go('/login'))
         .catch(error => console.log(error));
+    },
+    async setPassword(state, password) {
+      let { userName } = state.state;
+      let data = { Password: password };
+      return await api
+        .patch(`/redfish/v1/AccountService/Accounts/${userName}`, data)
+        .then(() => i18n.t('pageLocalUserManagement.toast.successSaveSettings'))
+        .catch(error => {
+          console.log(error);
+          throw new Error(
+            i18n.t('pageLocalUserManagement.toast.errorSaveSettings')
+          );
+        });
     }
   }
 };
