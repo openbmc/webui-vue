255dea1d2c93f19cb0d97b91ddc6a4fd5babdc13
Sukanya Pandey <sukapan1@in.ibm.com>
2020-05-20 15:32:57 +0530

    Add code for Serial Over LAN
    - The output of serial connection of the hosts on the workstation
    terminal.
    - The library used is xterm which will provide the terminal to show the
    data.
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I6000cae42f237fffe216e2079cf2a6c39db236fd

---
+++ src/views/Control/SerialOverLan/SerialOverLan.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-container fluid="xl">
+    <page-title />
+
+    <page-section :section-title="$t('pageSerialoverLAN.subTitle')">
+      <p>{{ $t('pageSerialoverLAN.subTitleDesc') }}</p>
+
+      <serial-over-lan-console />
+
+      <div class="text-right">
+        <b-button
+          variant="link"
+          type="button"
+          class="button-launch"
+          @click="openConsoleWindow()"
+        >
+          <launch />
+
+          {{ $t('pageSerialoverLAN.openNewTab') }}
+        </b-button>
+      </div>
+    </page-section>
+  </b-container>
+</template>
+
+<script>
+import Launch from '@carbon/icons-vue/es/launch/32';
+import PageTitle from '@/components/Global/PageTitle';
+import PageSection from '@/components/Global/PageSection';
+import SerialOverLanConsole from './SerialOverLanConsole';
+
+export default {
+  name: 'SerialOverLan',
+  components: { Launch, PageSection, PageTitle, SerialOverLanConsole },
+  mounted() {
+    this.openTerminal();
+  },
+  methods: {
+    openConsoleWindow() {
+      window.open(
+        '#/console/serial-over-lan',
+        '_blank',
+        'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=550'
+      );
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button-launch > svg {
+  height: 25px;
+}
+</style>
