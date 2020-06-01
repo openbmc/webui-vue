9e7bdedd003595930ab5c9faae3cb7d11adbed94
SurenNeware <sneware9@in.ibm.com>
2020-06-01 15:31:00 +0530

    Global Search component
    Added global Search component to Sensors page.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: I3121cc4e582c782fa7e9937738cefbeddc23610e

---
+++ src/components/Global/Search.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="search-global">
+    <b-form-group
+      label="Search"
+      label-cols-sm="12"
+      label-align-sm="left"
+      label-size="sm"
+      label-for="searchInput"
+      label-sr-only
+      class="mb-3"
+    >
+      <b-input-group size="sm" class="align-items-center">
+        <b-input-group-prepend>
+          <icon-search class="search-icon" />
+        </b-input-group-prepend>
+        <b-form-input
+          id="searchInput"
+          v-model="filter"
+          type="search"
+          placeholder="Search for sensors"
+          @input="onChangeInput"
+        ></b-form-input>
+      </b-input-group>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+import IconSearch from '@carbon/icons-vue/es/search/16';
+export default {
+  name: 'Search',
+  components: { IconSearch },
+  data() {
+    return {
+      filter: null
+    };
+  },
+  methods: {
+    onChangeInput() {
+      this.$emit('changeSearch', this.filter);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#searchInput:not(:focus) {
+  padding-left: 36px;
+}
+.search-icon {
+  position: absolute;
+  left: 10px;
+  top: 8px;
+  z-index: 1;
+  stroke: #c3c2c2;
+}
+</style>
