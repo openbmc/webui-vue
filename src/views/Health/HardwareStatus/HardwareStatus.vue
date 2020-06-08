0962399c60d309d678750edcca5e88ebe796409a
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-06-08 07:36:59 -0700

    Add Chassis table to hardware status page
    Add each chassis at /redfish/v1/Chassis endpoint to a table
    with an expansion row to view property details.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I8d4c64fecac3857e0d4ece9fad81d9035e236c92

--- src/views/Health/HardwareStatus/HardwareStatus.vue
+++ src/views/Health/HardwareStatus/HardwareStatus.vue
@@ -4,25 +4,34 @@
 
     <!-- System table -->
     <table-system />
+
+    <!-- Chassis table -->
+    <table-chassis />
   </b-container>
 </template>
 
 <script>
 import PageTitle from '@/components/Global/PageTitle';
 import TableSystem from './HardwareStatusTableStystem';
+import TableChassis from './HardwareStatusTableChassis';
 import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
 
 export default {
-  components: { PageTitle, TableSystem },
+  components: { PageTitle, TableSystem, TableChassis },
   mixins: [LoadingBarMixin],
   created() {
     this.startLoader();
     const systemTablePromise = new Promise(resolve => {
       this.$root.$on('hardwareStatus::system::complete', () => resolve());
     });
+    const chassisTablePromise = new Promise(resolve => {
+      this.$root.$on('hardwareStatus::chassis::complete', () => resolve());
+    });
     // Combine all child component Promises to indicate
     // when page data load complete
-    Promise.all([systemTablePromise]).finally(() => this.endLoader());
+    Promise.all([systemTablePromise, chassisTablePromise]).finally(() =>
+      this.endLoader()
+    );
   },
   beforeRouteLeave(to, from, next) {
     // Hide loader if user navigates away from page
