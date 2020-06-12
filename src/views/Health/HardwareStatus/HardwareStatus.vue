cdc80a46a680b0033e543ce523daf487aeec5fce
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-06-12 08:29:42 -0700

    Add BMC manager table to hardware status page
    Add properties at /redfish/v1/Managers/bmc endpoint in a
    table with expandable row to view details.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Ieb32a9b2a535ddd7d24edcb68761c51eace2e5a8

--- src/views/Health/HardwareStatus/HardwareStatus.vue
+++ src/views/Health/HardwareStatus/HardwareStatus.vue
@@ -5,6 +5,9 @@
     <!-- System table -->
     <table-system />
 
+    <!-- BMC manager table -->
+    <table-bmc-manager />
+
     <!-- Chassis table -->
     <table-chassis />
   </b-container>
@@ -13,25 +16,36 @@
 <script>
 import PageTitle from '@/components/Global/PageTitle';
 import TableSystem from './HardwareStatusTableStystem';
+import TableBmcManager from './HardwareStatusTableBmcManager';
 import TableChassis from './HardwareStatusTableChassis';
 import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
 
 export default {
-  components: { PageTitle, TableSystem, TableChassis },
+  components: {
+    PageTitle,
+    TableSystem,
+    TableBmcManager,
+    TableChassis
+  },
   mixins: [LoadingBarMixin],
   created() {
     this.startLoader();
     const systemTablePromise = new Promise(resolve => {
       this.$root.$on('hardwareStatus::system::complete', () => resolve());
     });
+    const bmcManagerTablePromise = new Promise(resolve => {
+      this.$root.$on('hardwareStatus::bmcManager::complete', () => resolve());
+    });
     const chassisTablePromise = new Promise(resolve => {
       this.$root.$on('hardwareStatus::chassis::complete', () => resolve());
     });
     // Combine all child component Promises to indicate
     // when page data load complete
-    Promise.all([systemTablePromise, chassisTablePromise]).finally(() =>
-      this.endLoader()
-    );
+    Promise.all([
+      systemTablePromise,
+      bmcManagerTablePromise,
+      chassisTablePromise
+    ]).finally(() => this.endLoader());
   },
   beforeRouteLeave(to, from, next) {
     // Hide loader if user navigates away from page
