d4caa23766b4d21e65bcd1e15f7a4a2d9cb2ddb0
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-06-12 07:10:28 -0700

    Add Motherboard table to hardware status page
    Adds properites at /redfish/v1/Chassis/motherboard endpoint
    in a table with expandable row to view details.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I61c3434b53e12c27bf35afd94a760513578cfaef

--- src/views/Health/HardwareStatus/HardwareStatus.vue
+++ src/views/Health/HardwareStatus/HardwareStatus.vue
@@ -7,6 +7,9 @@
 
     <!-- Chassis table -->
     <table-chassis />
+
+    <!-- Chassis table -->
+    <table-motherboard />
   </b-container>
 </template>
 
@@ -14,10 +17,11 @@
 import PageTitle from '@/components/Global/PageTitle';
 import TableSystem from './HardwareStatusTableStystem';
 import TableChassis from './HardwareStatusTableChassis';
+import TableMotherboard from './HardwareStatusTableMotherboard';
 import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
 
 export default {
-  components: { PageTitle, TableSystem, TableChassis },
+  components: { PageTitle, TableSystem, TableChassis, TableMotherboard },
   mixins: [LoadingBarMixin],
   created() {
     this.startLoader();
@@ -27,11 +31,16 @@ export default {
     const chassisTablePromise = new Promise(resolve => {
       this.$root.$on('hardwareStatus::chassis::complete', () => resolve());
     });
+    const motherboardTablePromise = new Promise(resolve => {
+      this.$root.$on('hardwareStatus::motherboard::complete', () => resolve());
+    });
     // Combine all child component Promises to indicate
     // when page data load complete
-    Promise.all([systemTablePromise, chassisTablePromise]).finally(() =>
-      this.endLoader()
-    );
+    Promise.all([
+      systemTablePromise,
+      chassisTablePromise,
+      motherboardTablePromise
+    ]).finally(() => this.endLoader());
   },
   beforeRouteLeave(to, from, next) {
     // Hide loader if user navigates away from page
