202c599e71cb6691e12fa516b579b2b0007b94f1
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-06-18 12:02:57 -0700

    Update Sensors page to use table mixins
    - Add number formatting to TableDataFormatter mixin
    - Use TableDataFormatter mixin to show different status icons for sensor
      status (OK, Warning, Critical)
    - Use TableSortMixin to sort by status (OK, Warning, Critical)
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I99899bbc19db3529f0fdfe34be30b09d41456b42

--- src/components/Mixins/TableDataFormatterMixin.js
+++ src/components/Mixins/TableDataFormatterMixin.js
@@ -3,6 +3,8 @@ const TableDataFormatterMixin = {
     tableFormatter(value) {
       if (value === undefined || value === null || value === '') {
         return '--';
+      } else if (typeof value === 'number') {
+        return parseFloat(value.toFixed(3));
       } else {
         return value;
       }
