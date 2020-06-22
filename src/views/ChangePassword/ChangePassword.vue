096788edf815c0b05712e5ddf4454f87331128fb
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-06-22 13:28:09 -0700

    Add check if password change required at Login
    After successfully authenticating on the Login page, check
    /redfish/v1/AccountService/Accounts/${username} endpoint for
    the PasswordChangeRequired property to see whether or not the
    password is expired. If the password is expired, then navigate
    to the Change password page, if the password isn't expired
    navigate to the Overview page.
    After successfully changing an expired password, navigate to the
    Overview page.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I32de5f71bcfcbe4099c2953a31c05ba0ebe670bc

--- src/views/ChangePassword/ChangePassword.vue
+++ src/views/ChangePassword/ChangePassword.vue
@@ -19,7 +19,7 @@
             autofocus="autofocus"
             type="password"
             :state="getValidationState($v.form.password)"
-            @blur="$v.form.password.$touch()"
+            @change="$v.form.password.$touch()"
           >
           </b-form-input>
           <b-form-invalid-feedback role="alert">
@@ -39,7 +39,7 @@
             v-model="form.passwordConfirm"
             type="password"
             :state="getValidationState($v.form.passwordConfirm)"
-            @blur="$v.form.passwordConfirm.$touch()"
+            @change="$v.form.passwordConfirm.$touch()"
           >
           </b-form-input>
           <b-form-invalid-feedback role="alert">
@@ -53,7 +53,7 @@
         </input-password-toggle>
       </b-form-group>
       <div class="text-right">
-        <b-button type="button" variant="link" to="login" @click="goBack">
+        <b-button type="button" variant="link" @click="goBack">
           {{ $t('pageChangePassword.goBack') }}
         </b-button>
         <b-button type="submit" variant="primary">
@@ -69,11 +69,12 @@ import { required, sameAs } from 'vuelidate/lib/validators';
 import Alert from '@/components/Global/Alert';
 import VuelidateMixin from '@/components/Mixins/VuelidateMixin';
 import InputPasswordToggle from '@/components/Global/InputPasswordToggle';
+import BVToastMixin from '@/components/Mixins/BVToastMixin';
 
 export default {
   name: 'ChangePassword',
   components: { Alert, InputPasswordToggle },
-  mixins: [VuelidateMixin],
+  mixins: [VuelidateMixin, BVToastMixin],
   data() {
     return {
       form: {
@@ -96,13 +97,20 @@ export default {
   },
   methods: {
     goBack() {
-      // Remove temporary session created if navigating back
-      // to the Login page
-      this.$store.commit('authentication/logout');
+      // Remove session created if navigating back to the Login page
+      this.$store.dispatch('authentication/logout');
     },
     changePassword() {
-      // Should make PATCH request with new password
-      // then reroute to Overview page
+      this.$v.$touch();
+      if (this.$v.$invalid) return;
+      let data = {
+        originalUsername: this.username,
+        password: this.form.password
+      };
+
+      this.$store
+        .dispatch('localUsers/updateUser', data)
+        .then(() => this.$router.push('/'));
     }
   }
 };
