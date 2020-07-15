6860094bb001e2e3d0e6fd2307ad052062eeaaab
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-07-15 10:30:31 -0700

    Add Firmware page
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I04450e5a170d374122908c4c0349ba3b6e93ed2c

--- src/store/modules/Configuration/FirmwareStore.js
+++ src/store/modules/Configuration/FirmwareStore.js
@@ -3,26 +3,70 @@ import api from '../../api';
 const FirmwareStore = {
   namespaced: true,
   state: {
-    bmcFirmwareVersion: '--'
+    systemFirmwareVersion: '--',
+    applyTime: null
   },
   getters: {
-    bmcFirmwareVersion: state => state.bmcFirmwareVersion
+    systemFirmwareVersion: state => state.systemFirmwareVersion
   },
   mutations: {
-    setBmcFirmwareVersion: (state, bmcFirmwareVersion) =>
-      (state.bmcFirmwareVersion = bmcFirmwareVersion)
+    setsystemFirmwareVersion: (state, systemFirmwareVersion) =>
+      (state.systemFirmwareVersion = systemFirmwareVersion),
+    setApplyTime: (state, applyTime) => (state.applyTime = applyTime)
   },
   actions: {
-    async getBmcFirmware({ commit }) {
+    async getSystemFirwareVersion({ commit }) {
       return await api
         .get('/redfish/v1/Managers/bmc')
-        .then(response => {
-          const bmcFirmwareVersion = response.data.FirmwareVersion;
-          commit('setBmcFirmwareVersion', bmcFirmwareVersion);
+        .then(({ data: { Links: { ActiveSoftwareImage } } }) => {
+          const location = ActiveSoftwareImage['@odata.id'];
+          return api.get(location);
         })
-        .catch(error => {
-          console.log(error);
-        });
+        .then(({ data: { Version } }) => {
+          commit('setsystemFirmwareVersion', Version);
+        })
+        .catch(error => console.log(error));
+    },
+    getUpdateServiceApplyTime({ commit }) {
+      api
+        .get('/redfish/v1/UpdateService')
+        .then(({ data }) => {
+          const applyTime =
+            data.HttpPushUriOptions.HttpPushUriApplyTime.ApplyTime;
+          commit('setApplyTime', applyTime);
+        })
+        .catch(error => console.log(error));
+    },
+    setApplyTimeImmediate({ commit }) {
+      const data = {
+        HttpPushUriOptions: {
+          HttpPushUriApplyTime: {
+            ApplyTime: 'Immediate'
+          }
+        }
+      };
+      return api
+        .patch('/redfish/v1/UpdateService', data)
+        .then(() => commit('setApplyTime', 'Immediate'))
+        .catch(error => console.log(error));
+    },
+    async uploadFirmware({ state, dispatch }, image) {
+      if (state.applyTime !== 'Immediate') {
+        // ApplyTime must be set to Immediate before making
+        // request to update firmware
+        await dispatch('setApplyTimeImmediate');
+      }
+      api
+        .post('/redfish/v1/UpdateService', image, {
+          headers: { 'Content-Type': 'application/octet-stream' }
+        })
+        .then(response => {
+          console.log('post image success:', response);
+        })
+        .catch(error => console.log('post image error:', error));
+    },
+    uploadFirmwareTFTP(_, { address, filename }) {
+      console.log(address, filename);
     }
   }
 };
