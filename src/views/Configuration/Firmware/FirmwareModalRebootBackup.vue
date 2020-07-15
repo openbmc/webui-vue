194baae80b6858114dd1e9e46b3744f1432dd153
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-07-15 10:30:31 -0700

    Add Firmware page
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I04450e5a170d374122908c4c0349ba3b6e93ed2c

---
+++ src/views/Configuration/Firmware/FirmwareModalRebootBackup.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-modal
+    id="modal-reboot-backup"
+    header-class="justify-content-start"
+    :ok-title="$t('pageFirmware.modal.rebootFromBackup.primaryAction')"
+    @ok="$emit('ok')"
+  >
+    <template v-slot:modal-header>
+      <status-icon status="warning" class="pr-2" />{{
+        $t('pageFirmware.modal.rebootFromBackup.title')
+      }}
+    </template>
+    <p>
+      {{ $t('pageFirmware.modal.rebootFromBackup.message1', { backup }) }}
+    </p>
+    <p>
+      {{ $t('pageFirmware.modal.rebootFromBackup.message2', { current }) }}
+    </p>
+    <p class="font-weight-bold">
+      {{ $t('pageFirmware.modal.rebootFromBackup.message3', { backup }) }}
+    </p>
+  </b-modal>
+</template>
+
+<script>
+import StatusIcon from '@/components/Global/StatusIcon';
+export default {
+  components: { StatusIcon },
+  props: {
+    current: {
+      type: String,
+      required: true
+    },
+    backup: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/views/Configuration/Firmware/FirmwareModalUpload.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-modal
+    id="modal-upload"
+    :title="$t('pageFirmware.modal.uploadAndReboot.title')"
+    :ok-title="$t('pageFirmware.modal.uploadAndReboot.primaryAction')"
+    @ok="$emit('ok')"
+  >
+    <p>
+      {{ $t('pageFirmware.modal.uploadAndReboot.message1') }}
+    </p>
+    <p>
+      {{ $t('pageFirmware.modal.uploadAndReboot.message2') }}
+    </p>
+    <p class="font-weight-bold">
+      {{ $t('pageFirmware.modal.uploadAndReboot.message3') }}
+    </p>
+  </b-modal>
+</template>
