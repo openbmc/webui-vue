590646596969e891b61e5d880145fddd1852ae34
Sukanya Pandey <sukapan1@in.ibm.com>
2020-07-27 21:44:47 +0530

    Show total and filtered number of items in a table
     -The total number of items and the filtered items will be shown in the
       EventLogs, Sensors and HardwareStatus table.
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I0ee6410bf675038a350a71a02ec076f9e8baf004

---
+++ src/components/Global/TableCellCount.vue
@@ -0,0 +1,33 @@
+<template>
+  <p v-if="!filterActive" class="m-0 pt-2">
+    {{ $t('global.table.items', { count: totalNumberOfCells }) }}
+  </p>
+  <p v-else class="m-0 pt-2">
+    {{
+      $t('global.table.selectedItems', {
+        count: totalNumberOfCells,
+        filterCount: filteredItemsCount
+      })
+    }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    filteredItemsCount: {
+      type: Number,
+      required: true
+    },
+    totalNumberOfCells: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    filterActive() {
+      return this.filteredItemsCount !== this.totalNumberOfCells;
+    }
+  }
+};
+</script>
