0c35a45fae5704f09a9277853d6a42c1fe278a1b
Sukanya Pandey <sukapan1@in.ibm.com>
2020-07-27 21:44:47 +0530

    Add code for table row count
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I0ee6410bf675038a350a71a02ec076f9e8baf004

---
+++ src/components/Global/TableCellCount.vue
@@ -0,0 +1,32 @@
+<template>
+  <p v-if="!filterActive" class="tableCount">{{ totalNumberOfCells }} cells</p>
+  <p v-else class="tableCount">
+    {{ selectedItemsCount }} cells of {{ totalNumberOfCells }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    selectedItemsCount: {
+      type: Number,
+      required: true
+    },
+    filterActive: {
+      type: Boolean,
+      required: true,
+      default: () => false
+    },
+    totalNumberOfCells: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+.tableCount {
+  margin: 0;
+  padding-top: 0.5rem;
+}
+</style>
