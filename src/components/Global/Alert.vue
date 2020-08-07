c3c1b84fdc97a9da96c5d680d299f28b18681f6d
SurenNeware <sneware9@in.ibm.com>
2020-08-07 18:22:47 +0530

    Add info icon for informational alert
    -Add new style carbon icon for informational alert
    -Change alert icon position from center to top
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: If32d712db16472ce9a78e04e5871d0b799bb5895

--- src/components/Global/Alert.vue
+++ src/components/Global/Alert.vue
@@ -1,6 +1,14 @@
 <template>
   <b-alert :show="show" :variant="variant" :class="{ small }">
-    <div v-if="variant == 'warning' || variant == 'danger'" class="alert-icon">
+    <div
+      v-if="
+        variant == 'info' ||
+          variant == 'success' ||
+          variant == 'warning' ||
+          variant == 'danger'
+      "
+      class="alert-icon d-inline-flex mb-sm-0"
+    >
       <status-icon :status="variant" />
     </div>
     <div class="alert-content d-inline-flex align-items-center">
