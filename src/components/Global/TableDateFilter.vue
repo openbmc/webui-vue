8537a9eab5112754c72426fc30932c274f325545
SurenNeware <sneware9@in.ibm.com>
2020-08-17 18:42:20 +0530

    Add new style guide to global button style
    - Updated new style guide for Primary, Secondary and Link button variants
    - Changed box-shadow style from outer to inside on focus and active state
    - Updated font weight from bold to regular for normal buttons and font
    weight bold for link button.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: I38ca94baa9daec3fd03b830fc7b6d676fcce15b0

--- src/components/Global/TableDateFilter.vue
+++ src/components/Global/TableDateFilter.vue
@@ -165,3 +165,21 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+// Input datepicker icon
+.b-form-datepicker {
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: $zindex-dropdown + 1;
+  .btn-link {
+    padding: 5px 6px 6px 8px;
+    margin: 1px;
+    &:focus {
+      padding: 7px 8px;
+      margin: 0;
+    }
+  }
+}
+</style>
