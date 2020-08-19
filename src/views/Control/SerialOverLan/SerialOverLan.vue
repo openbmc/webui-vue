da60954775c5c333aef31ccfee9920062cbd9ea8
Sukanya Pandey <sukapan1@in.ibm.com>
2020-08-19 19:13:49 +0530

    Add changes related to SOL page
    -Changes incorporated can be referred through below link:
      -https://github.com/openbmc/webui-vue/issues/25
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I39d79259e0470d11c15e596908eefee7a799262a

--- src/views/Control/SerialOverLan/SerialOverLan.vue
+++ src/views/Control/SerialOverLan/SerialOverLan.vue
@@ -4,52 +4,25 @@
 
     <page-section :section-title="$t('pageSerialoverLAN.subTitle')">
       <p>{{ $t('pageSerialoverLAN.subTitleDesc') }}</p>
+    </page-section>
 
-      <div class="terminal-container">
-        <serial-over-lan-console />
-      </div>
-      <div class="text-right">
-        <b-button
-          variant="link"
-          type="button"
-          class="button-launch"
-          @click="openConsoleWindow()"
-        >
-          <icon-launch />
-
-          {{ $t('pageSerialoverLAN.openNewTab') }}
-        </b-button>
-      </div>
+    <page-section class="mb-0">
+      <serial-over-lan-console :is-full-window="false" />
     </page-section>
   </b-container>
 </template>
 
 <script>
-import IconLaunch from '@carbon/icons-vue/es/launch/32';
 import PageTitle from '@/components/Global/PageTitle';
 import PageSection from '@/components/Global/PageSection';
 import SerialOverLanConsole from './SerialOverLanConsole';
 
 export default {
   name: 'SerialOverLan',
-  components: { IconLaunch, PageSection, PageTitle, SerialOverLanConsole },
-  methods: {
-    openConsoleWindow() {
-      window.open(
-        '#/console/serial-over-lan-console',
-        '_blank',
-        'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=550'
-      );
-    }
+  components: {
+    PageSection,
+    PageTitle,
+    SerialOverLanConsole
   }
 };
 </script>
-
-<style scoped>
-.button-launch > svg {
-  height: 25px;
-}
-.terminal-container {
-  width: 100%;
-}
-</style>
