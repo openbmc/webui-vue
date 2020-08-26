b3db6faf31f2562d3b027ef5d27561a75ef52f18
Dixsie Wolmers <dixsie@ibm.com>
2020-08-26 14:53:01 -0500

    Add 404 page
    - User will be redirected to 404 page when navigating to
      undefined routes
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I5d7bbcfdd279b6f6c993370e3adb39a516dbb1ba

---
+++ src/views/PageNotFound/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container fluid="xl">
+    <page-title :description="$t('pagePageNotFound.description')" />
+  </b-container>
+</template>
+<script>
+import PageTitle from '@/components/Global/PageTitle';
+export default {
+  name: 'PageNotFound',
+  components: { PageTitle }
+};
+</script>
