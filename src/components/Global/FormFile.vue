8dd737954b9473d31304f9e39a46ddce0658459e
SurenNeware <sneware9@in.ibm.com>
2020-09-03 18:35:21 +0530

    Add new style for form file component
    - Update style guide for the form file component as discussed from casual payback 09/17/2020.
    - Now only Add file labled button will be visible and selected file title will come down to the button.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: Ib59135691495c4dddfdbbae0e4228b55cdfda2a8

---
+++ src/components/Global/FormFile.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="custom-form-file-container">
+    <b-form-file
+      :id="id"
+      v-model="file"
+      :accept="accept"
+      :browse-text="$t('global.fileUpload.browseText')"
+      :disabled="disabled"
+    >
+    </b-form-file>
+    <slot name="invalid"></slot>
+    <div v-if="file" class="clear-selected-file bg-light px-3 py-2 mt-2">
+      {{ file ? file.name : '' }}
+      <b-button variant="light" class="p-0 pl-4 ml-auto" @click="file = null"
+        ><icon-close
+      /></b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BFormFile } from 'bootstrap-vue';
+import { required } from 'vuelidate/lib/validators';
+import IconClose from '@carbon/icons-vue/es/close/20';
+
+export default {
+  name: 'FormFile',
+  components: { BFormFile, IconClose },
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    browseText: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    accept: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      file: null
+    };
+  },
+  validations() {
+    return {
+      form: {
+        file: {
+          required
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.clear-selected-file {
+  display: flex;
+  background-color: theme-color('$light');
+  .btn {
+    line-height: 1;
+    &:hover,
+    &:focus,
+    &:active {
+      box-shadow: none !important;
+      border-color: transparent !important;
+      background-color: transparent !important;
+    }
+  }
+}
+</style>
