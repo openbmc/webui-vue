fddf5eba8550ac8750c1fda939be7eedd98998a9
SurenNeware <sneware9@in.ibm.com>
2020-09-03 18:35:21 +0530

    Add new style for form file component
    - Add new style guide for form file component.
    - Now only Add file labled button will be visible and selected file title will come right of the button.
    - Also fixed table horizontal scroll from Network setting page.
    - Created global Form file component.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: Ib59135691495c4dddfdbbae0e4228b55cdfda2a8

---
+++ src/components/Global/FormFile.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-form-file
+    :id="id"
+    :value="file"
+    placeholder=""
+    :browse-text="$t('global.fileUpload.browseText')"
+    :disabled="disabled"
+  ></b-form-file>
+</template>
+
+<script>
+import { BFormFile } from 'bootstrap-vue';
+export default {
+  name: 'FormFile',
+  components: {
+    BFormFile: BFormFile
+  },
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    browseText: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      file: null
+    };
+  }
+};
+</script>
