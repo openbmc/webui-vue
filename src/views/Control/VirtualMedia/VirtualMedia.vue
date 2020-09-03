306766d97babc9d9285536071a4cbfe5fc8fc6c8
SurenNeware <sneware9@in.ibm.com>
2020-09-03 18:35:21 +0530

    Add new style for form file component
    - Update style guide for the form file component as discussed from casual payback 09/17/2020.
    - Now only Add file labled button will be visible and selected file title will come down to the button.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: Ib59135691495c4dddfdbbae0e4228b55cdfda2a8

--- src/views/Control/VirtualMedia/VirtualMedia.vue
+++ src/views/Control/VirtualMedia/VirtualMedia.vue
@@ -13,11 +13,19 @@
                 :label-for="dev.id"
                 label-class="bold"
               >
-                <b-form-file
+                <form-file
                   v-show="!dev.isActive"
-                  :id="dev.id"
+                  :id="concatId(dev.id)"
                   v-model="dev.file"
-                />
+                >
+                  <template v-slot:invalid>
+                    <b-form-invalid-feedback role="alert">
+                      <template>
+                        {{ $t('global.form.required') }}
+                      </template>
+                    </b-form-invalid-feedback>
+                  </template>
+                </form-file>
                 <p v-if="dev.isActive">{{ dev.file.name }}</p>
               </b-form-group>
               <b-button
@@ -102,10 +110,11 @@ import BVToastMixin from '@/components/Mixins/BVToastMixin';
 import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
 import ModalConfigureConnection from './ModalConfigureConnection';
 import NbdServer from '@/utilities/NBDServer';
+import FormFile from '@/components/Global/FormFile';
 
 export default {
   name: 'VirtualMedia',
-  components: { PageTitle, PageSection, ModalConfigureConnection },
+  components: { PageTitle, PageSection, ModalConfigureConnection, FormFile },
   mixins: [BVToastMixin, LoadingBarMixin],
   data() {
     return {
@@ -208,6 +217,12 @@ export default {
     configureConnection(connectionData) {
       this.modalConfigureConnection = connectionData;
       this.$bvModal.show('configure-connection');
+    },
+    concatId(val) {
+      return val
+        .split(' ')
+        .join('_')
+        .toLowerCase();
     }
   }
 };
