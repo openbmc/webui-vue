883570705423d2cedc1bcf6f0783edd77e269538
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-09-03 13:58:35 -0700

    Make customimzations to IBM router
    Removes KVM definition from default router. Before this change,
    the IBM environment build would remove KVM from the app navigation,
    but users could still access the page by typing in url directly for the
    KVM page. With this change, the route should no longer be accessible
    and will redirect to our 404 page.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I75439bc9443f9cbabbedf80b1142dd97209f5456

--- src/env/router/ibm.js
+++ src/env/router/ibm.js
@@ -1,3 +1,15 @@
+import { remove } from 'lodash';
 import routes from '@/router/routes';
 
-export default routes;
+const customRoutes = routes.map(route => {
+  // Removes router definition that includes kvm
+  // in name property (main and console layouts)
+
+  // TODO: will revisit this, removing the definition
+  // removes the route from the application but
+  // the component is still included in the final build
+  remove(route.children, ({ name }) => name.includes('kvm'));
+  return route;
+});
+
+export default customRoutes;
