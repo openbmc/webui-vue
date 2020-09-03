af33bf08c55dbaa710f63785cf861facbd658721
SurenNeware <sneware9@in.ibm.com>
2020-09-03 18:35:21 +0530

    Add new style for form file component
    - Now only add file button will be visible.
    - And selected file name will come down to the button.
    - Changed form file component from custom type to plain.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: Ib59135691495c4dddfdbbae0e4228b55cdfda2a8

---
+++ src/components/Global/FormFile.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="custom-form-file-container">
+    <b-form-file
+      :id="id"
+      v-model="file"
+      :accept="accept"
+      :disabled="disabled"
+      :state="state"
+      plain
+      @input="$emit('input', file)"
+    >
+    </b-form-file>
+    <slot name="invalid"></slot>
+    <b-button variant="primary" class="add-file-btn">
+      {{ $t('global.fileUpload.browseText') }}
+    </b-button>
+    <div v-if="file" class="clear-selected-file px-3 py-2 mt-2">
+      {{ file ? file.name : '' }}
+      <b-button variant="light" class="px-2 ml-auto" @click="file = null"
+        ><icon-close :title="$t('global.fileUpload.clearSelectedFile')"
+      /></b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BFormFile } from 'bootstrap-vue';
+import IconClose from '@carbon/icons-vue/es/close/20';
+
+export default {
+  name: 'FormFile',
+  components: { BFormFile, IconClose },
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    accept: {
+      type: String,
+      default: '',
+    },
+    state: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      file: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-form-file-container {
+  position: relative;
+}
+.form-control-file {
+  opacity: 0;
+  position: relative;
+  z-index: $zindex-fixed;
+  height: 40px;
+}
+.clear-selected-file {
+  display: flex;
+  align-items: center;
+  background-color: theme-color('light');
+  .btn {
+    width: 36px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+
+    &:focus {
+      box-shadow: inset 0 0 0 2px theme-color('primary');
+    }
+  }
+}
+.add-file-btn {
+  position: absolute;
+  top: 0;
+  padding: 0.438rem 1rem;
+}
+</style>
