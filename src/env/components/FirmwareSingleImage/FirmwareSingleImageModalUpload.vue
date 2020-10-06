b2a573570621a11fcc638e67552c4911b97d61dd
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-10-06 10:00:19 -0700

    Two file firmware update
    Adds the ability to upload separate Host and BMC firmware images through
    the GUI. By default, the two file firmware update page will be shown.
    Changes to the .env configurations will enable the single file upload
    page. The IBM env is configured to allow single file firmware update.
    
    Two file upload features:
     - File upload through local workstation or tftp server
     - Reboot from backup BMC image
     - Does not have ability to reboot from backup host image
     - Does not implement checks in GUI for host status, which is the
       same as what we have in phosphor-webui
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Ibf2a2d9ffc3952dd5a5454c723350c61d9f91c3e

--- src/env/components/FirmwareSingleImage/FirmwareSingleImageModalUpload.vue
+++ src/env/components/FirmwareSingleImage/FirmwareSingleImageModalUpload.vue
@@ -1,18 +1,20 @@
 <template>
   <b-modal
     id="modal-upload"
-    :title="$t('pageFirmware.modal.uploadAndReboot.title')"
-    :ok-title="$t('pageFirmware.modal.uploadAndReboot.primaryAction')"
+    :title="$t('pageFirmware.modal.uploadAndRebootSingleImage.title')"
+    :ok-title="
+      $t('pageFirmware.modal.uploadAndRebootSingleImage.primaryAction')
+    "
     @ok="$emit('ok')"
   >
     <p>
-      {{ $t('pageFirmware.modal.uploadAndReboot.message1') }}
+      {{ $t('pageFirmware.modal.uploadAndRebootSingleImage.message1') }}
     </p>
     <p>
-      {{ $t('pageFirmware.modal.uploadAndReboot.message2') }}
+      {{ $t('pageFirmware.modal.uploadAndRebootSingleImage.message2') }}
     </p>
     <p class="font-weight-bold">
-      {{ $t('pageFirmware.modal.uploadAndReboot.message3') }}
+      {{ $t('pageFirmware.modal.uploadAndRebootSingleImage.message3') }}
     </p>
   </b-modal>
 </template>
