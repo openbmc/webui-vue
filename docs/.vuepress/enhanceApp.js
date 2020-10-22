2fabb1c8cf48a7e53dfb25efc2d898d384b46a15
Derick Montague <derick.montague@ibm.com>
2020-10-22 11:23:17 -0500

    Fix icon color in documentation
    - Add StatusIcon to ehnaceApp and use the component imported into the
    the documentation's component/app-imports directory to resolve fill
    rendering bug in the Alert component
    - Remove the scoped attribute to make the changes work for alerts in
    documentation
    - Update StatusIcon component to match changes to application component
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I4090d58f78a1d10d9745ee592424ca0c0c91c610

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,7 @@
 
 import "./styles/_index.scss";
 import Alert from "../../src/components/Global/Alert";
+import StatusIcon from "./components/app-imports/StatusIcon";
 
 // Bootstrap-vue Plugin imports
 import {
@@ -17,4 +18,5 @@ export default ({ Vue }) => {
 
       // BMC Components and Mixins
       Vue.component('Alert', Alert);
+      Vue.component('StatusIcon', StatusIcon);
 }
