ebf668a59611d3d5c6045ebda600fe6a33d9abb8
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-10-26 10:57:53 -0700

    Remove Virtual Media from IBM build
    Removes KVM from app navigation, router definition, and unregisters
    the store module.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I0e3ab2a6cb28b9923efbeb6b2c212b0a09d263a9

--- src/env/store/ibm.js
+++ src/env/store/ibm.js
@@ -1,6 +1,8 @@
 import store from '@/store';
 import FirmwareSingleImageStore from './FirmwareSingleImage/FirmwareSingleImageStore';
 
+store.unregisterModule('virtualMedia');
+
 store.unregisterModule('firmware');
 store.registerModule('firmwareSingleImage', FirmwareSingleImageStore);
 
