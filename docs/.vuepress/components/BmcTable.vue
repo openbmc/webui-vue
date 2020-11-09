095d80d01fe2bc743c8f481d4995c71d618f8f27
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-11-09 12:58:44 -0800

    Add documentation for table row expansion
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: I6123210454c0d18f973cf4ee91a6891a938579a2

--- docs/.vuepress/components/BmcTable.vue
+++ docs/.vuepress/components/BmcTable.vue
@@ -10,11 +10,31 @@
     sort-by="rank"
     :items="items"
     :fields="fields"
-  />
+  >
+    <!-- Expand chevron icon -->
+    <template #cell(expandRow)="row">
+      <b-button
+        variant="link"
+        :aria-label="expandRowLabel"
+        @click="toggleRowDetails(row)"
+      >
+        <icon-chevron :title="expandRowLabel" />
+      </b-button>
+    </template>
+
+    <!-- Expansion row -->
+    <template #row-details="{ item }">
+      <h3>Expanded row details</h3>
+      {{ item }}
+    </template>
+  </b-table>
 </template>
 
 <script>
+import IconChevron from '@carbon/icons-vue/es/chevron--down/20';
+
 export default {
+  components: {IconChevron},
   data() {
     return {
       items: [
@@ -36,6 +56,12 @@ export default {
       ],
       fields: [
         {
+          key: 'expandRow',
+          label: '',
+          tdClass: 'table-row-expand',
+          sortable: false,
+        },
+        {
           key: 'name',
           label: 'Name',
           sortable: true
@@ -50,8 +76,17 @@ export default {
           label: 'Description',
           sortable: false
         }
-      ]
+      ],
+      expandRowLabel: 'Expand table row'
     }
-  }
+  },
+  methods: {
+    toggleRowDetails(row) {
+      row.toggleDetails();
+      row.detailsShowing
+        ? (this.expandRowLabel = 'Expand table row')
+        : (this.expandRowLabel = 'Collapse table row');
+    },
+  },
 }
 </script>
