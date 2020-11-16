1f4ff5f94690ca6511a125cf048f0f02fc03879c
Dixsie Wolmers <dixsie@ibm.com>
2020-11-16 16:46:01 -0600

    Add back to top button to hardware status page
    - Created global back to top button
    - Button should be used on pages longer than 3 screens
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I32022613e1ba62667b46150463c8f407b0b4ff11

---
+++ src/components/Global/ButtonBackToTop.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-button
+    v-if="showButton"
+    id="scrollToTopBtn"
+    class="btn-top btn-icon-only"
+    variant="secondary"
+    :title="$t('global.ariaLabel.scrollToTop')"
+    :aria-label="$t('global.ariaLabel.scrollToTop')"
+    @click="scrollToTop"
+  >
+    <icon-up-to-top />
+  </b-button>
+</template>
+
+<script>
+import UpToTop24 from '@carbon/icons-vue/es/up-to-top/24';
+
+import { debounce } from 'lodash';
+
+export default {
+  name: 'BackToTop',
+  components: { IconUpToTop: UpToTop24 },
+  data() {
+    return {
+      showButton: false,
+    };
+  },
+  created() {
+    window.addEventListener('scroll', debounce(this.handleScroll, 200));
+  },
+  methods: {
+    handleScroll() {
+      document.documentElement.scrollTop > 500
+        ? (this.showButton = true)
+        : (this.showButton = false);
+    },
+    scrollToTop() {
+      document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn-top {
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  z-index: $zindex-fixed;
+  box-shadow: $box-shadow;
+  @media (min-width: 1600px) {
+    left: 1485px;
+  }
+}
+</style>
