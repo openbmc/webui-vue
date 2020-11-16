e2707ef0ca20b669aca7ac06274591bd795719a9
Dixsie Wolmers <dixsie@ibm.com>
2020-11-15 21:45:03 -0600

    Add page components documentation
    Components include: b-container, page-title, and page-section.
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I5933767d44504dbfb87c2772d140de4d0607d4ab

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,6 +49,7 @@ module.exports = {
             "/guide/components/",
             "/guide/components/alert",
             "/guide/components/buttons/",
+            "/guide/components/page",
             "/guide/components/status-icon/",
             "/guide/components/table/",
             "/guide/components/toast",
