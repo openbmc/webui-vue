22d4d527af48d87ca70a8766bacc5b1ec0cfe9b7
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2020-12-03 10:58:35 -0800

    Add dumps page to IBM env
    Adds a non-functional dumps page with majority of layout and
    user flows in place. Page visible with IBM dotenv configurations.
    
    Includes:
    - Initial GET for BMC dumps
    - Table search, sort, filter, batch and row action flows
    - New dump form, validations, and toast notification
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Id59ec4327744f4e10cc99e4e3c0b2db5b0476f9c

--- src/env/store/ibm.js
+++ src/env/store/ibm.js
@@ -1,9 +1,11 @@
 import store from '@/store';
 import FirmwareSingleImageStore from './FirmwareSingleImage/FirmwareSingleImageStore';
+import DumpsStore from './Dumps/DumpsStore';
 
 store.unregisterModule('virtualMedia');
 
 store.unregisterModule('firmware');
 store.registerModule('firmwareSingleImage', FirmwareSingleImageStore);
+store.registerModule('dumps', DumpsStore);
 
 export default store;
