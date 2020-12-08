99204baa84999d39db19248fa14ec37afa71ddf7
SurenNeware <sneware9@in.ibm.com>
2020-12-08 20:06:44 +0530

    Add factory reset page
    - Adds a factory reset page with the majority of layout and
    user flows in place.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: I50a5912617d6dfb12d82bbcc632ced565652a56b

---
+++ src/store/modules/Configuration/FactoryResetStore.js
@@ -0,0 +1,21 @@
+// import api from '@/store/api';
+
+const FactoryResetStore = {
+  namespaced: true,
+  state: {
+    // Todo waiting for API availabilty
+  },
+  getters: {
+    // Todo waiting for API availabilty
+  },
+  mutations: {
+    // Todo waiting for API availabilty
+  },
+  actions: {
+    async getResetConfirmationValue() {
+      // Todo waiting for API availabilty
+    },
+  },
+};
+
+export default FactoryResetStore;
