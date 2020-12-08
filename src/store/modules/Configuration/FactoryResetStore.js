e7e6e56ddbb65db8b35a183109aee2882bb9256e
SurenNeware <sneware9@in.ibm.com>
2020-12-08 20:06:44 +0530

    Add factory reset page
    - Adds a factory reset page with the majority of layout and
    user flows in place.
    
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: I50a5912617d6dfb12d82bbcc632ced565652a56b

---
+++ src/store/modules/Configuration/FactoryResetStore.js
@@ -0,0 +1,34 @@
+// import api from '@/store/api';
+import i18n from '@/i18n';
+
+const FactoryResetStore = {
+  namespaced: true,
+  actions: {
+    async resetHostFirmwareSettings() {
+      // Todo waiting for API availabilty
+      // return await api
+      //   .post('/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios')
+      //   .then((response) => {
+      //     console.log(response);
+      //   })
+      //   .catch((error) => {
+      //     console.log('Error', error);
+      //     return error;
+      //   });
+      return new Promise((resolve) => setTimeout(resolve, 2000)).then(() =>
+        i18n.t('pageFactoryReset.toast.successHostFirmwareSettings')
+      );
+    },
+    async resetBmcHostFirmwareSettings() {
+      // Todo waiting for API availabilty
+      // return await api.post(
+      //   '/redfish/v1/Managers/bmc/Actions/Manager.ResetToDefaults'
+      // );
+      return new Promise((resolve) => setTimeout(resolve, 2000)).then(() =>
+        i18n.t('pageFactoryReset.toast.successBmcReboot')
+      );
+    },
+  },
+};
+
+export default FactoryResetStore;
