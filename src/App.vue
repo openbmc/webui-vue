33ee28e50f21161c9d473ad414429098db49ca2d
Sukanya Pandey <sukapan1@in.ibm.com>
2020-12-09 16:14:19 +0530

    Show asset name in the app header
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: If5394604d6c91b3604eaadb33178376fe6da672c

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,22 @@
 <script>
 export default {
   name: 'App',
+  computed: {
+    assetTag() {
+      return this.$store.getters['global/assetTag'];
+    },
+  },
   watch: {
+    assetTag: function (tag) {
+      if (tag) {
+        document.title = `${tag} - ${this.$route.meta.title}`;
+      }
+    },
     $route: function (to) {
       document.title = to.meta.title || 'Page is missing title';
+      if (this.assetTag) {
+        document.title = `${this.assetTag} - ${to.meta.title}`;
+      }
     },
   },
   created() {
