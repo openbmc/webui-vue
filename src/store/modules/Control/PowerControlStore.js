55565237578e8e1158d5a37bf8bf29648ff21e7d
Sukanya Pandey <sukapan1@in.ibm.com>
2020-12-16 20:08:01 +0530

    Add power restore policy page
    - This page will show how the system boots once power
      is restored after a power disturbance.
    
    - API used to fetch the JSON:
      /redfish/v1/JsonSchemas/ComputerSystem/ComputerSystem.json
    - API used to fetch the current policy and to save updated policy:
      /redfish/v1/Systems/system/
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I203bd3c5d26071b882cce6b9950cdb3fb1deb212

--- src/store/modules/Control/PowerControlStore.js
+++ src/store/modules/Control/PowerControlStore.js
@@ -6,16 +6,25 @@ const PowerControlStore = {
   state: {
     powerCapValue: null,
     powerConsumptionValue: null,
+    powerPolicy: null,
+    powerRestoreCurrentPolicy: null,
+    powerRestorePolicies: [],
   },
   getters: {
     powerCapValue: (state) => state.powerCapValue,
     powerConsumptionValue: (state) => state.powerConsumptionValue,
+    powerRestoreCurrentPolicy: (state) => state.powerRestoreCurrentPolicy,
+    powerRestorePolicies: (state) => state.powerRestorePolicies,
   },
   mutations: {
     setPowerCapValue: (state, powerCapValue) =>
       (state.powerCapValue = powerCapValue),
     setPowerConsumptionValue: (state, powerConsumptionValue) =>
       (state.powerConsumptionValue = powerConsumptionValue),
+    setPowerRestoreCurrentPolicy: (state, powerRestoreCurrentPolicy) =>
+      (state.powerRestoreCurrentPolicy = powerRestoreCurrentPolicy),
+    setPowerRestorePolicies: (state, powerRestorePolicies) =>
+      (state.powerRestorePolicies = powerRestorePolicies),
   },
   actions: {
     setPowerCapUpdatedValue({ commit }, value) {
@@ -51,6 +60,54 @@ const PowerControlStore = {
           console.log(error);
           throw new Error(
             i18n.t('pageServerPowerOperations.toast.errorSaveSettings')
+          );
+        });
+    },
+    async getPowerRestorePolicies({ commit, dispatch }) {
+      dispatch('getPowerRestoreCurrentPolicy');
+      return await api
+        .get('/redfish/v1/JsonSchemas/ComputerSystem/ComputerSystem.json')
+        .then(
+          ({
+            data: {
+              definitions: { PowerRestorePolicyTypes = {} },
+            },
+          }) => {
+            let powerPoliciesData = PowerRestorePolicyTypes.enum.map(
+              (powerState) => {
+                return {
+                  state: powerState,
+                  desc: `${i18n.t(
+                    `pagePowerRestorePolicy.policies.${powerState}`
+                  )} - ${PowerRestorePolicyTypes.enumDescriptions[powerState]}`,
+                };
+              }
+            );
+            commit('setPowerRestorePolicies', powerPoliciesData);
+          }
+        );
+    },
+    async getPowerRestoreCurrentPolicy({ commit }) {
+      api
+        .get('/redfish/v1/Systems/system')
+        .then(({ data: { PowerRestorePolicy } }) => {
+          commit('setPowerRestoreCurrentPolicy', PowerRestorePolicy);
+        })
+        .catch((error) => console.log(error));
+    },
+    async setPowerRestorePolicy({ commit }, powerPolicy) {
+      const data = { PowerRestorePolicy: powerPolicy };
+
+      return await api
+        .patch('/redfish/v1/Systems/system', data)
+        .then(() =>
+          commit('setPowerRestoreCurrentPolicy', data.PowerRestorePolicy)
+        )
+        .then(() => i18n.t('pagePowerRestorePolicy.toast.successSaveSettings'))
+        .catch((error) => {
+          console.log(error);
+          throw new Error(
+            i18n.t('pagePowerRestorePolicy.toast.errorSaveSettings')
           );
         });
     },
