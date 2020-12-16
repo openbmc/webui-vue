67348b3d72a9326cf3bfe6167d3369fa4c772016
Sukanya Pandey <sukapan1@in.ibm.com>
2020-12-16 20:08:01 +0530

    Add power restore policy page
    - This page will show how the system boots once power
      is restored after a power disturbance.
    
    - API used to fetch the JSON:
      /redfish/v1/JsonSchemas/ComputerSystem/ComputerSystem.json
    - API used to fetch the current policy and to save updated policy:
      /redfish/v1/Systems/system/
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I203bd3c5d26071b882cce6b9950cdb3fb1deb212

---
+++ src/views/Control/PowerRestorePolicy/PowerRestorePolicy.vue
@@ -0,0 +1,96 @@
+<template>
+  <b-container fluid="xl">
+    <page-title :description="$t('pagePowerRestorePolicy.description')" />
+
+    <b-row>
+      <b-col sm="8" md="6" xl="12">
+        <dl>
+          <dt>{{ $t('pagePowerRestorePolicy.currentPolicy') }}</dt>
+          <dd>
+            {{ currentPowerRestorePolicy }}
+          </dd>
+        </dl>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col sm="8" md="6" xl="12">
+        <b-form-group :label="$t('pagePowerRestorePolicy.powerPoliciesLabel')">
+          <b-form-radio
+            v-for="policy in powerRestorePolicies"
+            :key="policy.state"
+            v-model="currentPowerRestorePolicy"
+            :value="policy.state"
+            name="power-restore-policy"
+          >
+            {{ policy.desc }}
+          </b-form-radio>
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <b-button
+      variant="primary"
+      type="submit"
+      data-test-id="managePowerUsage-button-savePowerCapValue"
+      @click="submitForm"
+    >
+      {{ $t('global.action.saveSettings') }}
+    </b-button>
+  </b-container>
+</template>
+
+<script>
+import PageTitle from '@/components/Global/PageTitle';
+import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
+import VuelidateMixin from '@/components/Mixins/VuelidateMixin.js';
+import BVToastMixin from '@/components/Mixins/BVToastMixin';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'PowerRestorePolicy',
+  components: { PageTitle },
+  mixins: [VuelidateMixin, BVToastMixin, LoadingBarMixin],
+  beforeRouteLeave(to, from, next) {
+    this.hideLoader();
+    next();
+  },
+  data() {
+    return {
+      PolicyValue: null,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      powerRestorePolicies: 'powerControl/powerRestorePolicies',
+    }),
+    currentPowerRestorePolicy: {
+      get() {
+        return this.$store.getters['powerControl/powerRestoreCurrentPolicy'];
+      },
+      set(policy) {
+        this.PolicyValue = policy;
+      },
+    },
+  },
+  created() {
+    this.startLoader();
+    this.$store
+      .dispatch('powerControl/getPowerRestorePolicies')
+      .finally(() => this.endLoader());
+  },
+  methods: {
+    submitForm() {
+      this.startLoader();
+      this.$store
+        .dispatch(
+          'powerControl/setPowerRestorePolicy',
+          this.PolicyValue || this.currentPowerRestorePolicy
+        )
+        .then((message) => this.successToast(message))
+        .catch(({ message }) => this.errorToast(message))
+        .finally(() => this.endLoader());
+    },
+  },
+};
+</script>
