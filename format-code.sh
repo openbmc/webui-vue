91f8ad36de0dc21dc93786b40e65a53a6b888807
Patrick Williams <patrick@stwcx.xyz>
2020-12-29 17:19:12 -0600

    lint: update format-code script
    Due to openbmc/openbmc-build-scripts@55448ad, format-code scripts are
    now required to have a #!.  Add one here.
    
    Format-code scripts are also ran from an arbitrary directory but the
    path to the root of the repository is passed as the first argument.
    Switch to that directory so `npm` does the right thing.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ic15ef0af7d7ebd224762e9eaba41d3b92ce5e5bd

--- format-code.sh
+++ format-code.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Run GUI Linting and Formatting as part of the CI Build process
 #
 # This is a short term solution. The long term solution to will be to
@@ -5,6 +6,13 @@
 #
 
 set -e
+
+# When called from openbmc-build-scripts, the `pwd` could be anywhere, but
+# the root of the repo is passed in the first argument.  Switch to the repo
+# root so npm/git run in the right place.
+if [ -n "$1" ]; then
+    cd "$1"
+fi
 
 npm ci
 npm run lint
