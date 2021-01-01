77b64af4a73ede7af657025c7cc711cbf30e5915
SurenNeware <sneware9@in.ibm.com>
2021-01-01 19:20:10 +0530

    Add unit test cases for alert component
    Signed-off-by: Suren Neware <sneware9@in.ibm.com>
    Change-Id: I12edb7bcf1f9698d5fd847e7b8047f358342711e

---
+++ tests/unit/Global/Alert.spec.js
@@ -0,0 +1,35 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Alert from '@/components/Global/Alert';
+
+const localVue = createLocalVue();
+
+describe('Alert.vue', () => {
+  const wrapper = mount(Alert, {
+    localVue,
+    propsData: {
+      show: true,
+      variant: 'Test a type of alert',
+      small: false,
+    },
+    mocks: {
+      $t: (key) => key,
+    },
+  });
+  it('should exist', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+  it('should render alert element', () => {
+    expect(wrapper.find('.alert').isVisible()).toBe(true);
+  });
+  it('should show alert', async () => {
+    await wrapper.setProps({ show: true });
+    expect(wrapper.find('.alert').exists()).toBe(true);
+  });
+  it('should hide alert', async () => {
+    await wrapper.setProps({ show: false });
+    expect(wrapper.find('.alert').exists()).toBe(false);
+  });
+  it('should render correctly', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
