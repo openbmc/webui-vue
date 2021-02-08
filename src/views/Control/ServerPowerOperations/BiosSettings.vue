2999cad910e2af7e5002cebec006ce36a76d9905
Sukanya Pandey <sukapan1@in.ibm.com>
2021-02-08 23:00:12 +0530

    Add BIOS attributes in server power operations page
    - This page will show the BIOS options.
    - APIs used are:
      - To get the BIOS attributes '/redfish/v1/Systems/system/Bios'
      - To get the values of BIOS attributes
      '/redfish/v1/Registries/BiosAttributeRegistry/BiosAttributeRegistry'
      - To save the updated BIOS attributes
      '/redfish/v1/Systems/system/Bios/Settings'
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: Iae489aa86977fcddca337f09da8537bdb9ce6eb2

---
+++ src/views/Control/ServerPowerOperations/BiosSettings.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bios-form-background">
+    <b-form-group
+      v-for="(values, key) of attributeKeys"
+      :key="key"
+      :label="$t(`${'pageServerPowerOperations.biosSettings'}.${key}`)"
+      class="mb-4"
+    >
+      <b-form-select
+        id="bios-option-sysOp-mode"
+        :value="values"
+        :options="attributeValues[key]"
+        @change="updateAttributeKeys(key, $event)"
+      >
+      </b-form-select>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BiosSettings',
+  props: {
+    attributes: {
+      type: Object,
+      default: null,
+    },
+    attributeValues: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      attributeKeys: { ...this.attributes },
+    };
+  },
+  methods: {
+    updateAttributeKeys(key, value) {
+      this.attributeKeys[key] = value;
+      this.$emit('updated-attributes', this.attributeKeys);
+    },
+  },
+};
+</script>
