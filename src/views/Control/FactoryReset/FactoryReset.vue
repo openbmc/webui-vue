fdeba88af1c3fb3db2b09f11ca834876590c1b36
Derick Montague <derick.montague@ibm.com>
2021-02-14 19:21:44 -0600

    Add Factory Reset functionality
    Status
    - Done: Nav, Router, Layout, Modals, Store base with mock calls
    - ToDo: Call API, Testing
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I32a10dbce27a03fb84e24d7eae7c44eef9cffea5

---
+++ src/views/Control/FactoryReset/FactoryReset.vue
@@ -0,0 +1,110 @@
+<template>
+  <b-container fluid="xl">
+    <page-title :description="$t('pageFactoryReset.page.description')" />
+
+    <!-- Reset Form -->
+    <b-form id="factory-reset" @submit.prevent="onResetSubmit">
+      <b-row>
+        <b-col md="8">
+          <b-form-group :label="$t('pageFactoryReset.page.resetOptionsLabel')">
+            <b-form-radio-group
+              id="factoryResetOptions"
+              v-model="resetOptions"
+              name="factoryResetOptions"
+              stacked
+            >
+              <b-form-radio
+                class="mb-1"
+                value="resetBios"
+                aria-describedby="resetBios"
+                data-test-id="factoryReset-radio-resetBios"
+              >
+                {{ $t('pageFactoryReset.page.resetBiosOptionLabel') }}
+              </b-form-radio>
+              <b-form-text id="resetBios" class="ml-4 mb-3">
+                {{ $t('pageFactoryReset.page.resetBiosOptionHelperText') }}
+              </b-form-text>
+
+              <b-form-radio
+                class="mb-1"
+                value="resetToDefaults"
+                aria-describedby="resetToDefaults"
+                data-test-id="factoryReset-radio-resetToDefaults"
+              >
+                {{ $t('pageFactoryReset.page.resetToDefaultsOptionLabel') }}
+              </b-form-radio>
+              <b-form-text id="resetToDefaults" class="ml-4 mb-3">
+                {{
+                  $t('pageFactoryReset.page.resetToDefaultsOptionHelperText')
+                }}
+              </b-form-text>
+            </b-form-radio-group>
+          </b-form-group>
+          <b-button
+            type="submit"
+            variant="primary"
+            data-test-id="factoryReset-button-submit"
+          >
+            {{ $t('global.action.reset') }}
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+
+    <!-- Modals -->
+    <modal-reset :reset-type="resetOptions" @okConfirm="onOkConfirm" />
+  </b-container>
+</template>
+
+<script>
+import PageTitle from '@/components/Global/PageTitle';
+import BVToastMixin from '@/components/Mixins/BVToastMixin';
+import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
+import ModalReset from './FactoryResetModal';
+
+export default {
+  name: 'FactoryReset',
+  components: { PageTitle, ModalReset },
+  mixins: [LoadingBarMixin, BVToastMixin],
+  data() {
+    return {
+      resetOptions: 'resetBios',
+    };
+  },
+  created() {
+    this.hideLoader();
+  },
+  methods: {
+    onResetSubmit() {
+      this.$bvModal.show('modal-reset');
+    },
+    onOkConfirm() {
+      if (this.resetOptions == 'resetBios') {
+        this.onResetBiosConfirm();
+      } else {
+        this.onResetToDefaultsConfirm();
+      }
+    },
+    onResetBiosConfirm() {
+      this.$store
+        .dispatch('factoryReset/resetBios')
+        .then((message) => {
+          this.successToast(message);
+        })
+        .catch(({ message }) => {
+          this.errorToast(message);
+        });
+    },
+    onResetToDefaultsConfirm() {
+      this.$store
+        .dispatch('factoryReset/resetToDefaults')
+        .then((message) => {
+          this.successToast(message);
+        })
+        .catch(({ message }) => {
+          this.errorToast(message);
+        });
+    },
+  },
+};
+</script>
