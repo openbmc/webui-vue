bb6e8ae0d1e09d72d31027f67177145ec5d68481
Derick Montague <derick.montague@ibm.com>
2021-02-14 19:21:44 -0600

    Add factory reset page
    This new page will be included in the Control section of the primary
    navigation. The user will be able to choose between two different
    reset actions.
    
    The  user  can make the following calls:
    - /redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios
    - /redfish/v1/Managers/bmc/Actions/Manager.ResetToDefaults
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I32a10dbce27a03fb84e24d7eae7c44eef9cffea5

---
+++ src/store/modules/Control/FactoryResetStore.js
@@ -0,0 +1,32 @@
+import api from '@/store/api';
+import i18n from '@/i18n';
+
+const FactoryResetStore = {
+  namespaced: true,
+  actions: {
+    async resetToDefaults() {
+      return await api
+        .post('/redfish/v1/Managers/bmc/Actions/Manager.ResetToDefaults', {
+          ResetToDefaultsType: 'ResetAll',
+        })
+        .then(() => i18n.t('pageFactoryReset.toast.resetToDefaultsSuccess'))
+        .catch((error) => {
+          console.log('Factory Reset: ', error);
+          throw new Error(
+            i18n.t('pageFactoryReset.toast.resetToDefaultsError')
+          );
+        });
+    },
+    async resetBios() {
+      return await api
+        .post('/redfish/v1/Systems/system/Bios/Actions/Bios.ResetBios')
+        .then(() => i18n.t('pageFactoryReset.toast.resetBiosSuccess'))
+        .catch((error) => {
+          console.log('Factory Reset: ', error);
+          throw new Error(i18n.t('pageFactoryReset.toast.resetBiosError'));
+        });
+    },
+  },
+};
+
+export default FactoryResetStore;
