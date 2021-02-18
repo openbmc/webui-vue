b290e899baa5f548bd2586462165e0b232165e5d
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2021-02-18 15:24:14 -0800

    Update the default firmware page
    - Updates made to the general layout and style
      - Small changes to the firmware cards
      - Changes to some page copy
      - Move update firmware form to bottom of page
    - Adds dynamic TFTP upload option
    - Adds dynamic card layout (2 cards or 4 cards)
    - Removes FirmwareSingleImage components that was used in IBM builds
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>
    Change-Id: Ib5465ecc30dd1505824bf41c82d33b7655d5e598

--- src/views/Configuration/Firmware/FirmwareModalSwitchToRunning.vue
+++ src/views/Configuration/Firmware/FirmwareModalSwitchToRunning.vue
@@ -1,17 +1,17 @@
 <template>
   <b-modal
     id="modal-switch-to-running"
-    :ok-title="$t('pageFirmware.singleFileUpload.modal.switchImages')"
+    :ok-title="$t('pageFirmware.modal.switchImages')"
     :cancel-title="$t('global.action.cancel')"
-    :title="$t('pageFirmware.singleFileUpload.modal.switchRunningImage')"
+    :title="$t('pageFirmware.modal.switchRunningImage')"
     @ok="$emit('ok')"
   >
     <p>
-      {{ $t('pageFirmware.singleFileUpload.modal.switchRunningImageInfo') }}
+      {{ $t('pageFirmware.modal.switchRunningImageInfo') }}
     </p>
     <p class="m-0">
       {{
-        $t('pageFirmware.singleFileUpload.modal.switchRunningImageInfo2', {
+        $t('pageFirmware.modal.switchRunningImageInfo2', {
           backup,
         })
       }}
--- src/views/Configuration/Firmware/FirmwareModalUpdateFirmware.vue
+++ src/views/Configuration/Firmware/FirmwareModalUpdateFirmware.vue
@@ -1,26 +1,24 @@
 <template>
   <b-modal
     id="modal-update-firmware"
-    :title="$t('pageFirmware.singleFileUpload.sectionTitleUpdateFirmware')"
-    :ok-title="
-      $t('pageFirmware.singleFileUpload.form.updateFirmware.startUpdate')
-    "
+    :title="$t('pageFirmware.sectionTitleUpdateFirmware')"
+    :ok-title="$t('pageFirmware.form.updateFirmware.startUpdate')"
     :cancel-title="$t('global.action.cancel')"
     @ok="$emit('ok')"
   >
     <template v-if="isSingleFileUploadEnabled">
       <p>
-        {{ $t('pageFirmware.singleFileUpload.modal.updateFirmwareInfo') }}
+        {{ $t('pageFirmware.modal.updateFirmwareInfo') }}
       </p>
       <p>
         {{
-          $t('pageFirmware.singleFileUpload.modal.updateFirmwareInfo2', {
+          $t('pageFirmware.modal.updateFirmwareInfo2', {
             running: runningBmcVersion,
           })
         }}
       </p>
       <p class="m-0">
-        {{ $t('pageFirmware.singleFileUpload.modal.updateFirmwareInfo3') }}
+        {{ $t('pageFirmware.modal.updateFirmwareInfo3') }}
       </p>
     </template>
     <template v-else>
@@ -33,15 +31,13 @@
 export default {
   computed: {
     runningBmc() {
-      return this.$store.getters['firmwareSingleImage/activeBmcFirmware'];
+      return this.$store.getters['firmware/activeBmcFirmware'];
     },
     runningBmcVersion() {
       return this.runningBmc?.version || '--';
     },
     isSingleFileUploadEnabled() {
-      return this.$store.getters[
-        'firmwareSingleImage/isSingleFileUploadEnabled'
-      ];
+      return this.$store.getters['firmware/isSingleFileUploadEnabled'];
     },
   },
 };
