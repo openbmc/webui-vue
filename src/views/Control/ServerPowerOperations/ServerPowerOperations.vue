241f8f2094aa61247e74e4e748d782c53bd84f55
Sukanya Pandey <sukapan1@in.ibm.com>
2021-03-30 19:49:08 +0530

    Hide and show boot settings override field
    -Hide and show boot settings override field as this field will be
     available in few systems.
    
    -Visibility of this field is dependent on API value which is
     "BootSourceOverrideTarget@Redfish.AllowableValues" from API
     "/redfish/v1/Systems/system/"
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: Ibfd5d6b1f08e23ddc8d7deb0f6943ab6dd5b3756

--- src/views/Control/ServerPowerOperations/ServerPowerOperations.vue
+++ src/views/Control/ServerPowerOperations/ServerPowerOperations.vue
@@ -49,7 +49,7 @@
       </b-col>
     </b-row>
     <b-row>
-      <b-col sm="8" md="6" xl="4">
+      <b-col v-if="isBootSourceOptions" sm="8" md="6" xl="4">
         <page-section
           :section-title="$t('pageServerPowerOperations.hostOsBootSettings')"
         >
@@ -181,6 +181,12 @@ export default {
     },
     oneTimeBootEnabled() {
       return this.$store.getters['hostBootSettings/overrideEnabled'];
+    },
+    isBootSourceOptions() {
+      let bootOptions = this.$store.getters[
+        'hostBootSettings/bootSourceOptions'
+      ];
+      return bootOptions.length !== 0;
     },
   },
   created() {
