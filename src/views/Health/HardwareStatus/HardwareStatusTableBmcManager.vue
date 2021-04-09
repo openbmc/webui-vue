faa6da530406c1a7c7bca5b4d477293965b276ca
Sandeepa Singh <sandeepa.singh@ibm.com>
2021-04-09 18:08:22 +0530

    Add BMC table missing properties
    - Added the following properties: Hardware type, FRU number,
    Manufacturer type, Manager type, BMC date and time, Last reset time
    
    - Design has been updated for BMC table
    
    Signed-off-by: Sandeepa Singh <sandeepa.singh@ibm.com>
    Change-Id: I6d84f867f3c272f3989f76c86f054f143b8b1bbd

--- src/views/Health/HardwareStatus/HardwareStatusTableBmcManager.vue
+++ src/views/Health/HardwareStatus/HardwareStatusTableBmcManager.vue
@@ -31,102 +31,130 @@
       <template #row-details="{ item }">
         <b-container fluid>
           <b-row>
-            <b-col sm="6">
+            <b-col class="mt-2" sm="6">
               <dl>
-                <!-- Description -->
-                <dt class="d-block">
-                  {{ $t('pageHardwareStatus.table.description') }}:
-                </dt>
-                <dd class="mb-4">
-                  {{ tableFormatter(item.description) }}
-                </dd>
+                <!-- Part number -->
+                <dt>{{ $t('pageHardwareStatus.table.partNumber') }}:</dt>
+                <dd>{{ tableFormatter(item.partNumber) }}</dd>
                 <br />
-                <!-- Firmware version -->
-                <dt class="d-block">
-                  {{ $t('pageHardwareStatus.table.firmwareVersion') }}:
-                </dt>
-                <dd class="mb-4">
-                  {{ tableFormatter(item.firmwareVersion) }}
-                </dd>
+                <!-- Serial number -->
+                <dt>{{ $t('pageHardwareStatus.table.serialNumber') }}:</dt>
+                <dd>{{ tableFormatter(item.serialNumber) }}</dd>
                 <br />
-                <!-- Service entry point UUID -->
-                <dt class="d-block">
-                  {{ $t('pageHardwareStatus.table.serviceEntryPointUuid') }}:
-                </dt>
-                <dd class="mb-4">
-                  {{ tableFormatter(item.serviceEntryPointUuid) }}
-                </dd>
+                <!-- Fru Number -->
+                <dt>{{ $t('pageHardwareStatus.table.fruNumber') }}:</dt>
+                <dd>{{ tableFormatter(item.sparePartNumber) }}</dd>
                 <br />
-                <!-- UUID -->
-                <dt class="d-block">
-                  {{ $t('pageHardwareStatus.table.uuid') }}:
-                </dt>
-                <dd class="mb-4">
-                  {{ tableFormatter(item.uuid) }}
-                </dd>
-              </dl>
-            </b-col>
-            <b-col sm="6">
-              <dl>
-                <!-- Power state -->
-                <dt>{{ $t('pageHardwareStatus.table.powerState') }}:</dt>
-                <dd>{{ tableFormatter(item.powerState) }}</dd>
-                <br />
-
                 <!-- Model -->
                 <dt>{{ $t('pageHardwareStatus.table.model') }}:</dt>
                 <dd>{{ tableFormatter(item.model) }}</dd>
                 <br />
-
-                <!-- Health rollup -->
-                <dt>
-                  {{ $t('pageHardwareStatus.table.statusHealthRollup') }}:
-                </dt>
-                <dd>{{ tableFormatter(item.healthRollup) }}</dd>
+                <!-- UUID -->
+                <dt>{{ $t('pageHardwareStatus.table.uuid') }}:</dt>
+                <dd>{{ tableFormatter(item.uuid) }}</dd>
                 <br />
-
+                <!-- Service entry point UUID -->
+                <dt>
+                  {{ $t('pageHardwareStatus.table.serviceEntryPointUuid') }}:
+                </dt>
+                <dd>{{ tableFormatter(item.serviceEntryPointUuid) }}</dd>
+              </dl>
+            </b-col>
+            <b-col class="mt-2" sm="6">
+              <dl>
                 <!-- Status state -->
                 <dt>{{ $t('pageHardwareStatus.table.statusState') }}:</dt>
                 <dd>{{ tableFormatter(item.statusState) }}</dd>
                 <br />
-
+                <!-- Power state -->
+                <dt>{{ $t('pageHardwareStatus.table.power') }}:</dt>
+                <dd>{{ tableFormatter(item.powerState) }}</dd>
+                <br />
+                <!-- Health rollup -->
+                <dt>{{ $t('pageHardwareStatus.table.healthRollup') }}:</dt>
+                <dd>{{ tableFormatter(item.healthRollup) }}</dd>
+                <br />
+                <!-- BMC date and time -->
+                <dt>{{ $t('pageHardwareStatus.table.bmcDateTime') }}:</dt>
+                <dd>
+                  {{ item.dateTime | formatDate }}
+                  {{ item.dateTime | formatTime }}
+                </dd>
+                <br />
+                <!-- Reset date and time -->
+                <dt>{{ $t('pageHardwareStatus.table.lastResetTime') }}:</dt>
+                <dd>
+                  {{ item.lastResetTime | formatDate }}
+                  {{ item.lastResetTime | formatTime }}
+                </dd>
+              </dl>
+            </b-col>
+          </b-row>
+          <div class="section-divider"></div>
+          <b-row>
+            <b-col class="mt-2" sm="6">
+              <dl>
+                <!-- Manufacturer -->
+                <dt>{{ $t('pageHardwareStatus.table.manufacturer') }}:</dt>
+                <dd>{{ tableFormatter(item.manufacturer) }}</dd>
+                <br />
+                <!-- Description -->
+                <dt>{{ $t('pageHardwareStatus.table.description') }}:</dt>
+                <dd>{{ tableFormatter(item.description) }}</dd>
+                <br />
+                <!-- Manager type -->
+                <dt>{{ $t('pageHardwareStatus.table.managerType') }}:</dt>
+                <dd>{{ tableFormatter(item.managerType) }}</dd>
+                <br />
+              </dl>
+            </b-col>
+            <b-col class="mt-1" sm="6">
+              <dl>
+                <!-- Firmware Version  -->
+                <dt class="mt-2 mb-2">
+                  {{ $t('pageHardwareStatus.table.firmwareVersion') }}:
+                </dt>
+                <dd>{{ item.firmwareVersion }}</dd>
                 <!-- Graphical console -->
-                <dt class="font-weight-bold mt-3 mb-2 d-block">
+                <dt class="mt-1 mb-2 d-block">
                   {{ $t('pageHardwareStatus.table.graphicalConsole') }}
                 </dt>
-                <dt>
+                <dt class="ml-4">
                   {{ $t('pageHardwareStatus.table.connectTypesSupported') }}:
                 </dt>
                 <dd>
                   {{ tableFormatterArray(item.graphicalConsoleConnectTypes) }}
                 </dd>
                 <br />
-                <dt>
+                <dt class="ml-4">
                   {{ $t('pageHardwareStatus.table.maxConcurrentSessions') }}:
                 </dt>
                 <dd>{{ tableFormatter(item.graphicalConsoleMaxSessions) }}</dd>
                 <br />
-                <dt>{{ $t('pageHardwareStatus.table.serviceEnabled') }}:</dt>
+                <dt class="ml-4">
+                  {{ $t('pageHardwareStatus.table.serviceEnabled') }}:
+                </dt>
                 <dd>{{ tableFormatter(item.graphicalConsoleEnabled) }}</dd>
-                <br />
 
                 <!-- Serial console -->
-                <dt class="font-weight-bold mt-3 mb-2 d-block">
+                <dt class="mt-1 mb-2 d-block">
                   {{ $t('pageHardwareStatus.table.serialConsole') }}
                 </dt>
-                <dt>
+                <dt class="ml-4">
                   {{ $t('pageHardwareStatus.table.connectTypesSupported') }}:
                 </dt>
                 <dd>
                   {{ tableFormatterArray(item.serialConsoleConnectTypes) }}
                 </dd>
                 <br />
-                <dt>
+                <dt class="ml-4">
                   {{ $t('pageHardwareStatus.table.maxConcurrentSessions') }}:
                 </dt>
                 <dd>{{ tableFormatter(item.serialConsoleMaxSessions) }}</dd>
                 <br />
-                <dt>{{ $t('pageHardwareStatus.table.serviceEnabled') }}:</dt>
+                <dt class="ml-4">
+                  {{ $t('pageHardwareStatus.table.serviceEnabled') }}:
+                </dt>
                 <dd>{{ tableFormatter(item.serialConsoleEnabled) }}</dd>
               </dl>
             </b-col>
@@ -165,19 +193,23 @@ export default {
           formatter: this.tableFormatter,
         },
         {
+          key: 'hardwareType',
+          label: this.$t('pageHardwareStatus.table.hardwareType'),
+          formatter: this.tableFormatter,
+        },
+        {
           key: 'health',
           label: this.$t('pageHardwareStatus.table.health'),
           formatter: this.tableFormatter,
-          tdClass: 'text-nowrap',
         },
         {
-          key: 'partNumber',
-          label: this.$t('pageHardwareStatus.table.partNumber'),
+          key: 'locationNumber',
+          label: this.$t('pageHardwareStatus.table.locationNumber'),
           formatter: this.tableFormatter,
         },
         {
-          key: 'serialNumber',
-          label: this.$t('pageHardwareStatus.table.serialNumber'),
+          key: 'identifyLed',
+          label: this.$t('pageHardwareStatus.table.identifyLed'),
           formatter: this.tableFormatter,
         },
       ],
