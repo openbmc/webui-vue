4015d4f186528b84a082774338267885bedcc6af
Sukanya Pandey <sukapan1@in.ibm.com>
2021-04-09 15:40:27 +0530

    Add system table missing properties
    - Design is updated for system table.
    
    - Missing properties are added, added properties are: Hardware type, Location number, Memory summary
      data, Processor summary data and submodel.
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I0e5143638ce2254f9b4fd2c8c520544c1db8d342

--- src/store/modules/Health/SystemStore.js
+++ src/store/modules/Health/SystemStore.js
@@ -14,15 +14,25 @@ const SystemStore = {
       system.assetTag = data.AssetTag;
       system.description = data.Description;
       system.firmwareVersion = data.BiosVersion;
+      system.hardwareType = data.Name;
       system.health = data.Status.Health;
       system.id = data.Id;
       system.locationIndicatorActive = data.LocationIndicatorActive;
+      system.locationNumber = data.LocationNumber;
       system.manufacturer = data.Manufacturer;
+      system.memorySummaryHealth = data.MemorySummary.Status.Health;
+      system.memorySummaryHealthRollup = data.MemorySummary.Status.HealthRollup;
+      system.memorySummaryState = data.MemorySummary.Status.State;
       system.model = data.Model;
-      system.partNumber = data.PartNumber;
+      system.processorSummaryCount = data.ProcessorSummary.Count;
+      system.processorSummaryHealth = data.ProcessorSummary.Status.Health;
+      system.processorSummaryHealthRoll =
+        data.ProcessorSummary.Status.HealthRollup;
+      system.processorSummaryState = data.ProcessorSummary.Status.State;
       system.powerState = data.PowerState;
       system.serialNumber = data.SerialNumber;
       system.healthRollup = data.Status.HealthRollup;
+      system.subModel = data.SubModel;
       system.statusState = data.Status.State;
       system.systemType = data.SystemType;
       state.systems = [system];
@@ -34,6 +44,11 @@ const SystemStore = {
         .get('/redfish/v1/Systems/system')
         .then(({ data }) => commit('setSystemInfo', data))
         .catch((error) => console.log(error));
+    },
+    saveIdentifyLEDState(_, state) {
+      api.patch('/redfish/v1/Systems/system', {
+        LocationIndicatorActive: state,
+      });
     },
   },
 };
