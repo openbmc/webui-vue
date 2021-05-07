dca9b0a5baddfec7ea0ea5e4bdb63c2dd0be27b2
MichalX Szopinski <michalx.szopinski@intel.com>
2021-05-07 13:14:50 +0200

    Remove hardcoded chassis in power supplies
    According to our conversation on community meeting we want to remove all hardcoded chassis names in request URL. This change corrects the "Power supplies" table in "Hardware Status" view.
    
    Signed-off-by: MichalX Szopinski <michalx.szopinski@intel.com>
    Change-Id: I5219c68b71477adee5f6785c58808eeaa5d35986

--- src/store/modules/Health/PowerSupplyStore.js
+++ src/store/modules/Health/PowerSupplyStore.js
@@ -38,12 +38,32 @@ const PowerSupplyStore = {
     },
   },
   actions: {
-    async getPowerSupply({ commit }) {
+    async getChassisCollection() {
       return await api
-        .get('/redfish/v1/Chassis/chassis/Power')
-        .then(({ data: { PowerSupplies } }) =>
-          commit('setPowerSupply', PowerSupplies)
+        .get('/redfish/v1/Chassis')
+        .then(({ data: { Members } }) =>
+          Members.map((member) => member['@odata.id'])
         )
+        .catch((error) => console.log(error));
+    },
+    async getAllPowerSupplies({ dispatch, commit }) {
+      const collection = await dispatch('getChassisCollection');
+      if (!collection) return;
+      return await api
+        .all(collection.map((chassis) => dispatch('getChassisPower', chassis)))
+        .then((supplies) => {
+          let suppliesList = [];
+          supplies.forEach(
+            (supply) => (suppliesList = [...suppliesList, ...supply])
+          );
+          commit('setPowerSupply', suppliesList);
+        })
+        .catch((error) => console.log(error));
+    },
+    async getChassisPower(_, id) {
+      return await api
+        .get(`${id}/Power`)
+        .then(({ data: { PowerSupplies } }) => PowerSupplies || [])
         .catch((error) => console.log(error));
     },
   },
