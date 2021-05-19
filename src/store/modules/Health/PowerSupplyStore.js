f8accc5468c4691dd8d2402843f47997382f6df5
Dixsie Wolmers <dixsie@ibm.com>
2021-05-19 09:30:36 -0500

    Update power supply table to use PowerSubsystem schema
    Adds toggle switch in identify LED column to enable/disable
    the LED. Updates efficiencyPercent in store per pending schema
    changes.
    
    Waiting for the following commits to be merged to test:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42221
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/40326/14
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I515e15d91ae0a143a76ceda83a6d31fff2fafe39

--- src/store/modules/Health/PowerSupplyStore.js
+++ src/store/modules/Health/PowerSupplyStore.js
@@ -1,4 +1,5 @@
 import api from '@/store/api';
+import i18n from '@/i18n';
 
 const PowerSupplyStore = {
   namespaced: true,
@@ -12,7 +13,7 @@ const PowerSupplyStore = {
     setPowerSupply: (state, data) => {
       state.powerSupplies = data.map((powerSupply) => {
         const {
-          EfficiencyPercent,
+          EfficiencyRatings,
           FirmwareVersion,
           LocationIndicatorActive,
           MemberId,
@@ -29,7 +30,7 @@ const PowerSupplyStore = {
           health: Status.Health,
           partNumber: PartNumber,
           serialNumber: SerialNumber,
-          efficiencyPercent: EfficiencyPercent,
+          efficiencyPercent: EfficiencyRatings[0].EfficiencyPercent,
           firmwareVersion: FirmwareVersion,
           identifyLed: LocationIndicatorActive,
           manufacturer: Manufacturer,
@@ -37,6 +38,7 @@ const PowerSupplyStore = {
           name: Name,
           sparePartNumber: SparePartNumber,
           statusState: Status.State,
+          uri: powerSupply['@odata.id'],
         };
       });
     },
@@ -44,11 +46,42 @@ const PowerSupplyStore = {
   actions: {
     async getPowerSupply({ commit }) {
       return await api
-        .get('/redfish/v1/Chassis/chassis/Power')
+        // Todo: update hardcoded endpoint after https://gerrit.openbmc-project.xyz/c/openbmc/webui-vue/+/42988
+        .get('/redfish/v1/Chassis/chassis/PowerSubsystem')
         .then(({ data: { PowerSupplies } }) =>
           commit('setPowerSupply', PowerSupplies)
         )
         .catch((error) => console.log(error));
+    },
+    async updateIdentifyLedValue({ dispatch }, led) {
+      const uri = led.uri;
+      const updatedIdentifyLedValue = {
+        LocationIndicatorActive: led.identifyLed,
+      };
+      return await api
+        // TODO: Test when functionality is merged https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42221
+        .patch(uri, updatedIdentifyLedValue)
+        .then(() => dispatch('getPowerSupply'))
+        .then(() => {
+          if (led.identifyLed) {
+            return i18n.t('pageHardwareStatus.toast.successEnableIdentifyLed');
+          } else {
+            return i18n.t('pageHardwareStatus.toast.successDisableIdentifyLed');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log('error', led.identifyLed);
+          if (led.identifyLed) {
+            throw new Error(
+              i18n.t('pageHardwareStatus.toast.errorEnableIdentifyLed')
+            );
+          } else {
+            throw new Error(
+              i18n.t('pageHardwareStatus.toast.errorDisableIdentifyLed')
+            );
+          }
+        });
     },
   },
 };
