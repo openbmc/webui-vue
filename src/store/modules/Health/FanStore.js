f39e7ca3890ab091a0d6a5c3908491b9d709b059
Dixsie Wolmers <dixsie@ibm.com>
2021-05-19 13:19:32 -0500

    Update fans table to use ThermalSubsytem schema
    Adds toggle switch for identify Led (LocationIndicatorActive),
    adds speed percent and updates to use thermal subsystem endpoint.
    
    Waiting for the following to be merged to test  the UI:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/41086/28
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42210
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: If830426b637bed3e791953a510cf6320c36beebb

--- src/store/modules/Health/FanStore.js
+++ src/store/modules/Health/FanStore.js
@@ -1,4 +1,5 @@
 import api from '@/store/api';
+import i18n from '@/i18n';
 
 const FanStore = {
   namespaced: true,
@@ -12,27 +13,27 @@ const FanStore = {
     setFanInfo: (state, data) => {
       state.fans = data.map((fan) => {
         const {
-          IndicatorLED,
+          LocationIndicatorActive,
           Location,
-          MemberId,
+          Id,
           Name,
-          Reading,
-          ReadingUnits,
+          SpeedPercent,
           Status = {},
           PartNumber,
           SerialNumber,
         } = fan;
         return {
-          id: MemberId,
+          id: Id,
           health: Status.Health,
           partNumber: PartNumber,
           serialNumber: SerialNumber,
           healthRollup: Status.HealthRollup,
-          identifyLed: IndicatorLED,
+          identifyLed: LocationIndicatorActive,
           locationNumber: Location,
           hardwareType: Name,
-          speed: Reading + ' ' + ReadingUnits,
+          speedPercent: SpeedPercent.Reading,
           statusState: Status.State,
+          uri: fan['@odata.id'],
         };
       });
     },
@@ -40,9 +41,41 @@ const FanStore = {
   actions: {
     async getFanInfo({ commit }) {
       return await api
-        .get('/redfish/v1/Chassis/chassis/Thermal')
+        // TODO: update hardcoded endpoint after https://gerrit.openbmc-project.xyz/c/openbmc/webui-vue/+/42988
+        .get('/redfish/v1/Chassis/chassis/ThermalSubsystem')
         .then(({ data: { Fans = [] } }) => commit('setFanInfo', Fans))
         .catch((error) => console.log(error));
+    },
+    async updateIdentifyLedValue({ dispatch }, led) {
+      const uri = led.uri;
+      const updatedIdentifyLedValue = {
+        LocationIndicatorActive: led.identifyLed,
+      };
+      return await api
+        // TODO: Test when functionality is merged
+        // https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42210
+        .patch(uri, updatedIdentifyLedValue)
+        .then(() => dispatch('getPowerSupply'))
+        .then(() => {
+          if (led.getFanInfo) {
+            return i18n.t('pageHardwareStatus.toast.successEnableIdentifyLed');
+          } else {
+            return i18n.t('pageHardwareStatus.toast.successDisableIdentifyLed');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log('error', led.identifyLed);
+          if (led.identifyLed) {
+            throw new Error(
+              i18n.t('pageHardwareStatus.toast.errorEnableIdentifyLed')
+            );
+          } else {
+            throw new Error(
+              i18n.t('pageHardwareStatus.toast.errorDisableIdentifyLed')
+            );
+          }
+        });
     },
   },
 };
