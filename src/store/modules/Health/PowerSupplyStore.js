b3f3ad798ea541d558de2a55a35f0e0dfdadc71e
Dixsie Wolmers <dixsie@ibm.com>
2021-05-19 09:30:36 -0500

    Update power supply table to use PowerSubsystem schema
    Adds toggle switch in identify LED column to enable/disable
    the LED. Updates efficiencyPercent in store per pending schema
    changes.
    
    Waiting for the following commits to be merged to test:
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/42221
    https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/40326/14
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I515e15d91ae0a143a76ceda83a6d31fff2fafe39

--- src/store/modules/Health/PowerSupplyStore.js
+++ src/store/modules/Health/PowerSupplyStore.js
@@ -1,4 +1,5 @@
 import api from '@/store/api';
+import i18n from '@/i18n';
 
 const PowerSupplyStore = {
   namespaced: true,
@@ -12,10 +13,10 @@ const PowerSupplyStore = {
     setPowerSupply: (state, data) => {
       state.powerSupplies = data.map((powerSupply) => {
         const {
-          EfficiencyPercent,
+          EfficiencyRatings,
           FirmwareVersion,
           LocationIndicatorActive,
-          MemberId,
+          Id,
           Manufacturer,
           Model,
           Name,
@@ -25,11 +26,11 @@ const PowerSupplyStore = {
           Status = {},
         } = powerSupply;
         return {
-          id: MemberId,
+          id: Id,
           health: Status.Health,
           partNumber: PartNumber,
           serialNumber: SerialNumber,
-          efficiencyPercent: EfficiencyPercent,
+          efficiencyPercent: EfficiencyRatings[0].EfficiencyPercent,
           firmwareVersion: FirmwareVersion,
           identifyLed: LocationIndicatorActive,
           manufacturer: Manufacturer,
@@ -37,6 +38,7 @@ const PowerSupplyStore = {
           name: Name,
           sparePartNumber: SparePartNumber,
           statusState: Status.State,
+          uri: powerSupply['@odata.id'],
         };
       });
     },
@@ -50,25 +52,47 @@ const PowerSupplyStore = {
         )
         .catch((error) => console.log(error));
     },
-    async getAllPowerSupplies({ dispatch, commit }) {
+    async getAllPowerSupplies({ dispatch }) {
       const collection = await dispatch('getChassisCollection');
       if (!collection) return;
       return await api
         .all(collection.map((chassis) => dispatch('getChassisPower', chassis)))
-        .then((supplies) => {
-          let suppliesList = [];
-          supplies.forEach(
-            (supply) => (suppliesList = [...suppliesList, ...supply])
+        .catch((error) => console.log(error));
+    },
+    async getChassisPower({ commit }, id) {
+      return await api
+        .get(`${id}/PowerSubsystem/PowerSupplies`)
+        .then((response) =>
+          response.data.Members.map((powerSupply) => powerSupply['@odata.id'])
+        )
+        .then((powerSupplyIds) =>
+          api.all(powerSupplyIds.map((powerSupply) => api.get(powerSupply)))
+        )
+        .then((powerSupplies) => {
+          const powerSuppliesData = powerSupplies.map(
+            (powerSupplies) => powerSupplies.data
           );
-          commit('setPowerSupply', suppliesList);
+          commit('setPowerSupply', powerSuppliesData);
         })
         .catch((error) => console.log(error));
     },
-    async getChassisPower(_, id) {
-      return await api
-        .get(`${id}/Power`)
-        .then(({ data: { PowerSupplies } }) => PowerSupplies || [])
-        .catch((error) => console.log(error));
+    async updateIdentifyLedValue(_, led) {
+      const uri = led.uri;
+      const updatedIdentifyLedValue = {
+        LocationIndicatorActive: led.identifyLed,
+      };
+      return await api.patch(uri, updatedIdentifyLedValue).catch((error) => {
+        console.log(error);
+        if (led.identifyLed) {
+          throw new Error(
+            i18n.t('pageHardwareStatus.toast.errorEnableIdentifyLed')
+          );
+        } else {
+          throw new Error(
+            i18n.t('pageHardwareStatus.toast.errorDisableIdentifyLed')
+          );
+        }
+      });
     },
   },
 };
