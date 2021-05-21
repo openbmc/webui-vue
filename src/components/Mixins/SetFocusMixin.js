97da2cec991cdcdf80d73bf1531123743c33f627
Dixsie Wolmers <dixsie@ibm.com>
2021-05-20 19:01:42 -0500

    Add quicklinks to hardware status page
    Adds setFocusJumpLinks mixin method to scroll to and set focus to
    selected page elements.
    
    Scroll offset is required to show table header below the nav header.
    Setting focus is required for accessibility.
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I500a2d70727c5a78aeae4a6193ba22a38e4f0b6f

--- src/components/Mixins/SetFocusMixin.js
+++ src/components/Mixins/SetFocusMixin.js
@@ -6,6 +6,21 @@ const setFocusMixin = {
       // Reason: https://axesslab.com/skip-links/#update-3-a-comment-from-gov-uk
       element.removeAttribute('tabindex');
     },
+    scrollToAnchor(event) {
+      // Select element to scroll to
+      const ref = event.target.getAttribute('data-ref');
+      const element = this.$refs[ref].$el;
+
+      // Set focus and tabindex on selected element
+      this.setFocus(element);
+
+      // Set scroll offset below header
+      const offset = element.offsetTop - 50;
+      window.scroll({
+        top: offset,
+        behavior: 'smooth',
+      });
+    },
   },
 };
 
