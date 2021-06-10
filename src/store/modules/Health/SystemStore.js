535cd8a753b6ca7b22133e545904e6d1e4bef485
Sukanya Pandey <sukapan1@in.ibm.com>
2021-06-10 15:35:21 +0530

    Add system attention indicators
    - Different LEDs and statuses will be added to hardware status page
    - Status for power will be shown and LED included is System identify Led
    - TODO: Attention LED, waiting from backend for implementation
            (backend will be available by Sept 2021)
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I8689f7bf3cc02a7a90379ec50b005bf344c091e4

--- src/store/modules/Health/SystemStore.js
+++ src/store/modules/Health/SystemStore.js
@@ -16,25 +16,26 @@ const SystemStore = {
       system.description = data.Description;
       system.firmwareVersion = data.BiosVersion;
       system.hardwareType = data.Name;
-      system.health = data.Status.Health;
+      system.health = data.Status?.Health;
       system.id = data.Id;
       system.locationIndicatorActive = data.LocationIndicatorActive;
       system.locationNumber = data.LocationNumber;
       system.manufacturer = data.Manufacturer;
-      system.memorySummaryHealth = data.MemorySummary.Status.Health;
-      system.memorySummaryHealthRollup = data.MemorySummary.Status.HealthRollup;
-      system.memorySummaryState = data.MemorySummary.Status.State;
+      system.memorySummaryHealth = data.MemorySummary?.Status.Health;
+      system.memorySummaryHealthRollup =
+        data.MemorySummary?.Status?.HealthRollup;
+      system.memorySummaryState = data.MemorySummary?.Status?.State;
       system.model = data.Model;
-      system.processorSummaryCount = data.ProcessorSummary.Count;
-      system.processorSummaryHealth = data.ProcessorSummary.Status.Health;
+      system.processorSummaryCount = data.ProcessorSummary?.Count;
+      system.processorSummaryHealth = data.ProcessorSummary?.Status?.Health;
       system.processorSummaryHealthRoll =
-        data.ProcessorSummary.Status.HealthRollup;
-      system.processorSummaryState = data.ProcessorSummary.Status.State;
+        data.ProcessorSummary?.Status.HealthRollup;
+      system.processorSummaryState = data.ProcessorSummary?.Status?.State;
       system.powerState = data.PowerState;
       system.serialNumber = data.SerialNumber;
-      system.healthRollup = data.Status.HealthRollup;
+      system.healthRollup = data.Status?.HealthRollup;
       system.subModel = data.SubModel;
-      system.statusState = data.Status.State;
+      system.statusState = data.Status?.State;
       system.systemType = data.SystemType;
       state.systems = [system];
     },
@@ -46,14 +47,13 @@ const SystemStore = {
         .then(({ data }) => commit('setSystemInfo', data))
         .catch((error) => console.log(error));
     },
-    changeIdentifyLedState({ dispatch }, ledState) {
-      api
+    async changeIdentifyLedState({ commit }, ledState) {
+      return await api
         .patch('/redfish/v1/Systems/system', {
           LocationIndicatorActive: ledState,
         })
-        .then(() => dispatch('getSystem'))
         .catch((error) => {
-          dispatch('getSystem');
+          commit('setSystemInfo', this.state.system.systems[0]);
           console.log('error', error);
           if (ledState) {
             throw new Error(
