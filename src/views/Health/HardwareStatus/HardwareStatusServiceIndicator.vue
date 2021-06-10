d38ad3838eda29fd97a2a080e68bc73b03730c11
Sukanya Pandey <sukapan1@in.ibm.com>
2021-06-10 15:35:21 +0530

    Add system attention indicators
    - Different LEDs and statuses will be added to hardware status page
    - Status for power will be shown
    - LEDs included are System identify Led and System attention Led
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I8689f7bf3cc02a7a90379ec50b005bf344c091e4

---
+++ src/views/Health/HardwareStatus/HardwareStatusServiceIndicator.vue
@@ -0,0 +1,81 @@
+<template>
+  <page-section :section-title="$t('pageHardwareStatus.systemIndicator.title')">
+    <div class="form-background pl-4 pt-4 pb-1">
+      <b-row>
+        <b-col sm="6" md="3">
+          <dl>
+            <dt>{{ $t('pageHardwareStatus.systemIndicator.powerStatus') }}</dt>
+            <dd>{{ $t('global.status.on') }}</dd>
+          </dl>
+        </b-col>
+        <b-col sm="6" md="3">
+          <dl>
+            <dt>
+              {{ $t('pageHardwareStatus.systemIndicator.identifyLed') }}
+            </dt>
+            <dd>
+              <b-form-checkbox
+                id="identifyLedSwitch"
+                v-model="systems.locationIndicatorActive"
+                data-test-id="hardwareStatus-toggle-identifyLed"
+                switch
+                @change="toggleIdentifyLedSwitch"
+              >
+                <span class="sr-only">
+                  {{ $t('pageHardwareStatus.systemIndicator.identifyLed') }}
+                </span>
+                <span v-if="systems.locationIndicatorActive">
+                  {{ $t('global.status.on') }}
+                </span>
+                <span v-else>{{ $t('global.status.off') }}</span>
+              </b-form-checkbox>
+            </dd>
+          </dl>
+        </b-col>
+        <b-col sm="6" md="3">
+          <dl>
+            <dt>
+              {{ $t('pageHardwareStatus.systemIndicator.attentionLed') }}
+            </dt>
+            <dd>
+              <b-form-checkbox
+                v-if="false"
+                id="attentionLedSwitch"
+                data-test-id="hardwareStatus-toggle-attentionLed"
+                switch
+              >
+                <span class="sr-only">
+                  {{ $t('pageHardwareStatus.systemIndicator.attentionLed') }}
+                </span>
+                <span>
+                  {{ $t('global.status.on') }}
+                </span>
+              </b-form-checkbox>
+              <span v-else>{{ $t('global.status.off') }}</span>
+            </dd>
+          </dl>
+        </b-col>
+      </b-row>
+    </div>
+  </page-section>
+</template>
+<script>
+import PageSection from '@/components/Global/PageSection';
+
+export default {
+  components: { PageSection },
+  computed: {
+    systems() {
+      return this.$store.getters['system/systems'][0];
+    },
+  },
+  created() {
+    this.$store.dispatch('system/getSystem');
+  },
+  methods: {
+    toggleIdentifyLedSwitch(state) {
+      this.$store.dispatch('system/changeIdentifyLedState', state);
+    },
+  },
+};
+</script>
