b39c254bd2478af537e70629ecab7ccc92d64d44
Sandeepa Singh <sandeepa.singh@ibm.com>
2021-07-19 18:04:18 +0530

    Update configuration section to settings
    This is the fourth update to information architecture changes and has
    the following changes:
    
    - The configuration section is updated to settings
    - The date and time settings page is updated to date and time
    - The network settings page is updated to network
    - The power restore policy page in operations section is moved to
    settings section
    
    Signed-off-by: Sandeepa Singh <sandeepa.singh@ibm.com>
    Change-Id: I6f5ab25f5227530be430bd39a4d9629b3bf09d8b

--- src/env/router/ibm.js
+++ src/env/router/ibm.js
@@ -2,7 +2,7 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import ChangePassword from '@/views/ChangePassword';
 import ClientSessions from '@/views/AccessControl/ClientSessions';
 import ConsoleLayout from '@/layouts/ConsoleLayout.vue';
-import DateTimeSettings from '@/views/Configuration/DateTimeSettings';
+import DateTime from '@/views/Settings/DateTime';
 import EventLogs from '@/views/Logs/EventLogs';
 import FactoryReset from '@/views/Operations/FactoryReset';
 import Firmware from '@/views/Operations/Firmware';
@@ -12,14 +12,14 @@ import LocalUserManagement from '@/views/AccessControl/LocalUserManagement';
 import Login from '@/views/Login';
 import LoginLayout from '@/layouts/LoginLayout';
 import ManagePowerUsage from '@/views/Operations/ManagePowerUsage';
-import NetworkSettings from '@/views/Configuration/NetworkSettings';
+import Network from '@/views/Settings/Network';
 import Overview from '@/views/Overview';
 import PageNotFound from '@/views/PageNotFound';
 import PostCodeLogs from '@/views/HardwareStatus/PostCodeLogs';
-import PowerRestorePolicy from '@/views/Operations/PowerRestorePolicy';
+import PowerRestorePolicy from '@/views/Settings/PowerRestorePolicy';
 import ProfileSettings from '@/views/ProfileSettings';
 import RebootBmc from '@/views/Operations/RebootBmc';
-import SecuritySettings from '@/views/Configuration/SecuritySettings';
+import SecuritySettings from '@/views/Settings/SecuritySettings';
 import Sensors from '@/views/HardwareStatus/Sensors';
 import SerialOverLan from '@/views/Operations/SerialOverLan';
 import SerialOverLanConsole from '@/views/Operations/SerialOverLan/SerialOverLanConsole';
@@ -167,11 +167,11 @@ const routes = [
         },
       },
       {
-        path: '/configuration/date-time-settings',
-        name: 'date-time-settings',
-        component: DateTimeSettings,
+        path: '/settings/date-time',
+        name: 'date-time',
+        component: DateTime,
         meta: {
-          title: i18n.t('appPageTitle.dateTimeSettings'),
+          title: i18n.t('appPageTitle.dateTime'),
         },
       },
       {
@@ -191,7 +191,15 @@ const routes = [
         },
       },
       {
-        path: '/operations/power-restore-policy',
+        path: '/settings/network',
+        name: 'network',
+        component: Network,
+        meta: {
+          title: i18n.t('appPageTitle.network'),
+        },
+      },
+      {
+        path: '/settings/power-restore-policy',
         name: 'power-restore-policy',
         component: PowerRestorePolicy,
         meta: {
@@ -199,15 +207,7 @@ const routes = [
         },
       },
       {
-        path: '/configuration/network-settings',
-        name: 'network-settings',
-        component: NetworkSettings,
-        meta: {
-          title: i18n.t('appPageTitle.networkSettings'),
-        },
-      },
-      {
-        path: '/configuration/security-settings',
+        path: '/settings/security-settings',
         name: 'security-settings',
         component: SecuritySettings,
         meta: {
