3629bf8747bdccb15c67e0d051262c3ddd116938
Sukanya Pandey <sukapan1@in.ibm.com>
2021-08-19 18:08:22 +0530

    Resolve bug while toggling switch in processor table
    Bug: While toggling identify LED for cpu0, in return other identify LEDs are also getting toggled which belongs
    to different CPUs.
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: Ic48eba7b391a28f861e38f306233387819ad944c

--- src/store/modules/HardwareStatus/ProcessorStore.js
+++ src/store/modules/HardwareStatus/ProcessorStore.js
@@ -81,22 +81,17 @@ const ProcessorStore = {
       const updatedIdentifyLedValue = {
         LocationIndicatorActive: led.identifyLed,
       };
-      return await api
-        .patch(uri, updatedIdentifyLedValue)
-        .then(() => dispatch('getProcessorsInfo'))
-        .catch((error) => {
-          dispatch('getProcessorsInfo');
-          console.log('error', error);
-          if (led.identifyLed) {
-            throw new Error(
-              i18n.t('pageInventory.toast.errorEnableIdentifyLed')
-            );
-          } else {
-            throw new Error(
-              i18n.t('pageInventory.toast.errorDisableIdentifyLed')
-            );
-          }
-        });
+      return await api.patch(uri, updatedIdentifyLedValue).catch((error) => {
+        dispatch('getProcessorsInfo');
+        console.log('error', error);
+        if (led.identifyLed) {
+          throw new Error(i18n.t('pageInventory.toast.errorEnableIdentifyLed'));
+        } else {
+          throw new Error(
+            i18n.t('pageInventory.toast.errorDisableIdentifyLed')
+          );
+        }
+      });
     },
   },
 };
