4b9cabcb6489bdcb03c3b8c2b9ef5d0482871b64
Sukanya Pandey <sukapan1@in.ibm.com>
2021-08-23 18:11:02 +0530

    Add ability to view all the dumps
    The story is integration of these two API responses
    for their respective dumps:
    
    1.Resource and Hostboot dumps:
    redfish/v1/Systems/system/LogServices/Dump/Entries
    2.BMC Dumps: /redfish/v1/Managers/bmc/LogServices/Dump/Entries
    
    Signed-off-by: Sukanya Pandey <sukapan1@in.ibm.com>
    Change-Id: I24ded733e682d21904e92e2e8be1951e46d33b09

--- src/store/modules/Logs/DumpsStore.js
+++ src/store/modules/Logs/DumpsStore.js
@@ -4,14 +4,14 @@ import i18n from '@/i18n';
 const DumpsStore = {
   namespaced: true,
   state: {
-    bmcDumps: [],
+    allDumps: [],
   },
   getters: {
-    bmcDumps: (state) => state.bmcDumps,
+    allDumps: (state) => state.allDumps,
   },
   mutations: {
-    setBmcDumps: (state, dumps) => {
-      state.bmcDumps = dumps.map((dump) => ({
+    setAllDumps: (state, dumps) => {
+      state.allDumps = dumps.map((dump) => ({
         data: dump.AdditionalDataURI,
         dateTime: new Date(dump.Created),
         dumpType: dump.Name,
@@ -22,10 +22,30 @@ const DumpsStore = {
     },
   },
   actions: {
-    async getBmcDumps({ commit }) {
+    async getBmcDumps() {
+      return api
+        .get('/redfish/v1/')
+        .then((response) => api.get(response.data.Managers['@odata.id']))
+        .then((response) => api.get(response.data.Members[0]['@odata.id']))
+        .then((response) => api.get(response.data.LogServices['@odata.id']))
+        .then((response) => api.get(response.data.Members[0]['@odata.id']))
+        .then((response) => api.get(response.data.Entries['@odata.id']))
+        .catch((error) => console.log(error));
+    },
+    async getAllDumps({ commit, dispatch }) {
       return await api
-        .get('/redfish/v1/Managers/bmc/LogServices/Dump/Entries')
-        .then(({ data = {} }) => commit('setBmcDumps', data.Members || []))
+        .all([
+          dispatch('getBmcDumps'),
+          //For below URL hypermedia @odata.id cannot be used because API structure is
+          //not appropriate and we doesnt know under Members array where the object will present
+          api.get('/redfish/v1/Systems/system/LogServices/Dump/Entries'),
+        ])
+        .then((response) => {
+          const bmcDumps = response[0].data?.Members || [];
+          const sysAndResourceDumps = response[1].data?.Members || [];
+          const allDumps = [...bmcDumps, ...sysAndResourceDumps];
+          commit('setAllDumps', allDumps);
+        })
         .catch((error) => console.log(error));
     },
     async createBmcDump() {
@@ -66,7 +86,7 @@ const DumpsStore = {
       return await api
         .all(promises)
         .then((response) => {
-          dispatch('getBmcDumps');
+          dispatch('getAllDumps');
           return response;
         })
         .then(
@@ -95,13 +115,13 @@ const DumpsStore = {
         );
     },
     async deleteAllDumps({ commit, state }) {
-      const totalDumpCount = state.bmcDumps.length;
+      const totalDumpCount = state.allDumps.length;
       return await api
         .post(
           '/redfish/v1/Managers/bmc/LogServices/Dump/Actions/LogService.ClearLog'
         )
         .then(() => {
-          commit('setBmcDumps', []);
+          commit('setAllDumps', []);
           return i18n.tc('pageDumps.toast.successDeleteDump', totalDumpCount);
         })
         .catch((error) => {
--- src/views/Logs/Dumps/DumpsForm.vue
+++ src/views/Logs/Dumps/DumpsForm.vue
@@ -34,10 +34,8 @@
 
 <script>
 import { required } from 'vuelidate/lib/validators';
-
 import ModalConfirmation from './DumpsModalConfirmation';
 import Alert from '@/components/Global/Alert';
-
 import BVToastMixin from '@/components/Mixins/BVToastMixin';
 import VuelidateMixin from '@/components/Mixins/VuelidateMixin.js';
 
@@ -62,9 +60,15 @@ export default {
     handleSubmit() {
       this.$v.$touch();
       if (this.$v.$invalid) return;
+
+      // Dumps intitiation
+
+      // System dump initiation
       if (this.selectedDumpType === 'system') {
         this.showConfirmationModal();
-      } else {
+      }
+      // BMC dump initiation
+      else if (this.selectedDumpType === 'bmc') {
         this.$store
           .dispatch('dumps/createBmcDump')
           .then(() =>
