aac31db559aa4fc2900f3d351f6acd35459c741a
Derick Montague <derick.montague@ibm.com>
2021-08-26 14:06:49 -0500

    Update login page layout
    This update will
    
    - Change the positioning of the form to be on the left
    - Add the built on OpenBMC logo to the bottom right corner of the
    screen
    - Add the ability to include a GUI custom name using a .env variable.
    If the variable is not present, the login page will not include the <h1>
    heading.
    
    Github story: https://github.com/openbmc/webui-vue/issues/63
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>
    Change-Id: I83ac5aecff0b3858c3ab5f38ab1aaa603d59acf1

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,23 +1,32 @@
 <template>
   <main>
-    <b-container class="login-container" fluid>
-      <b-row class="login-row" align-v="center">
-        <b-col class="login-branding mt-5 mb-5 text-center" md="6">
-          <div class="login-branding__container">
-            <img
-              class="logo"
-              width="200px"
-              src="@/assets/images/logo-login.svg"
-              :alt="altLogo"
-            />
-            <h1>OpenBMC</h1>
-          </div>
-        </b-col>
-        <b-col md="6">
-          <router-view />
-        </b-col>
-      </b-row>
-    </b-container>
+    <div class="login-container">
+      <div class="login-main">
+        <div class="login-brand mb-5">
+          <img
+            width="90px"
+            src="@/assets/images/login-company-logo.svg"
+            :alt="altLogo"
+          />
+        </div>
+        <h1 v-if="customizableGuiName" class="mb-5">
+          {{ customizableGuiName }}
+        </h1>
+        <router-view class="login=form form-background" />
+      </div>
+      <div class="login-aside">
+        <div class="login-aside__logo-brand">
+          <!-- Add Secondary brand logo if needed -->
+        </div>
+        <div class="login-aside__logo-bmc">
+          <img
+            height="60px"
+            src="@/assets/images/built-on-openbmc-logo.svg"
+            alt="Built on OpenBMC"
+          />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -26,7 +35,8 @@ export default {
   name: 'LoginLayout',
   data() {
     return {
-      altLogo: `${process.env.VUE_APP_COMPANY_NAME} logo`,
+      altLogo: process.env.VUE_APP_COMPANY_NAME || 'OpenBMC',
+      customizableGuiName: process.env.VUE_APP_GUI_NAME || '',
     };
   },
 };
@@ -34,25 +44,66 @@ export default {
 
 <style lang="scss" scoped>
 .login-container {
-  @include media-breakpoint-up(md) {
-    background: linear-gradient(
-      to right,
-      theme-color('light') 50%,
-      gray('200') 50%
-    );
+  display: flex;
+  flex-direction: column;
+  gap: $spacer * 2;
+  max-width: 1280px;
+  min-width: 320px;
+  height: 100vh;
+
+  @include media-breakpoint-up('md') {
+    flex-direction: row;
+  }
+
+  @include media-breakpoint-up('xxl') {
+    margin: auto;
   }
 }
 
-.login-row {
-  @include media-breakpoint-up(md) {
+.login-main {
+  background: gray('100');
+  min-height: 50vh;
+  padding: $spacer * 3;
+
+  @include media-breakpoint-up('md') {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 75%;
     min-height: 100vh;
+    padding: 0 $spacer * 5;
+    justify-content: center;
   }
 }
 
-.login-branding__container {
-  @include media-breakpoint-up(md) {
-    float: right;
-    margin-right: 4rem;
+.login-form {
+  @include media-breakpoint-up('md') {
+    max-width: 360px;
+  }
+}
+
+.login-aside {
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  gap: $spacer * 1.5;
+  margin-right: $spacer * 3;
+
+  @include media-breakpoint-up('md') {
+    min-height: 100vh;
+    padding-bottom: $spacer;
+    flex: 1 1 25%;
+  }
+}
+
+.login-aside__logo-brand:not(:empty) {
+  &::after {
+    content: '';
+    display: inline-block;
+    height: 2.5rem;
+    width: 2px;
+    background-color: gray('200');
+    margin-left: $spacer * 1.5;
+    vertical-align: middle;
   }
 }
 </style>
