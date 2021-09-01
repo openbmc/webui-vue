22057ceca8320398fc68ad7378f727b0610136c9
Grzegorz Raguza <grzegorzx.raguza@intel.com>
2021-09-01 14:07:17 +0000

    Added notification that session is about to expire
    Until now when user had long inactivity, after peformed
    action was redirected to login screen. Now, after 28min
    user will see notification, that the session will
    expire. User can click button Continue Session to keep
    session alive or do nothing. After next 2 minutes,
    message will dissapear and user will be logged off, and
    redirected to login screen.
    
    Signed-off-by: Grzegorz Raguza <grzegorzx.raguza@intel.com>
    Change-Id: Ie9c9876b37fde01ad8225d5451d1c4d9334903a7

--- src/store/api.js
+++ src/store/api.js
@@ -8,27 +8,32 @@ const api = Axios.create({
   withCredentials: true,
 });
 
-api.interceptors.response.use(undefined, (error) => {
-  let response = error.response;
+api.interceptors.response.use(
+  (response) => {
+    store.dispatch('authentication/setLogoutMessageTimeout');
+    return response;
+  },
+  (error) => {
+    let response = error.response;
 
-  // TODO: Provide user with a notification and way to keep system active
-  if (response.status == 401) {
-    if (response.config.url != '/login') {
-      window.location = '/login';
-      // Commit logout to remove XSRF-TOKEN cookie
-      store.commit('authentication/logout');
+    if (response.status == 401) {
+      if (response.config.url != '/login') {
+        window.location = '/login';
+        // Commit logout to remove XSRF-TOKEN cookie
+        store.commit('authentication/logout');
+      }
     }
-  }
 
-  if (response.status == 403) {
-    // Check if action is unauthorized.
-    // Toast error message will appear on screen
-    // when the action is unauthorized.
-    store.commit('global/setUnauthorized');
-  }
+    if (response.status == 403) {
+      // Check if action is unauthorized.
+      // Toast error message will appear on screen
+      // when the action is unauthorized.
+      store.commit('global/setUnauthorized');
+    }
 
-  return Promise.reject(error);
-});
+    return Promise.reject(error);
+  }
+);
 
 export default {
   get(path) {
--- src/store/modules/Authentication/AuthenticanStore.js
+++ src/store/modules/Authentication/AuthenticanStore.js
@@ -1,6 +1,24 @@
 import api from '@/store/api';
 import Cookies from 'js-cookie';
 import router from '@/router';
+import i18n from '@/i18n';
+
+const getTimeoutMessageOptions = (messageDelay, dispatch) => {
+  return {
+    text: i18n.t('global.toast.sessionTimeoutMessage'),
+    options: {
+      id: `session-toast-${Date.now()}`,
+      title: i18n.t('global.toast.sessionTimeoutTitle'),
+      variant: 'warning',
+      noCloseButton: true,
+      autoHideDelay: messageDelay,
+    },
+    button: {
+      title: i18n.t('global.toast.sessionTimeoutButton'),
+      click: () => dispatch('keepSession'),
+    },
+  };
+};
 
 const AuthenticationStore = {
   namespaced: true,
@@ -8,6 +26,8 @@ const AuthenticationStore = {
     authError: false,
     xsrfCookie: Cookies.get('XSRF-TOKEN'),
     isAuthenticatedCookie: Cookies.get('IsAuthenticated'),
+    authMessageTimeout: undefined,
+    authLogoutTimeout: undefined,
   },
   getters: {
     authError: (state) => state.authError,
@@ -33,6 +53,12 @@ const AuthenticationStore = {
       state.xsrfCookie = undefined;
       state.isAuthenticatedCookie = undefined;
     },
+    setLogoutMessageTimeout(state, payload) {
+      clearTimeout(state.authMessageTimeout);
+      clearTimeout(state.authLogoutTimeout);
+      state.authLogoutTimeout = payload.authLogoutTimeout;
+      state.authMessageTimeout = payload.authMessageTimeout;
+    },
   },
   actions: {
     login({ commit }, { username, password }) {
@@ -48,6 +74,7 @@ const AuthenticationStore = {
     logout({ commit }) {
       api
         .post('/logout', { data: [] })
+        .then(() => commit('setLogoutMessageTimeout', {}))
         .then(() => commit('logout'))
         .then(() => router.go('/login'))
         .catch((error) => console.log(error));
@@ -58,10 +85,52 @@ const AuthenticationStore = {
         .then(({ data: { PasswordChangeRequired } }) => PasswordChangeRequired)
         .catch((error) => console.log(error));
     },
-    resetStoreState({ state }) {
+    resetStoreState({ state, commit }) {
       state.authError = false;
       state.xsrfCookie = Cookies.get('XSRF-TOKEN');
       state.isAuthenticatedCookie = Cookies.get('IsAuthenticated');
+      commit('setLogoutMessageTimeout', {});
+    },
+    /**
+     * uses setTimeout function to create toast message,
+     * uses setTimeout function to ensure system logout
+     * @param context action context
+     */
+    setLogoutMessageTimeout({ commit, dispatch }) {
+      // we want to display message after 28min
+      const messageTimeout = 28 * 60 * 1000;
+      // we want to show message for 2 minutes
+      const messageDelay = 2 * 60 * 1000;
+      // then after sum of the above, logout user
+      const authTimeout = messageTimeout + messageDelay;
+
+      const authMessageTimeout = setTimeout(() => {
+        const message = getTimeoutMessageOptions(messageDelay, dispatch);
+        dispatch('displayToast', message, { root: true });
+      }, messageTimeout);
+
+      const authLogoutTimeout = setTimeout(
+        () => dispatch('logout'),
+        authTimeout
+      );
+
+      commit('setLogoutMessageTimeout', {
+        authMessageTimeout,
+        authLogoutTimeout,
+      });
+    },
+    /**
+     * calls endpoint to keep session alive
+     * @see http://redfish.dmtf.org/schemas/DSP0266_1.11.0.html#session-lifetime
+     * @param context action context
+     * @returns Promise
+     */
+    async keepSession({ commit }) {
+      commit('setLogoutMessageTimeout', {});
+      const username = localStorage.getItem('storedUsername');
+      return await api
+        .get(`/redfish/v1/AccountService/Accounts/${username}`)
+        .then();
     },
   },
 };
