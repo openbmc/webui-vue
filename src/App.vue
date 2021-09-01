f282b3d11971ed753c8bf92f9ccffc132a6e1cf5
Grzegorz Raguza <grzegorzx.raguza@intel.com>
2021-09-01 14:07:17 +0000

    Added notification that session is about to expire
    Until now when user had long inactivity, after performed action was
    redirected to login screen. Now, after 28min user will see notification,
    that the session will expire. User can click button Continue Session to
    keep session alive or do nothing. After next 2 minutes, message will
    dissappear and user will be logged off and redirected to login screen.
    
    Signed-off-by: Grzegorz Raguza <grzegorzx.raguza@intel.com>
    Change-Id: Ie9c9876b37fde01ad8225d5451d1c4d9334903a7

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,30 @@ export default {
     assetTag() {
       return this.$store.getters['global/assetTag'];
     },
+    toast() {
+      return this.$store.getters['toast'];
+    },
   },
   watch: {
     assetTag: function (tag) {
       if (tag) {
         document.title = `${tag} - ${this.$route.meta.title}`;
       }
+    },
+    toast: function ({ text, options, button: { title, click } }) {
+      const btn = this.$createElement(
+        'b-button',
+        {
+          on: {
+            click: () => {
+              this.$bvToast.hide(options.id);
+              click();
+            },
+          },
+        },
+        title
+      );
+      this.$bvToast.toast([text, btn], options);
     },
     $route: function (to) {
       document.title = to.meta.title || 'Page is missing title';
