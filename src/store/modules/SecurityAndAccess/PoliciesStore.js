1ea959b980d6c32e47829adc42c463cd4a88141c
Dixsie Wolmers <dixsie@ibm.com>
2021-09-16 16:15:07 -0500

    Move TPM policy to Policies page
    - TPM policy was removed from server power ops boot settings
    section and moved to Policies page.
    - Uses new Redfish endpoint
    - Refactored policy page to include aria-labelledby and
    aria-describedby for each toggle
    - Refactored toast messages
    - Removed unused translations from en-US for policy page
    
    Signed-off-by: Dixsie Wolmers <dixsie@ibm.com>
    Change-Id: I73ded824ec81cd20552ced20308ac80fe50b614d

--- src/store/modules/SecurityAndAccess/PoliciesStore.js
+++ src/store/modules/SecurityAndAccess/PoliciesStore.js
@@ -6,16 +6,20 @@ const PoliciesStore = {
   state: {
     sshProtocolEnabled: false,
     ipmiProtocolEnabled: false,
+    tpmPolicyEnabled: false,
   },
   getters: {
     sshProtocolEnabled: (state) => state.sshProtocolEnabled,
     ipmiProtocolEnabled: (state) => state.ipmiProtocolEnabled,
+    tpmPolicyEnabled: (state) => state.tpmPolicyEnabled,
   },
   mutations: {
     setSshProtocolEnabled: (state, sshProtocolEnabled) =>
       (state.sshProtocolEnabled = sshProtocolEnabled),
     setIpmiProtocolEnabled: (state, ipmiProtocolEnabled) =>
       (state.ipmiProtocolEnabled = ipmiProtocolEnabled),
+    setTpmPolicyEnabled: (state, tpmPolicyEnabled) =>
+      (state.tpmPolicyEnabled = tpmPolicyEnabled),
   },
   actions: {
     async getNetworkProtocolStatus({ commit }) {
@@ -29,6 +33,42 @@ const PoliciesStore = {
         })
         .catch((error) => console.log(error));
     },
+    async getTpmPolicy({ commit }) {
+      return await api
+        .get('/redfish/v1/Systems/system')
+        .then((response) => {
+          const tpmState = response.data.Boot.TrustedModuleRequiredToBoot;
+          commit('setTpmPolicyEnabled', tpmState === 'Required');
+        })
+        .catch((error) => console.log(error));
+    },
+    async saveTpmPolicy({ commit }, protocolEnabled) {
+      commit('setTpmPolicyEnabled', protocolEnabled);
+      const data = {
+        Boot: {
+          TrustedModuleRequiredToBoot: protocolEnabled,
+        },
+      };
+      // TODO: waiting for
+      // https://gerrit.openbmc-project.xyz/c/openbmc/bmcweb/+/46928 to merge in
+      // order to test patch
+      return api
+        .patch('/redfish/v1/Systems/system', data)
+        .then(() => {
+          return i18n.t('pagePolicies.toast.successNetworkPolicyUpdate', {
+            policy: i18n.t('pagePolicies.hostTpm'),
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          commit('setTpmPolicyEnabled', !protocolEnabled);
+          throw new Error(
+            i18n.t('pagePolicies.toast.errorNetworkPolicyUpdate', {
+              policy: i18n.t('pagePolicies.hostTpm'),
+            })
+          );
+        });
+    },
     async saveIpmiProtocolState({ commit }, protocolEnabled) {
       commit('setIpmiProtocolEnabled', protocolEnabled);
       const ipmi = {
@@ -39,20 +79,18 @@ const PoliciesStore = {
       return await api
         .patch('/redfish/v1/Managers/bmc/NetworkProtocol', ipmi)
         .then(() => {
-          if (protocolEnabled) {
-            return i18n.t('pagePolicies.toast.successIpmiEnabled');
-          } else {
-            return i18n.t('pagePolicies.toast.successIpmiDisabled');
-          }
+          return i18n.t('pagePolicies.toast.successNetworkPolicyUpdate', {
+            policy: i18n.t('pagePolicies.ipmi'),
+          });
         })
         .catch((error) => {
           console.log(error);
-          commit('setIpmiProtocolEnabled', !protocolEnabled);
-          if (protocolEnabled) {
-            throw new Error(i18n.t('pagePolicies.toast.errorIpmiEnabled'));
-          } else {
-            throw new Error(i18n.t('pagePolicies.toast.errorIpmiDisabled'));
-          }
+          commit('setTpmPolicyEnabled', !protocolEnabled);
+          throw new Error(
+            i18n.t('pagePolicies.toast.errorNetworkPolicyUpdate', {
+              policy: i18n.t('pagePolicies.ipmi'),
+            })
+          );
         });
     },
     async saveSshProtocolState({ commit }, protocolEnabled) {
@@ -65,20 +103,18 @@ const PoliciesStore = {
       return await api
         .patch('/redfish/v1/Managers/bmc/NetworkProtocol', ssh)
         .then(() => {
-          if (protocolEnabled) {
-            return i18n.t('pagePolicies.toast.successSshEnabled');
-          } else {
-            return i18n.t('pagePolicies.toast.successSshDisabled');
-          }
+          return i18n.t('pagePolicies.toast.successNetworkPolicyUpdate', {
+            policy: i18n.t('pagePolicies.ssh'),
+          });
         })
         .catch((error) => {
           console.log(error);
-          commit('setSshProtocolEnabled', !protocolEnabled);
-          if (protocolEnabled) {
-            throw new Error(i18n.t('pagePolicies.toast.errorSshEnabled'));
-          } else {
-            throw new Error(i18n.t('pagePolicies.toast.errorSshDisabled'));
-          }
+          commit('setTpmPolicyEnabled', !protocolEnabled);
+          throw new Error(
+            i18n.t('pagePolicies.toast.errorNetworkPolicyUpdate', {
+              policy: i18n.t('pagePolicies.ssh'),
+            })
+          );
         });
     },
   },
--- src/views/SecurityAndAccess/Policies/Policies.vue
+++ src/views/SecurityAndAccess/Policies/Policies.vue
@@ -4,11 +4,11 @@
     <b-row>
       <b-col md="8">
         <page-section :section-title="$t('pagePolicies.networkServices')">
-          <b-row v-if="!modifySSHPolicyDisabled" class="setting-section">
+          <b-row v-if="!modifySSHPolicyDisabled" class="section-divider">
             <b-col class="d-flex align-items-center justify-content-between">
               <dl class="mr-3 w-75">
-                <dt>{{ $t('pagePolicies.ssh') }}</dt>
-                <dd>
+                <dt id="ssh-label">{{ $t('pagePolicies.ssh') }}</dt>
+                <dd id="ssh-description">
                   {{ $t('pagePolicies.sshDescription') }}
                 </dd>
               </dl>
@@ -16,12 +16,11 @@
                 id="sshSwitch"
                 v-model="sshProtocolState"
                 data-test-id="policies-toggle-bmcShell"
+                aria-labelledby="ssh-label"
+                aria-describedby="ssh-description"
                 switch
                 @change="changeSshProtocolState"
               >
-                <span class="sr-only">
-                  {{ $t('pagePolicies.ssh') }}
-                </span>
                 <span v-if="sshProtocolState">
                   {{ $t('global.status.enabled') }}
                 </span>
@@ -29,11 +28,11 @@
               </b-form-checkbox>
             </b-col>
           </b-row>
-          <b-row class="setting-section">
+          <b-row class="section-divider">
             <b-col class="d-flex align-items-center justify-content-between">
               <dl class="mt-3 mr-3 w-75">
-                <dt>{{ $t('pagePolicies.ipmi') }}</dt>
-                <dd>
+                <dt id="ipmi-label">{{ $t('pagePolicies.ipmi') }}</dt>
+                <dd id="ipmi-description">
                   {{ $t('pagePolicies.ipmiDescription') }}
                 </dd>
               </dl>
@@ -41,14 +40,36 @@
                 id="ipmiSwitch"
                 v-model="ipmiProtocolState"
                 data-test-id="polices-toggle-networkIpmi"
+                aria-labelledby="ipmi-label"
+                aria-describedby="ipmi-description"
                 switch
                 @change="changeIpmiProtocolState"
               >
-                <span class="sr-only">
-                  {{ $t('pagePolicies.ipmi') }}
-                </span>
                 <span v-if="ipmiProtocolState">
                   {{ $t('global.status.enabled') }}
+                </span>
+                <span v-else>{{ $t('global.status.disabled') }}</span>
+              </b-form-checkbox>
+            </b-col>
+          </b-row>
+          <b-row class="section-divider">
+            <b-col class="d-flex align-items-center justify-content-between">
+              <dl class="mt-3 mr-3 w-75">
+                <dt id="host-tpm-label">{{ $t('pagePolicies.hostTpm') }}</dt>
+                <dd id="host-tpm-description">
+                  {{ $t('pagePolicies.hostTpmDescription') }}
+                </dd>
+              </dl>
+              <b-form-checkbox
+                id="host-tpm-policy"
+                v-model="tpmPolicyState"
+                aria-labelledby="host-tpm-label"
+                aria-describedby="host-tpm-description"
+                switch
+                @change="changeTpmPolicyState"
+              >
+                <span v-if="tpmPolicyState">
+                  {{ $t('global.form.required') }}
                 </span>
                 <span v-else>{{ $t('global.status.disabled') }}</span>
               </b-form-checkbox>
@@ -82,6 +103,9 @@ export default {
     };
   },
   computed: {
+    networkProtocol() {
+      return this.$store.getters['policies/networkProtocol'];
+    },
     sshProtocolState: {
       get() {
         return this.$store.getters['policies/sshProtocolEnabled'];
@@ -98,9 +122,18 @@ export default {
         return newValue;
       },
     },
+    tpmPolicyState: {
+      get() {
+        return this.$store.getters['policies/tpmPolicyEnabled'];
+      },
+      set(newValue) {
+        return newValue;
+      },
+    },
   },
   created() {
     this.startLoader();
+    this.$store.dispatch('policies/getTpmPolicy');
     this.$store
       .dispatch('policies/getNetworkProtocolStatus')
       .finally(() => this.endLoader());
@@ -118,12 +151,12 @@ export default {
         .then((message) => this.successToast(message))
         .catch(({ message }) => this.errorToast(message));
     },
+    changeTpmPolicyState(state) {
+      this.$store
+        .dispatch('policies/saveTpmPolicy', state)
+        .then((message) => this.successToast(message))
+        .catch(({ message }) => this.errorToast(message));
+    },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.setting-section {
-  border-bottom: 1px solid gray('300');
-}
-</style>
