18cde3ce0c1b2f99e94f5cef66661adcb22ba8f7
Nikhil Ashoka <a.nikhil@ibm.com>
2022-01-05 22:21:24 +0530

    Add values to system inventory table
    TotalSystemMemoryGiB and CoreCount field is not present.
    
    Added TotalSystemMemoryGiB under Memory summary
    Added CoreCount under Processor summary
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>
    Change-Id: I598abc7c05dd5113db0071ecd71bbcc32ad696c4

--- src/store/modules/HardwareStatus/SystemStore.js
+++ src/store/modules/HardwareStatus/SystemStore.js
@@ -17,6 +17,7 @@ const SystemStore = {
       system.firmwareVersion = data.BiosVersion;
       system.hardwareType = data.Name;
       system.health = data.Status?.Health;
+      system.totalSystemMemoryGiB = data.MemorySummary?.TotalSystemMemoryGiB;
       system.id = data.Id;
       system.locationIndicatorActive = data.LocationIndicatorActive;
       system.locationNumber = data.Location?.PartLocation?.ServiceLabel;
@@ -27,6 +28,7 @@ const SystemStore = {
       system.memorySummaryState = data.MemorySummary?.Status?.State;
       system.model = data.Model;
       system.processorSummaryCount = data.ProcessorSummary?.Count;
+      system.processorSummaryCoreCount = data.ProcessorSummary?.CoreCount;
       system.processorSummaryHealth = data.ProcessorSummary?.Status?.Health;
       system.processorSummaryHealthRoll =
         data.ProcessorSummary?.Status.HealthRollup;
