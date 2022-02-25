6d509ed7e37f328ecbb79875fd67215bf909e1eb
MichalX Szopinski <michalx.szopinski@intel.com>
2022-02-25 13:13:27 +0100

    Remove hardcoded chassis from Fan store
    This change removes the hardcoded chassis URL from FanStore. Now
    the URL is taken from the odata.id.
    
    Signed-off-by: MichalX Szopinski <michalx.szopinski@intel.com>
    Change-Id: I59e4d46a8aaa453f6662f4f396f32d7fad18fb91

--- src/store/modules/HardwareStatus/FanStore.js
+++ src/store/modules/HardwareStatus/FanStore.js
@@ -38,9 +38,21 @@ const FanStore = {
     },
   },
   actions: {
-    async getFanInfo({ commit }) {
+    async getChassisCollection() {
       return await api
-        .get('/redfish/v1/Chassis/chassis/Thermal')
+        .get('/redfish/v1/')
+        .then((response) => api.get(response.data.Chassis['@odata.id']))
+        .then(({ data: { Members } }) =>
+          Members.map((member) => member['@odata.id'])
+        )
+        .catch((error) => console.log(error));
+    },
+    async getFanInfo({ dispatch, commit }) {
+      const collection = await dispatch('getChassisCollection');
+      if (!collection || collection.length === 0) return;
+      return await api
+        .get(`${collection[0]}`)
+        .then((response) => api.get(response.data.Thermal['@odata.id']))
         .then(({ data: { Fans = [] } }) => commit('setFanInfo', Fans))
         .catch((error) => console.log(error));
     },
