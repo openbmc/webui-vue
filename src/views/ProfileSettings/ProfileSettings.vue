9bfe68fa587260e6aa0afa3f2972bc1ca0d9f897
Damian Celico <damianx.celico@intel.com>
2022-08-23 03:18:58 +0200

    Old password input in change password screen
    Added field with the current password, required to change
    password in profile setting, to prevent xss attacks
    
    Signed-off-by: Damian Celico <damianx.celico@intel.com>
    Change-Id: Idb8bc9d6ada420329c38407da76a08dc83fddd61

--- src/views/ProfileSettings/ProfileSettings.vue
+++ src/views/ProfileSettings/ProfileSettings.vue
@@ -24,6 +24,22 @@
             :section-title="$t('pageProfileSettings.changePassword')"
           >
             <b-form-group
+              id="input-group-0"
+              :label="$t('pageProfileSettings.currentPassword')"
+              label-for="input-0"
+            >
+              <input-password-toggle>
+                <b-form-input
+                  id="old-password"
+                  v-model="form.currentPassword"
+                  type="password"
+                  data-test-id="profileSettings-input-ocurrentPassword"
+                  class="form-control-with-button"
+                  @input="$v.form.currentPassword.$touch()"
+                />
+              </input-password-toggle>
+            </b-form-group>
+            <b-form-group
               id="input-group-1"
               :label="$t('pageProfileSettings.newPassword')"
               label-for="input-1"
@@ -151,6 +167,7 @@ export default {
       form: {
         newPassword: '',
         confirmPassword: '',
+        currentPassword: '',
         isUtcDisplay: this.$store.getters['global/isUtcDisplay'],
       },
     };
@@ -198,7 +215,9 @@ export default {
       this.$store
         .dispatch('userManagement/updateUser', userData)
         .then((message) => {
-          (this.form.newPassword = ''), (this.form.confirmPassword = '');
+          (this.form.newPassword = ''),
+            (this.form.confirmPassword = ''),
+            (this.form.currentPassword = '');
           this.$v.$reset();
           this.successToast(message);
         })
@@ -212,10 +231,33 @@ export default {
       );
     },
     submitForm() {
-      if (this.form.confirmPassword || this.form.newPassword) {
-        this.saveNewPasswordInputData();
+      if (
+        this.form.confirmPassword &&
+        this.form.newPassword &&
+        this.form.currentPassword
+      ) {
+        this.confirmAuthenticate();
       }
       this.saveTimeZonePrefrenceData();
+    },
+    confirmAuthenticate() {
+      this.$v.form.newPassword.$touch();
+      if (this.$v.$invalid) return;
+
+      const username = this.username;
+      const password = this.form.currentPassword;
+
+      this.$store
+        .dispatch('authentication/login', { username, password })
+        .then(() => {
+          this.saveNewPasswordInputData();
+        })
+        .catch(() => {
+          this.$v.$reset();
+          this.errorToast(
+            this.$t('pageProfileSettings.toast.wrongCredentials')
+          );
+        });
     },
   },
 };
