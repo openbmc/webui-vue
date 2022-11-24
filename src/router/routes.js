9eeedcbd5f7c7ce475781c299bc05d890f730843
Damian Celico <damianx.celico@intel.com>
2022-11-24 02:00:53 +0100

    Added route restrictions based on user privilege
    This commit allows us to add 'exclusiveToRoles' field to
    route config files, with the list of roles that can access
    this resource, if needed. In this case, only Administrator
    can access Virtual-Media page, and it is blocked for other
    users.
    
    Signed-off-by: Sivaprabu Ganesan <sivaprabug@ami.com>
    Change-Id: Ibcee18bd92d97c34414ecaf2caf6af28070c5538

--- src/router/routes.js
+++ src/router/routes.js
@@ -31,6 +31,13 @@ import VirtualMedia from '@/views/Operations/VirtualMedia';
 import Power from '@/views/ResourceManagement/Power';
 import i18n from '@/i18n';
 
+const roles = {
+  administrator: 'Administrator',
+  operator: 'Operator',
+  readonly: 'ReadOnly',
+  noaccess: 'NoAccess',
+};
+
 const routes = [
   {
     path: '/login',
@@ -269,6 +276,7 @@ const routes = [
         component: VirtualMedia,
         meta: {
           title: i18n.t('appPageTitle.virtualMedia'),
+          exclusiveToRoles: [roles.administrator],
         },
       },
       {
