0df5cd07bd36f3e9e16d63ea674ab8d7dbc75a6b
Konstantin Aladyshev <aladyshev22@gmail.com>
2023-02-15 12:49:11 +0000

    Add button to clear POST code logs
    Utilize '#LogService.ClearLog' action to provide a possibility for the
    user to clear POST code logs.
    
    Change-Id: Iadd94b9ebc93bc81b13ee979962103587c939fc7
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- src/store/modules/Logs/PostCodeLogsStore.js
+++ src/store/modules/Logs/PostCodeLogsStore.js
@@ -1,4 +1,5 @@
 import api from '@/store/api';
+import i18n from '@/i18n';
 
 const PostCodeLogsStore = {
   namespaced: true,
@@ -31,6 +32,22 @@ const PostCodeLogsStore = {
         })
         .catch((error) => {
           console.log('POST Codes Log Data:', error);
+        });
+    },
+    async deleteAllPostCodeLogs({ dispatch }, data) {
+      return await api
+        .post(
+          '/redfish/v1/Systems/system/LogServices/PostCodes/Actions/LogService.ClearLog'
+        )
+        .then(() => dispatch('getPostCodesLogData'))
+        .then(() =>
+          i18n.tc('pagePostCodeLogs.toast.successDelete', data.length)
+        )
+        .catch((error) => {
+          console.log(error);
+          throw new Error(
+            i18n.tc('pagePostCodeLogs.toast.errorDelete', data.length)
+          );
         });
     },
   },
