71d2ef8589c965f1f86bc3ffcd07d42a1f1e545a
kirankumarb07 <kirankumarb@ami.com>
2023-03-21 19:19:02 +0530

    Fix User Management Page issues title and Policy
    This patchset will fix the following issues in the user management page
    
    1. "lockoutThreshold" and "lockoutDuration" values are not shown in the
    account policy model when we navigate to any other page and come back
    to the user management page account policy model.
    Fix - The "lockoutThreshold" and "lockoutDuration" values are monitored
    and updated in the model window by using the watch. When navigated from
    another page, the watch is not updating the values, so the default
    values are displayed. Initialized the value with an empty object, so
    the watch will get triggered and update the fields with proper values.
    
    2. The User Management Page title is not translated properly. The
    router names are used to map each page's language objects. For the user
    management page, the router name and the object value in the locale
    file are different. Due to that, the title did not get translated
    properly.
    Fix - Changed the user management page title to match the locale file's
    language object key value.
    
    Change-Id: I1582c9d528633465c65f5d78e31d3dc740252cb0
    Signed-off-by: Kirankumar Ballapalli <kirankumarb@ami.com>

--- src/router/routes.js
+++ src/router/routes.js
@@ -153,7 +153,7 @@ const routes = [
       },
       {
         path: '/security-and-access/user-management',
-        name: 'local-users',
+        name: 'user-management',
         component: UserManagement,
         meta: {
           title: i18n.t('appPageTitle.userManagement'),
