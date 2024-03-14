77e8cbb0683fa0e9f484dac1de34434a9a7d258e
Konstantin Maskov <k.maskov@gagarin.me>
2024-03-14 13:47:22 +0300

    Add event destination settings page
    Implemented Event Destination Settings page includes:
    
      - List of created subscriptions.
    
      - Add subscription modal
        (Remote syslog protocol is supported).
    
      - Test message sending.
    
    Change-Id: I85970cbed65144d47a7bcde0e11451b52a7e4e55
    Signed-off-by: Konstantin Maskov <k.maskov@gagarin.me>

---
+++ src/views/Settings/EventSettings/ModalSubscription.vue
@@ -0,0 +1,191 @@
+<template>
+  <b-modal id="modal-subscription" @show="getSyslogInitial" @hidden="resetForm">
+    <template #modal-title>
+      {{ $t('pageEventSettings.addSubscription') }}
+    </template>
+
+    <b-form
+      id="form-subscription"
+      ref="formSubscription"
+      novalidate
+      @submit.prevent="submitForm"
+    >
+      <b-form-group>
+        <page-section>
+          <b-row>
+            <b-col lg="6">
+              <b-form-group
+                :label="$t('pageEventSettings.form.protocol')"
+                label-for="subscription-protocol"
+              >
+                <b-form-select
+                  id="subscription-protocol"
+                  v-model="form.protocol"
+                  data-test-id="eventsettings-subscription-protocol"
+                  :options="protocols"
+                />
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row v-if="form.protocol === 'Syslog'">
+            <b-col lg="6">
+              <b-form-group>
+                <b-form-radio-group v-model="form.syslogProtocol">
+                  <b-form-radio
+                    id="subscription-tcp"
+                    data-test-id="eventsettings-subscription-tcp"
+                    value="SyslogTCP"
+                    text="TCP"
+                  >
+                    TCP
+                  </b-form-radio>
+                  <b-form-radio
+                    id="subscription-udp"
+                    data-test-id="eventsettings-subscription-udp"
+                    value="SyslogUDP"
+                  >
+                    UDP
+                  </b-form-radio>
+                </b-form-radio-group>
+              </b-form-group>
+              <b-form-group
+                label-for="subscription-ip"
+                :label="$t('pageEventSettings.form.ip')"
+              >
+                <b-form-input
+                  id="subscription-ip"
+                  v-model="form.ip"
+                  data-test-id="eventsettings-subscription-ip"
+                  type="text"
+                />
+              </b-form-group>
+              <b-form-group
+                label-for="subscription-port"
+                :label="$t('pageEventSettings.form.port')"
+              >
+                <b-form-input
+                  id="subscription-port"
+                  v-model="form.port"
+                  data-test-id="eventsettings-subscription-port"
+                  type="text"
+                />
+              </b-form-group>
+              <b-form-text
+                v-if="form.protocol === 'Syslog'"
+                id="syslog-help-block"
+              >
+                {{ $t('pageEventSettings.syslogHelp') }}
+              </b-form-text>
+            </b-col>
+          </b-row>
+        </page-section>
+      </b-form-group>
+    </b-form>
+
+    <template #modal-footer="{ cancel }">
+      <b-button
+        variant="secondary"
+        data-test-id="subscription-button-cancel"
+        @click="cancel"
+      >
+        {{ $t('global.action.cancel') }}
+      </b-button>
+      <b-button
+        form="form-subscription"
+        data-test-id="subscription-button-save"
+        type="submit"
+        variant="primary"
+      >
+        {{ $t('global.action.add') }}
+      </b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import PageSection from '@/components/Global/PageSection';
+import BVToastMixin from '@/components/Mixins/BVToastMixin';
+import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
+import { required, helpers } from 'vuelidate/lib/validators';
+
+const validateIp = helpers.regex(
+  'validateIp',
+  /^(?=.*[^.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/,
+);
+const validatePort = helpers.regex('validatePort', /^[0-9]{3,4}$/);
+
+export default {
+  components: {
+    PageSection,
+  },
+  mixins: [LoadingBarMixin, BVToastMixin],
+  data() {
+    return {
+      form: {
+        protocol: '',
+        syslogProtocol: '',
+        ip: '',
+        port: '',
+      },
+      protocols: [{ value: 'Syslog', text: 'Remote Syslog' }],
+      protocolFormats: {
+        SyslogTCP: 'tcp',
+        SyslogUDP: 'udp',
+      },
+      syslogInitial: {},
+    };
+  },
+  validations() {
+    return {
+      form: {
+        protocol: {
+          required,
+        },
+        ip: {
+          pattern: validateIp,
+        },
+        port: {
+          pattern: validatePort,
+        },
+      },
+    };
+  },
+  methods: {
+    getSyslogInitial() {
+      this.form.ip = this.$store.getters['eventSettings/getSyslogIp'];
+      this.form.port = this.$store.getters['eventSettings/getSyslogPort'];
+      this.form.syslogProtocol =
+        this.$store.getters['eventSettings/getSyslogProtocol'];
+    },
+    resetForm() {
+      this.form.protocol = '';
+      this.$v.$reset();
+    },
+    submitForm() {
+      this.$v.$touch();
+      if (this.$v.$invalid) return;
+      this.startLoader();
+
+      let subscriptionForm = {
+        protocol: this.form.syslogProtocol,
+        destination:
+          this.protocolFormats[this.form.syslogProtocol] +
+          '://' +
+          this.form.ip +
+          ':' +
+          this.form.port,
+      };
+
+      this.$store
+        .dispatch('eventSettings/createSubscription', subscriptionForm)
+        .then((success) => this.successToast(success))
+        .catch(({ message }) => this.errorToast(message))
+        .finally(() => {
+          this.$v.$reset();
+          this.endLoader();
+        });
+      this.$bvModal.hide('modal-subscription');
+    },
+  },
+};
+</script>
