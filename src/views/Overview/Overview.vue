59e8151ab070f123535ce6a8993a1ac53e63312d
Ed Tanous <ed@tanous.net>
2024-03-23 14:56:34 -0700

    Changes from Surya Venkatesan
    These changes were originally stacked in[1].  For easier review they've
    been broken out.
    
    [1] https://gerrit.openbmc.org/c/openbmc/webui-vue/+/55438/5..6
    
    Change-Id: I9464d82c76dcc1445ce271983ea3ab9d7b03d265
    Signed-off-by: Surya V <suryav@ami.com>

--- src/views/Overview/Overview.vue
+++ src/views/Overview/Overview.vue
@@ -37,6 +37,7 @@ import OverviewQuickLinks from './OverviewQuickLinks';
 import OverviewServer from './OverviewServer';
 import PageSection from '@/components/Global/PageSection';
 import PageTitle from '@/components/Global/PageTitle';
+import { useI18n } from 'vue-i18n';
 
 export default {
   name: 'Overview',
@@ -55,6 +56,7 @@ export default {
   mixins: [LoadingBarMixin],
   data() {
     return {
+      $t: useI18n().t,
       showDumps: process.env.VUE_APP_ENV_NAME === 'ibm',
     };
   },
