582e954ecff4edf58c143dc644a21b15005e8109
Sean Zhang <xiazhang@nvidia.com>
2024-07-05 12:48:45 +0300

    Fix single event entry download
    For event entry download, the href not work since the event entry
    download only work with header of "Accept: application/octet-stream" or
    the default "*/*", change to click function to make it work.
    
    Refer: https://gerrit.openbmc.org/c/openbmc/bmcweb/+/40136
    
    Change-Id: I11051e913bfd71ef081bed93ffcbeeb1edd8c730
    Signed-off-by: Sean Zhang <xiazhang@nvidia.com>

--- src/store/modules/Logs/EventLogStore.js
+++ src/store/modules/Logs/EventLogStore.js
@@ -220,6 +220,22 @@ const EventLogStore = {
           throw new Error(i18n.t('pageEventLogs.toast.errorLogStatusUpdate'));
         });
     },
+    async downloadEntry(_, uri) {
+      return await api
+        .get(uri)
+        .then((response) => {
+          const blob = new Blob([response.data], {
+            type: response.headers['content-type'],
+          });
+          return blob;
+        })
+        .catch((error) => {
+          console.log(error);
+          throw new Error(
+            i18n.t('pageEventLogs.toast.errorDownloadEventEntry'),
+          );
+        });
+    },
   },
 };
 
