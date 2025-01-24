10308ece891e72caf6306922018632d7f26c2870
suryav9724 <suryav@ami.com>
2025-01-24 15:06:35 +0530

    style: add Sass configuration for Vue 3
    Configure sass in the vue.config.js file and remove the
    import of the scss file from each page. The styles are
    handled by the css loaderOptions in the config file.
    Every component and page has the appropriate style applied.
    The appearance adjusts to the openBmc webui in Vue 2.
    
    Change-Id: I3773c063646185b80b7bd61d05aa80a993cbd121
    Signed-off-by: Surya Venkatesan <suryav@ami.com>

--- src/components/Global/StatusIcon.vue
+++ src/components/Global/StatusIcon.vue
@@ -34,9 +34,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/bmc/helpers/_index.scss';
-@import '@/assets/styles/bootstrap/_helpers.scss';
-
 .status-icon {
   vertical-align: text-bottom;
 
--- src/components/Global/TableFilter.vue
+++ src/components/Global/TableFilter.vue
@@ -111,11 +111,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/bmc/helpers/_index.scss';
-@import '@/assets/styles/bootstrap/_helpers.scss';
-
-@import 'bootstrap/dist/css/bootstrap.css';
-
 .badge {
   margin-right: $spacer / 2;
 }
