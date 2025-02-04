c03282a72bf7b264e061f9ff1cea95ff5150d183
tiwari-nishant <tiwari.nishant@ibm.com>
2025-02-04 13:12:53 +0530

    Added PCIe Slots Table in Inventory and LEDs Page
    - Implemented a table to display Slot Type, Location Number,
      PCIe Type, State and Lanes for all PCIe Slots.
    
    - Added search functionality and Sorting Functionality.
    
    Change-Id: Ie37b8c44b064fb289baa4f82994bbf0218fbce7a
    Signed-off-by: tiwari-nishant <tiwari.nishant@ibm.com>

---
+++ src/store/modules/HardwareStatus/PcieSlotsStore.js
@@ -0,0 +1,51 @@
+import api from '@/store/api';
+
+const PcieSlotsStore = {
+  namespaced: true,
+  state: {
+    pcieSlots: [],
+  },
+  getters: {
+    pcieSlots: (state) => state.pcieSlots,
+  },
+  mutations: {
+    setPcieSlotsInfo: (state, data) => {
+      state.pcieSlots = data.map((slot) => {
+        const { PCIeType, Status, Lanes, Location, SlotType } = slot;
+        return {
+          type: SlotType,
+          pcieType: PCIeType,
+          lanes: Lanes,
+          statusState: Status?.State ? Status.State : '',
+          locationNumber: Location?.PartLocation?.ServiceLabel,
+        };
+      });
+    },
+  },
+  actions: {
+    async getChassisInfo() {
+      return await api.get(`/redfish/v1/Chassis`).then(({ data }) => {
+        return data?.Members;
+      });
+    },
+    async getPcieSlotsInfo({ dispatch, commit }) {
+      commit('setPcieSlotsInfo', []);
+      let tempPcieSlots = [];
+      const chassisInfo = await dispatch('getChassisInfo');
+      console.log('chassisInfo', chassisInfo);
+      await api.all(
+        chassisInfo.map(async (singleChassis) => {
+          return await api
+            .get(`${singleChassis['@odata.id']}/PCIeSlots`)
+            .then(({ data }) => {
+              tempPcieSlots.push(...data.Slots);
+            })
+            .catch((error) => console.log(error));
+        }),
+      );
+      commit('setPcieSlotsInfo', tempPcieSlots);
+    },
+  },
+};
+
+export default PcieSlotsStore;
