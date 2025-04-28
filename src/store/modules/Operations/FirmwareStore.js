250c774f93468bd0fe512761ca2c27b31ce2c887
Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>
2025-04-28 11:01:16 -0700

    Add applyTime option for uploadFirmwareMultipartHttpPush
    Add the applyTime option for the uploadFirmwareMultipartHttpPush call
    and set its default value to ‘Immediate’.
    
    Change-Id: I7e9a442ef0bd4487b67b921761b51b603a77d9ed
    Signed-off-by: Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>

--- src/store/modules/Operations/FirmwareStore.js
+++ src/store/modules/Operations/FirmwareStore.js
@@ -139,7 +139,10 @@ const FirmwareStore = {
           );
         });
     },
-    async uploadFirmwareMultipartHttpPush({ state }, { image, targets }) {
+    async uploadFirmwareMultipartHttpPush(
+      { state },
+      { image, targets, forceUpdate, applyTime = 'Immediate' },
+    ) {
       const formData = new FormData();
       formData.append('UpdateFile', image);
       let params = {};
@@ -150,6 +153,8 @@ const FirmwareStore = {
         // when bmcweb is updated
         params.Targets = [`${await this.dispatch('global/getBmcPath')}`];
       }
+      if (forceUpdate) params.ForceUpdate = true;
+      params['@Redfish.OperationApplyTime'] = applyTime;
       formData.append('UpdateParameters', JSON.stringify(params));
       return await api
         .post(state.multipartHttpPushUri, formData, {
