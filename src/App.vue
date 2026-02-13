2a234d63a3cfc1fe73b958248f42b92f33633e0f
Aravinth S <aravinths@ami.com>
2026-02-13 19:07:44 +0530

    Fix redirect handling to prevent Redfish logout
    Updated the WebUI redirect logic to ensure users remain authenticated
    when navigating Redfish sub-links. Previously, clicking a Redfish
    sub-link in the browser caused an unintended logout, while accessing
    WebUI pages directly did not show this issue.
    
    The problem was due to incorrect handling of Redfish API paths,
    resulting in session termination during redirects. This change adds
    proper checks for /redfish/v1 paths and preserves the correct Redfish
    route so that users are redirected appropriately without losing their
    session.
    
    Change-Id: I9de4d26beeb371067898f0cb7789278e8be8f3a9
    Signed-off-by: Aravinth Sri Krishna Raja Raghavan <aravinths@ami.com>

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 <script>
 import ConfirmModal from '@/components/Global/ConfirmModal.vue';
 import { BOrchestrator } from 'bootstrap-vue-next';
+import Cookies from 'js-cookie';
 
 export default {
   name: 'App',
@@ -36,6 +37,42 @@ export default {
   created() {
     document.title = '';
     //document.title = this.$route.meta.title || 'Page is missing title';
+    setTimeout(() => {
+      if (
+        Cookies.get('XSRF-TOKEN') &&
+        Cookies.get('loginSessionSuccess') === 'false' &&
+        window.location.href.indexOf('/redfish/v1') != -1
+      ) {
+        this.$bvModal.msgBoxOk(
+          this.$tc('global.action.same_session_running_infomation'),
+        );
+      }
+    }, 2000); // wait to read the XSRF-TOKEN
+  },
+  beforeUnmount() {
+    window.removeEventListener('beforeunload', this.handleRefresh);
+  },
+  methods: {
+    // Perform logout or any other actions needed before unload
+    // This function will be called before the page is unloaded or refreshed
+    handleRefresh(event) {
+      if (
+        window.location.href.indexOf('/redfish/v1') != -1
+      ) {
+        if (event.srcElement.URL.indexOf('login') == -1) {
+          console.log('event:', event);
+          store.dispatch('authentication/logout');
+          //Due to firefox browser behaviour keep busy the browser to logout
+          if (
+            window.navigator.userAgent.toLowerCase().indexOf('firefox') != -1
+          ) {
+            for (let i = 0; i <= 1000; i++) {
+              console.log();
+            }
+          }
+        }
+      }
+    },
   },
 };
 </script>
